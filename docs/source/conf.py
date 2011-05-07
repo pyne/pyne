d189f6b4669ae76aeb930fd77029b9e0b0517190
Anthony Scopatz <scopatz@gmail.com>
2011-05-06 19:34:50 -0500

    Prepping the numpy/scipy/ipython sphinx extentions.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,13 +16,14 @@ import sys, os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.append(os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../sphinxext'))
 
 # -- General configuration -----------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.pngmath', 'sphinx.ext.inheritance_diagram']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.pngmath', 
+              'sphinx.ext.inheritance_diagram', 'numpydoc',]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
