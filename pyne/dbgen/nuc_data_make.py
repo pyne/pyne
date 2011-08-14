d735230a97009ea40c7680a338a1af217488f7f1
Anthony Scopatz <ascopatz@enthought.com>
2011-08-14 14:47:37 -0500

    Made nuc_data_make into real program, commandline arguments and everything.

--- pyne/dbgen/nuc_data_make.py
+++ pyne/dbgen/nuc_data_make.py
@@ -1,11 +1,14 @@
 import os
+import argparse
+from distutils.file_util import copy_file, move_file
+from distutils.dir_util import mkpath, remove_tree
 
 from pyne.api import nuc_data
 from pyne.dbgen.api import build_dir
 from pyne.dbgen.atomic_weight import make_atomic_weight
 
 # Thanks to http://patorjk.com/software/taag/
-# and http://www.chris.com/ascii/index.php?art=creatures/dragons
+# and http://www.chris.com/ascii/index.php?art=creatures/dragons (Jeff Ferris)
 # for ASCII art inspiriation
 
 pyne_logo = """\
@@ -39,14 +42,41 @@ pyne_logo = """\
 def main():
     print pyne_logo
 
-    # Make the build dir
-    try:
-        os.makedirs(build_dir)
-    except OSError:
-        pass
+    # Parse the command line arguments
+    parser = argparse.ArgumentParser(description='Make a nuclear data library.')
+    parser.add_argument('-o', dest='nuc_data', action='store', default=nuc_data,
+                        help='path to the output database file.')
+    parser.add_argument('-b', dest='build_dir', action='store', default=build_dir,
+                        help='path to the build directory.')
+    parser.add_argument('--clean', dest='clean', type=int, default=0,
+                        help="""level to clean up existing files.
+                                0: no cleaning (default).
+                                1: clean nuc_data.
+                                2: clean nuc_data and build_dir.""")
+    args = parser.parse_args()
 
-    print "Making nuc_data.h5 at {0}".format(nuc_data)
-    make_atomic_weight(nuc_data, build_dir)
+    # clean nuc data
+    if args.clean in [1, 2]:
+        print "removing nuc_data from {0}".format(args.nuc_data)
+        try:
+            os.remove(args.nuc_data)
+        except OSError:
+            pass
+
+    # Make the build dir
+    if args.clean == 2:
+        print "removing build_dir from {0}".format(args.build_dir)
+        remove_tree(args.build_dir)
+    mkpath(args.build_dir)
+
+    # Stop after cleaning
+    if 0 < args.clean:
+        return
+
+    print "Making nuc_data at {0}".format(args.nuc_data)
+
+    # make atomic weight table
+    make_atomic_weight(args.nuc_data, args.build_dir)
 
 
 
