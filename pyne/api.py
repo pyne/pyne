17c8ec2f918abf1cef0a64a3bb14d6de73e9737e
Anthony Scopatz <ascopatz@enthought.com>
2011-08-13 21:23:08 -0500

    Added awesome ASCII logo.  That was totally worth the hour it took.

--- pyne/api.py
+++ pyne/api.py
@@ -1,5 +1,5 @@
 # Important config tools
-from pyne.pyne_config import nuc_data, pyne_start, pyne_config
+from pyne.pyne_config import nuc_data, pyne_start, pyne_conf
 
 # importnat naming functions
 from pyne.nucname import zzaaam, nuc_weight
