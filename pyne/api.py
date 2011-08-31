e7e5c6b3e818cbbfad675082d0d9daa06ac897cf
Anthony Scopatz <ascopatz@enthought.com>
2011-08-30 21:48:56 -0500

    Updated api

--- pyne/api.py
+++ pyne/api.py
@@ -2,7 +2,10 @@
 from pyne.pyne_config import nuc_data, pyne_start, pyne_conf
 
 # Important naming functions
-from pyne.nucname import zzaaam, name, nuc_weight
+from pyne.nucname import zzaaam, name
+
+# Get common data functions
+from pyne.data import nuc_weight
 
 # Materials
 from pyne.material import Material
