e6fdb5b53a5c4f66ee212bae7a4bd551ab4b4477
Anthony Scopatz <ascopatz@enthought.com>
2011-09-05 20:07:12 -0500

    Some changes to dbgen.

--- pyne/dbgen/kaeri.py
+++ pyne/dbgen/kaeri.py
@@ -1,4 +1,5 @@
 import os
+import re
 import urllib2
 
 from pyne import nucname
@@ -23,18 +24,17 @@ def grab_kaeri_nuclide(nuc, build_dir="", n=None):
 
     if n is None:
         filename = os.path.join(build_dir, nuc + '.html')
+        kaeri_url = 'http://atom.kaeri.re.kr/cgi-bin/nuclide?nuc={0}'.format(nuc)
     else:
         filename = os.path.join(build_dir, '{nuc}_{n}.html'.format(nuc=nuc, n=n))
+        kaeri_url = 'http://atom.kaeri.re.kr/cgi-bin/nuclide?nuc={0}&n={n}'.format(nuc, n=n)
     print "    getting {0} and placing in {1}".format(nuc, filename)
 
-    if n is None:
-        kaeri = urllib2.urlopen('http://atom.kaeri.re.kr/cgi-bin/nuclide?nuc={0}'.format(nuc))
-    else:
-        kaeri = urllib2.urlopen('http://atom.kaeri.re.kr/cgi-bin/nuclide?nuc={0}&n={n}'.format(nuc, n=n))
-
+    # Get the url 
+    req = urllib2.Request(kaeri_url, headers={'User-Agent': 'Mozilla/5.0'})
+    hdl = urllib2.urlopen(req)
     with open(filename, 'w') as f:
-        f.write(kaeri.read())
-
+        f.write(hdl.read())
 
 nat_iso_regex = re.compile('.*?/cgi-bin/nuclide[?]nuc=([A-Za-z]{1,2}\d{1,3}).*?[(].*?[)]')
 
