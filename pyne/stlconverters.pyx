2ede9d608ece76aa2fb54d9390bfdb1a26cb8a38
Anthony Scopatz <ascopatz@enthought.com>
2011-10-15 16:00:10 -0500

    Added a MapStrDouble proxy class

--- pyne/stlconverters.pyx
+++ pyne/stlconverters.pyx
@@ -916,6 +916,135 @@ class MapIntStr(_MapIntStr, collections.MutableMapping):
 
 
 
+# (Str, Double)
+cdef class MapIterStrDouble(object):
+    cdef void init(self, cpp_map[std.string, double] * map_ptr):
+        cdef cpp_map[std.string, double].iterator * itn = <cpp_map[std.string, double].iterator *> malloc(sizeof(map_ptr.begin()))
+        itn[0] = map_ptr.begin()
+        self.iter_now = itn
+
+        cdef cpp_map[std.string, double].iterator * ite = <cpp_map[std.string, double].iterator *> malloc(sizeof(map_ptr.end()))
+        ite[0] = map_ptr.end()
+        self.iter_end = ite
+        
+    def __dealloc__(self):
+        free(self.iter_now)
+        free(self.iter_end)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        cdef cpp_map[std.string, double].iterator inow = deref(self.iter_now)
+        cdef cpp_map[std.string, double].iterator iend = deref(self.iter_end)
+
+        if inow != iend:
+            pyval = str(deref(inow).first.c_str())
+        else:
+            raise StopIteration    
+
+        inc(deref(self.iter_now))
+        return pyval
+
+
+cdef class _MapStrDouble:
+    def __cinit__(self, new_map=True, bint free_map=True):
+        cdef std.string s
+        cdef pair[std.string, double] item
+
+        # Decide how to init map, if at all
+        if isinstance(new_map, _MapStrDouble):
+            self.map_ptr = (<_MapStrDouble> new_map).map_ptr
+        elif hasattr(new_map, 'items'):
+            self.map_ptr = new cpp_map[std.string, double]()
+            for key, value in new_map.items():
+                s = std.string(key)
+                item = pair[std.string, double](s, value)
+                self.map_ptr.insert(item)
+        elif hasattr(new_map, '__len__'):
+            self.map_ptr = new cpp_map[std.string, double]()
+            for i in new_map:
+                s = std.string(i[0])
+                item = pair[std.string, double](s, i[1])
+                self.map_ptr.insert(item)
+        elif bool(new_map):
+            self.map_ptr = new cpp_map[std.string, double]()
+
+        # Store free_map
+        self._free_map = free_map
+        
+    def __dealloc__(self):
+        if self._free_map:
+            del self.map_ptr
+
+    def __contains__(self, key):
+        cdef std.string s
+        if isinstance(key, str):
+            s = std.string(key)
+        else:
+            return False
+
+        if 0 < self.map_ptr.count(s):
+            return True
+        else:
+            return False
+
+    def __len__(self):
+        return self.map_ptr.size()
+
+    def __iter__(self):
+        cdef MapIterStrDouble mi = MapIterStrDouble()
+        mi.init(self.map_ptr)
+        return mi
+
+    def __getitem__(self, key):
+        cdef std.string s
+        if isinstance(key, basestring):
+            s = std.string(key)
+        else:
+            raise TypeError("Only string keys are valid.")
+
+        if 0 < self.map_ptr.count(s):
+            return deref(self.map_ptr)[s]
+        else:
+            raise KeyError
+
+    def __setitem__(self, char * key, value):
+        cdef std.string s = std.string(key)
+        cdef pair[std.string, double] item = pair[std.string, double](s, value)
+        self.map_ptr.insert(item)
+        
+    def __delitem__(self, char * key):
+        cdef std.string s
+        if key in self:
+            s = std.string(key)
+            self.map_ptr.erase(s)
+
+
+class MapStrDouble(_MapStrDouble, collections.MutableMapping):
+    """Wrapper class for C++ standard library maps of type <string, double>.
+    Provides dictionary like interface on the Python level.
+
+    Parameters
+    ----------
+    new_map : bool or dict-like
+        Boolean on whether to make a new map or not, or dict-like object
+        with keys and values which are castable to the appropriate type.
+    free_map : bool
+        Flag for whether the pointer to the C++ map should be deallocated
+        when the wrapper is dereferenced.
+    """
+    def __str__(self):
+        return self.__repr__()
+
+    def __repr__(self):
+        return "{" + ", ".join(["{0}: {1}".format(repr(key), value) for key, value in self.items()]) + "}"
+
+
+
+
+
+
 
 # (Int, Double)
 cdef class MapIterIntDouble(object):
