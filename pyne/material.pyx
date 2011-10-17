253a49d3d934ae704d5cf68ccfe5b73ea1918b56
Anthony Scopatz <ascopatz@enthought.com>
2011-10-16 22:54:48 -0500

    Added MapStrMaterial wrapper.

--- pyne/material.pyx
+++ pyne/material.pyx
@@ -40,7 +40,7 @@ cdef cpp_map[int, double] dict_to_comp(dict nucvec):
 
 cdef class _Material:
 
-    def __cinit__(self, nucvec=None, double mass=-1.0, char * name='', double atoms_per_mol=-1.0):
+    def __cinit__(self, nucvec=None, double mass=-1.0, char * name='', double atoms_per_mol=-1.0, bint free_mat=True):
         """Material C++ constuctor."""
         cdef cpp_map[int, double] comp
 
@@ -53,20 +53,25 @@ cdef class _Material:
             # Material from file
             self.mat_pointer = new cpp_material.Material(<char *> nucvec, mass, std.string(name), atoms_per_mol)
 
-        elif nucvec is None:
-            # Empty mass stream
-            self.mat_pointer = new cpp_material.Material()
+        elif (nucvec is None): 
+            if free_mat:
+                # Make empty mass stream
+                self.mat_pointer = new cpp_material.Material()
+            else:
+                self.mat_pointer = NULL
 
         else:
             # Bad Material 
             raise TypeError("The mass stream nucvec must be a dict, str, or None.")
 
-        # Init the poperty comp
+        # Init some meta-data
         self._comp = None
+        self._free_mat = free_mat
 
     def __dealloc__(self):
         """Material C++ destructor."""
-        del self.mat_pointer
+        if self._free_mat:
+            del self.mat_pointer
 
 
     #
@@ -973,6 +978,9 @@ class Material(_Material, collections.MutableMapping):
     atoms_per_mol : float, optional
         Number of atoms to per molecule of material.  Needed to obtain proper scaling
         of molecular weights.  For example, this value for water is 3.0.
+    free_mat : bool, optional
+        Flag for whether this wrapper 'owns' this underlying C++ pyne::Material object,
+        and thus determines whether or not to deallocate it on wrapper destruction.
 
     """
     def __str__(self):
@@ -1180,4 +1188,149 @@ cdef dict map_to_dict_str_matp(cpp_map[std.string, matp] cppmap):
         inc(mapiter)
 
     return pydict
+
+
+
+# (Str, Material)
+cdef class MapIterStrMaterial(object):
+    cdef void init(self, cpp_map[std.string, matp] * map_ptr):
+        cdef cpp_map[std.string, matp].iterator * itn = <cpp_map[std.string, matp].iterator *> malloc(sizeof(map_ptr.begin()))
+        itn[0] = map_ptr.begin()
+        self.iter_now = itn
+
+        cdef cpp_map[std.string, matp].iterator * ite = <cpp_map[std.string, matp].iterator *> malloc(sizeof(map_ptr.end()))
+        ite[0] = map_ptr.end()
+        self.iter_end = ite
+        
+    def __dealloc__(self):
+        free(self.iter_now)
+        free(self.iter_end)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        cdef cpp_map[std.string, matp].iterator inow = deref(self.iter_now)
+        cdef cpp_map[std.string, matp].iterator iend = deref(self.iter_end)
+
+        if inow != iend:
+            pyval = str(deref(inow).first.c_str())
+        else:
+            raise StopIteration    
+
+        inc(deref(self.iter_now))
+        return pyval
+
+
+cdef class _MapStrMaterial:
+    def __cinit__(self, new_map=True, bint free_map=True):
+        cdef std.string s
+        cdef cpp_pair[std.string, matp] item
+
+        # Cache needed to prevent Python from deref'ing 
+        # pointers before their time.
+        self._cache = {}
+
+        # Decide how to init map, if at all
+        if isinstance(new_map, _MapStrMaterial):
+            self.map_ptr = (<_MapStrMaterial> new_map).map_ptr
+            self._cache = (<_MapStrMaterial> new_map)._cache
+        elif hasattr(new_map, 'items'):
+            self.map_ptr = new cpp_map[std.string, matp]()
+            for key, value in new_map.items():
+                #s = std.string(key)
+                #item = cpp_pair[std.string, matp](s, (<_Material> value).mat_pointer)
+                #self.map_ptr.insert(item)
+                self[key] = value
+        elif hasattr(new_map, '__len__'):
+            self.map_ptr = new cpp_map[std.string, matp]()
+            for i in new_map:
+                #s = std.string(i[0])
+                #item = cpp_pair[std.string, matp](s, (<_Material> i[1]).mat_pointer)
+                #self.map_ptr.insert(item)
+                self[i[0]] = i[1]
+        elif bool(new_map):
+            self.map_ptr = new cpp_map[std.string, matp]()
+
+        # Store free_map
+        self._free_map = free_map
+        
+    def __dealloc__(self):
+        if self._free_map:
+            del self.map_ptr
+
+    def __contains__(self, key):
+        cdef std.string s
+        if isinstance(key, str):
+            s = std.string(key)
+        else:
+            return False
+
+        if 0 < self.map_ptr.count(s):
+            return True
+        else:
+            return False
+
+    def __len__(self):
+        return self.map_ptr.size()
+
+    def __iter__(self):
+        cdef MapIterStrMaterial mi = MapIterStrMaterial()
+        mi.init(self.map_ptr)
+        return mi
+
+    def __getitem__(self, key):
+        cdef std.string s
+        cdef _Material pymat
+
+        if isinstance(key, basestring):
+            s = std.string(key)
+        else:
+            raise TypeError("Only string keys are valid.")
+
+        if 0 < self.map_ptr.count(s):
+            if key not in self._cache:
+                pymat = Material(nucvec=None, free_mat=False)
+                pymat.mat_pointer = deref(self.map_ptr)[s]
+                self._cache[key] = pymat
+            return self._cache[key]
+        else:
+            raise KeyError(repr(key) + " not in map.")
+
+    def __setitem__(self, char * key, value):
+        cdef std.string s = std.string(key)
+        if not isinstance(value, _Material):
+            raise TypeError("may only set materials into this mapping.")
+        cdef cpp_pair[std.string, matp] item = cpp_pair[std.string, matp](s, (<_Material> value).mat_pointer)
+        self.map_ptr.insert(item)
+        self._cache[key] = value
+        
+    def __delitem__(self, char * key):
+        cdef std.string s
+        if key in self:
+            s = std.string(key)
+            self.map_ptr.erase(s)
+            del self._cache[key]
+
+
+class MapStrMaterial(_MapStrMaterial, collections.MutableMapping):
+    """Wrapper class for C++ standard library maps of type <string, Material *>.
+    Provides dictionary like interface on the Python level.
+
+    Parameters
+    ----------
+    new_map : bool or dict-like
+        Boolean on whether to make a new map or not, or dict-like object
+        with keys and values which are castable to the appropriate type.
+    free_map : bool
+        Flag for whether the pointer to the C++ map should be deallocated
+        when the wrapper is dereferenced.
+    """
+    def __str__(self):
+        return self.__repr__()
+
+    def __repr__(self):
+        return "{" + ", ".join(["{0}: {1}".format(repr(key), value) for key, value in self.items()]) + "}"
+
+
 
