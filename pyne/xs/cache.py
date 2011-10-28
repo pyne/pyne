cd0600cf330d8c470ad1c707b5e4a5cac0436baf
Anthony Scopatz <ascopatz@enthought.com>
2011-10-27 21:43:53 -0500

    Updated cache and models with partial_energy_matrix().

--- pyne/xs/cache.py
+++ pyne/xs/cache.py
@@ -8,6 +8,7 @@ import tables as tb
 
 from pyne import nucname
 from pyne.pyne_config import pyne_conf
+from pyne.xs.models import partial_energy_matrix
 
 
 ###############################################################################
@@ -267,61 +268,6 @@ def get_sigma_reaction_n(nuc, rx):
         sigma_rx_n = rows.sum(axis=0)
 
     return sigma_rx_n
-
-
-
-##############################
-### Partial group collapse ###
-##############################
-
-def partial_energy_matrix(E_g, E_n):
-    """Gerenates a matrix of fractional values that may be used to converts a high-resolution 
-    flux array with group structure E_n to a low-resolution flux array with group-structure E_g.
-
-    Args:
-        * E_n (sequence of floats): higher resolution energy group structure [MeV]
-          that is of length N+1. Ordered from lowest-to-highest energy.
-        * E_g (sequence of floats): lower resolution energy group structure [MeV]
-          that is of length G+1. Ordered from lowest-to-highest energy.
-
-    Returns:
-        * pem (array of fractions): This is a GxN sized matrix that when dotted with a 
-          high-resolution flux produces a low-resolution flux.
-    """
-    # Some convienence paramters
-    G = len(E_g) - 1
-    N = len(E_n) - 1
-
-    index_E_n = np.arange(N+1)
-
-    # Some assertions to ensure that everything is well formed
-    assert E_n[0] <= E_g[0]
-    assert E_g[-1] <= E_n[-1]
-
-    # Get the interior points for each gth group in n-space
-    inner_mask = np.array([(E_g[g] <= E_n) & (E_n <= E_g[g+1]) for g in range(G)])
-
-    # Get the upper and lower nth index for every gth group
-    lower_index = np.array([index_E_n[inner_mask[g]][0] for g in range(G)])
-    upper_index = np.array([index_E_n[inner_mask[g]][-1] for g in range(G)])
-
-    # Convert the mask to initialize the partial enery matrix
-    # Hack off the last index of the mask to make the right size
-    pem = np.array(inner_mask[:, :-1], dtype=float)
-
-    # Check for partial contibutions at the edges
-    for g in range(G):
-        # Lower bound
-        lig = lower_index[g]
-        if lig != 0:
-            pem[g][lig-1] = (E_n[lig] - E_g[g]) / (E_n[lig] - E_n[lig-1])
-
-        # Upper bound
-        uig = upper_index[g]
-        if uig < N:
-            pem[g][uig] = (E_g[g+1] - E_n[uig]) / (E_n[uig+1] - E_n[uig])
-
-    return pem
 
 
 def get_phi_g(E_g, E_n, phi_n):
--- pyne/xs/tests/test_models.py
+++ pyne/xs/tests/test_models.py
@@ -3,10 +3,11 @@ import os
 import numpy as np
 import tables as tb
 
-from nose.tools import assert_equal, assert_not_equal, assert_almost_equal, assert_true
+from nose.tools import assert_equal, assert_not_equal, assert_almost_equal, assert_true, \
+                       assert_raises
 from numpy.testing import assert_array_equal, assert_array_almost_equal
 
-from pyne.xs.models import partial_energy_matrix, partial_energy_matrix_inc
+from pyne.xs.models import partial_energy_matrix, partial_energy_matrix_mono
 from pyne.pyne_config import pyne_conf
 
 nuc_data = pyne_conf.NUC_DATA_PATH
@@ -20,18 +21,18 @@ def test_partial_energy_matrix_inc1():
     E_g = np.array([0.0, 10.0])
     E_n = np.array([0.0, 10.0])
 
-    pem = partial_energy_matrix_inc(E_g, E_n)
+    pem = partial_energy_matrix_mono(E_g, E_n, 1)
 
     expected = np.array([[1.0]])
 
     assert_array_equal(pem, expected)    
 
 
-def test_partial_energy_matrix_inc_inc2():
+def test_partial_energy_matrix_inc2():
     E_g = np.array([0.0, 5.0, 10.0])
     E_n = np.array([0.0, 5.0, 10.0])
 
-    pem = partial_energy_matrix_inc(E_g, E_n)
+    pem = partial_energy_matrix_mono(E_g, E_n, 1)
 
     expected = np.array([[1.0, 0.0], 
                          [0.0, 1.0]])
@@ -43,7 +44,7 @@ def test_partial_energy_matrix_inc3():
     E_g = np.array([1.25, 5.0, 7.5])
     E_n = np.array([0.0, 2.5, 5.0, 7.5, 10.0])
 
-    pem = partial_energy_matrix_inc(E_g, E_n)
+    pem = partial_energy_matrix_mono(E_g, E_n, 1)
 
     expected = np.array([[0.5, 1.0, 0.0, 0.0], 
                          [0.0, 0.0, 1.0, 0.0]])
@@ -55,7 +56,7 @@ def test_partial_energy_matrix_inc4():
     E_g = np.array([0.0, 5.0, 10.0])
     E_n = np.array([0.0, 2.5, 5.0, 7.5, 10.0])
 
-    pem = partial_energy_matrix_inc(E_g, E_n)
+    pem = partial_energy_matrix_mono(E_g, E_n, 1)
 
     expected = np.array([[1.0, 1.0, 0.0, 0.0], 
                          [0.0, 0.0, 1.0, 1.0]])
@@ -67,7 +68,7 @@ def test_partial_energy_matrix_inc5():
     E_g = np.array([0.0, 4.0, 10.0])
     E_n = np.array([0.0, 2.5, 5.0, 7.5, 10.0])
 
-    pem = partial_energy_matrix_inc(E_g, E_n)
+    pem = partial_energy_matrix_mono(E_g, E_n, 1)
 
     expected = np.array([[1.0, 0.6, 0.0, 0.0], 
                          [0.0, 0.4, 1.0, 1.0]])
@@ -79,10 +80,114 @@ def test_partial_energy_matrix_inc6():
     E_g = np.array([0.0, 4.0, 8.0])
     E_n = np.array([0.0, 2.5, 5.0, 7.5, 10.0])
 
-    pem = partial_energy_matrix_inc(E_g, E_n)
+    pem = partial_energy_matrix_mono(E_g, E_n, 1)
 
     expected = np.array([[1.0, 0.6, 0.0, 0.0], 
                          [0.0, 0.4, 1.0, 0.2]])
 
     assert_array_equal(pem, expected)    
+
+
+def test_partial_energy_matrix_dec1():
+    E_g = np.array([10.0, 0.0])
+    E_n = np.array([10.0, 0.0])
+
+    pem = partial_energy_matrix_mono(E_g, E_n, -1)
+
+    expected = np.array([[1.0]])
+
+    assert_array_equal(pem, expected)    
+
+
+def test_partial_energy_matrix_dec2():
+    E_g = np.array([10.0, 5.0, 0.0])
+    E_n = np.array([10.0, 5.0, 0.0])
+
+    pem = partial_energy_matrix_mono(E_g, E_n, -1)
+
+    expected = np.array([[1.0, 0.0], 
+                         [0.0, 1.0]])
+
+    assert_array_equal(pem, expected)    
+
+
+def test_partial_energy_matrix_dec3():
+    E_g = np.array([7.5, 5.0, 1.25])
+    E_n = np.array([10.0, 7.5, 5.0, 2.5, 0.0])
+
+    pem = partial_energy_matrix_mono(E_g, E_n, -1)
+
+    expected = np.array([[0.0, 1.0, 0.0, 0.0], 
+                         [0.0, 0.0, 1.0, 0.5]])
+
+    assert_array_equal(pem, expected)
+
+
+def test_partial_energy_matrix_dec4():
+    E_g = np.array([10.0, 5.0, 0.0])
+    E_n = np.array([10.0, 7.5, 5.0, 2.5, 0.0])
+
+    pem = partial_energy_matrix_mono(E_g, E_n, -1)
+
+    expected = np.array([[1.0, 1.0, 0.0, 0.0], 
+                         [0.0, 0.0, 1.0, 1.0]])
+
+    assert_array_equal(pem, expected)    
+
+
+def test_partial_energy_matrix_dec5():
+    E_g = np.array([10.0, 4.0, 0.0])
+    E_n = np.array([10.0, 7.5, 5.0, 2.5, 0.0])
+
+    pem = partial_energy_matrix_mono(E_g, E_n, -1)
+
+    expected = np.array([[1.0, 1.0, 0.4, 0.0], 
+                         [0.0, 0.0, 0.6, 1.0]])
+
+    assert_array_equal(pem, expected)  
+
+
+def test_partial_energy_matrix_dec6():
+    E_g = np.array([8.0, 4.0, 0.0])
+    E_n = np.array([10.0, 7.5, 5.0, 2.5, 0.0])
+
+    pem = partial_energy_matrix_mono(E_g, E_n, -1)
+
+    expected = np.array([[0.2, 1.0, 0.4, 0.0], 
+                         [0.0, 0.0, 0.6, 1.0]])
+
+    assert_array_equal(pem, expected)    
+
+
+def test_partial_energy_matrix1():
+    # tests dispach to inc
+    E_g = np.array([0.0, 4.0, 8.0])
+    E_n = np.array([0.0, 2.5, 5.0, 7.5, 10.0])
+
+    pem = partial_energy_matrix(E_g, E_n)
+
+    expected = np.array([[1.0, 0.6, 0.0, 0.0], 
+                         [0.0, 0.4, 1.0, 0.2]])
+
+    assert_array_equal(pem, expected)    
+
+
+def test_partial_energy_matrix2():
+    E_g = np.array([8.0, 4.0, 0.0])
+    E_n = np.array([10.0, 7.5, 5.0, 2.5, 0.0])
+
+    pem = partial_energy_matrix(E_g, E_n)
+
+    expected = np.array([[0.2, 1.0, 0.4, 0.0], 
+                         [0.0, 0.0, 0.6, 1.0]])
+
+    assert_array_equal(pem, expected)    
+
+
+def test_partial_energy_matrix3():
+    # tests monotonicity
+    E_g = np.array([0.0, 4.0, 8.0])
+    E_n = np.array([0.0, 2.5, 5.0, 7.5, 10.0][::-1])
+
+    assert_raises(ValueError,  partial_energy_matrix, E_g, E_n)
 
