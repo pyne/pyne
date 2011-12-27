e8cddaea91baee321d6a39d0530bc7634a1084b6
Anthony Scopatz <scopatz@gmail.com>
2011-12-27 15:19:15 -0800

    Added remove function to pyne.utils.

--- pyne/tests/test_utils.py
+++ pyne/tests/test_utils.py
@@ -1,10 +1,10 @@
 """PyNE utility tests"""
 import os
 
-from unittest import TestCase
 import nose 
 
-from nose.tools import assert_equal, assert_not_equal, assert_raises, raises, assert_in
+from nose.tools import assert_equal, assert_not_equal, assert_raises, raises, assert_in, \
+                       assert_true
 
 from pyne import utils
 import numpy as np
@@ -30,6 +30,32 @@ def test_failure():
         assert_equal('\033[1;31mWorld\033[0m', utils.failure('World'))
     else:
         assert_equal('*** FAILURE ***: World', utils.failure('World'))
+
+def test_remove():
+    # test file removal
+    assert_true('hello.txt' not in os.listdir('.'))
+    with open('hello.txt', 'w') as f:
+        f.write("Hello PyNE!")
+    assert_true('hello.txt' in os.listdir('.'))
+
+    utils.remove('hello.txt')
+    assert_true('hello.txt' not in os.listdir('.'))
+
+    # test recursive dir removal
+    assert_true('rawr' not in os.listdir('.'))
+    os.mkdir('rawr')
+
+    assert_true('hello.txt' not in os.listdir('rawr'))
+    with open('rawr/hello.txt', 'w') as f:
+        f.write("Hello PyNE!")
+    assert_true('hello.txt' in os.listdir('rawr'))
+
+    utils.remove('rawr')
+    assert_true('rawr' not in os.listdir('.'))
+
+    # Test pass through on non-existant 
+    assert_true('Medic!' not in os.listdir('.'))
+    utils.remove('Medic!')
 
 
 if __name__ == "__main__":
