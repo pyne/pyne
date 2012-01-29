cfe130893f48d94653020e1709e66772aff908b0
Anthony Scopatz <scopatz@gmail.com>
2012-01-29 17:58:13 -0600

    Added pyne.xs.channels.sigma_s()

--- pyne/xs/tests/test_channels.py
+++ pyne/xs/tests/test_channels.py
@@ -10,7 +10,7 @@ from numpy.testing import assert_array_equal, assert_array_almost_equal
 import pyne.data
 import pyne.xs.models
 from pyne.xs.cache import xs_cache
-from pyne.xs.channels import sigma_f, sigma_s_gh
+from pyne.xs.channels import sigma_f, sigma_s_gh, sigma_s
 from pyne.pyne_config import pyne_conf
 
 
@@ -46,4 +46,11 @@ def test_sigma_s_gh():
     E_centers = (E[1:] + E[:-1]) / 2.0
     expected = np.diag(pyne.xs.models.sigma_s(E_centers, b, aw, 600.0))
     observed = sigma_s_gh('H1', 600.0, E_g=E)
+    assert_array_equal(expected, observed)
+
+
+def test_sigma_s():
+    E_g = np.logspace(-6, 1, 10)[::-1]
+    expected = sigma_s_gh('H1', 600.0, E_g=E_g).sum(axis=1)
+    observed = sigma_s('H1', 600.0, E_g=E_g)
     assert_array_equal(expected, observed)
