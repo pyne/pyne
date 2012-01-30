6db53c2bb387725b2058cfd32fbc923a757ef854
Anthony Scopatz <scopatz@gmail.com>
2012-01-29 18:53:14 -0600

    Added metastable_ratio()

--- pyne/xs/tests/test_channels.py
+++ pyne/xs/tests/test_channels.py
@@ -10,7 +10,8 @@ from numpy.testing import assert_array_equal, assert_array_almost_equal
 import pyne.data
 import pyne.xs.models
 from pyne.xs.cache import xs_cache
-from pyne.xs.channels import sigma_f, sigma_s_gh, sigma_s, sigma_a_reaction
+from pyne.xs.channels import sigma_f, sigma_s_gh, sigma_s, sigma_a_reaction, \
+    metastable_ratio
 from pyne.pyne_config import pyne_conf
 
 
@@ -71,5 +72,26 @@ def test_sigma_a_reaction():
 
     sig_rx = sigma_a_reaction('H1', 'g')
     observed = (0.0 <= sig_rx).all()
+    assert_true(observed)
+
+
+def test_metastable_ratio():
+    # Hide warnings from numpy
+    np.seterr(divide='ignore')
+
+    E_g = np.array([10.0, 7.5, 5.0, 2.5, 0.1])
+    E_n = xs_cache['E_n']
+    phi_n = np.ones(len(E_n) - 1)
+
+    ms_rx = metastable_ratio('U238', '2n', E_g, E_n, phi_n)
+    observed = (0.0 <= ms_rx).all()
+    assert_true(observed)
+
+    ms_rx = metastable_ratio('U238', 'gamma', E_g, E_n, phi_n)
+    observed = (0.0 <= ms_rx).all()
+    assert_true(observed)
+
+    ms_rx = metastable_ratio('H1', 'g')
+    observed = (0.0 <= ms_rx).all()
     assert_true(observed)
 
