4a6cb9a08d0e849ddf96208db919bf4ca31588a7
sjackson <sjackson@3ca0fc55-144d-4cef-8816-fa8a2f6afc42>
2012-03-08 21:03:05 +0000

    Update the _vec3 type to use numpy usefully
    Updates for style and whitespace
    
    git-svn-id: https://repos.cae.wisc.edu/svn/svalinn/python/trunk/pydagmc@724 3ca0fc55-144d-4cef-8816-fa8a2f6afc42

--- __init__.py
+++ __init__.py
@@ -1,37 +1,45 @@
-import ctypes, ctypes.util
+from __future__ import print_function
+
+import ctypes
+import ctypes.util
 import sys
 import os.path
 
 # Attempt to find the dagmc_bridge shared library
 # ctypes.util.find_library is (sometimes) useful, but does not work the
 # same way on all platforms.
-_dirname = os.path.dirname( __file__ )
+_dirname = os.path.dirname(__file__)
 if sys.platform == 'darwin':
-    _bridgepath = ctypes.util.find_library(os.path.join(_dirname,'dagmc_bridge','dagmc_bridge'))
+    _bridgepath = ctypes.util.find_library(
+                    os.path.join(_dirname, 'dagmc_bridge', 'dagmc_bridge'))
 elif sys.platform.startswith('linux'):
-    _bridgepath = os.path.join(_dirname,'dagmc_bridge','dagmc_bridge.so')
+    _bridgepath = os.path.join(_dirname, 'dagmc_bridge', 'dagmc_bridge.so')
 else:
-    print(sys.stderr, 'Warning: unknown platform, lib import may fail')
+    print('Warning: unknown platform, dagmc_bridge lib import may fail',
+          file=sys.stderr)
     _bridgepath = ctypes.util.find_library('dagmc_bridge')
 
+
 daglib = ctypes.CDLL(_bridgepath)
+
 
 # Set the entity handle type; I don't know a cleaner way than to ask the daglib
 # to return the byte width of the type
 def get_EH_type():
     EH_size = daglib.dag_ent_handle_size()
-    if( EH_size == 8):
+    if EH_size == 8:
         EH_t = ctypes.c_uint64
-    elif( EH_size == 4):
+    elif EH_size == 4:
         EH_t = ctypes.c_uint32
     else:
-        raise TypeError("Unrecognized entity handle size in dagmc library: " + int(EH_size) )
-    return type("EntityHandle",(EH_t,),{})
+        raise TypeError("Unrecognized entity handle size in dagmc library: "
+                         + str(EH_size))
+    return type("EntityHandle", (EH_t,), {})
+
 
 EntityHandle = get_EH_type()
-_ErrorCode   = type( "ErrorCode", (ctypes.c_int,),{})
+_ErrorCode = type("ErrorCode", (ctypes.c_int,), {})
 
-class DagmcError( Exception ):
-    
+
+class DagmcError(Exception):
     pass
-
--- bridge.py
+++ bridge.py
@@ -1,10 +1,9 @@
 import ctypes
 from contextlib import contextmanager
-from numpy import ndarray, float64
-from numpy.ctypeslib import as_ctypes, ndpointer
+from numpy import float64
+from numpy.ctypeslib import ndpointer
 
 from pydagmc import daglib, EntityHandle, _ErrorCode, DagmcError
-
 
 lib = daglib
 
@@ -14,78 +13,68 @@ lib.dag_version.argtypes = []
 lib.dag_rev_version.restype = ctypes.c_uint
 lib.dag_rev_version.argtypes = []
 
-def _geom_dim_check( result, func, arguments ):
-    if arguments[0] not in (2,3): raise DagmcError('Incorrect geometric dimension: '+str(arguments[0]))
+
+def _geom_dim_check(result, func, arguments):
+    if arguments[0] not in (2, 3):
+        raise DagmcError('Incorrect geometric dimension: ' + str(arguments[0]))
     return result
 
-lib.geom_id_list.restype = ctypes.POINTER( ctypes.c_int )
-lib.geom_id_list.argtypes = [ ctypes.c_int, ctypes.POINTER( ctypes.c_int ) ]
+lib.geom_id_list.restype = ctypes.POINTER(ctypes.c_int)
+lib.geom_id_list.argtypes = [ctypes.c_int, ctypes.POINTER(ctypes.c_int)]
 lib.geom_id_list.errcheck = _geom_dim_check
 
 lib.handle_from_id.restype = EntityHandle
-lib.handle_from_id.argtypes = [ ctypes.c_int, ctypes.c_int ]
+lib.handle_from_id.argtypes = [ctypes.c_int, ctypes.c_int]
 lib.handle_from_id.errcheck = _geom_dim_check
 
 lib.id_from_handle.restype = ctypes.c_int
-lib.id_from_handle.argtypes = [ EntityHandle ]
+lib.id_from_handle.argtypes = [EntityHandle]
 
-def _moab_error_check( result, func, arguments ):
+
+def _moab_error_check(result, func, arguments):
     if result.value != 0:
-        raise DagmcError( "Error code " + str(result.value) + " returned from " + func.__name__ )
+        raise DagmcError("Error code " + str(result.value) +
+                         " returned from " + func.__name__)
     return result
 
-def _returns_moab_errors( function ):
+
+def _returns_moab_errors(function):
     function.restype = _ErrorCode
     function.errcheck = _moab_error_check
 
-_returns_moab_errors( lib.dag_load )
-lib.dag_load.argtypes = [ ctypes.c_char_p ]
+_returns_moab_errors(lib.dag_load)
+lib.dag_load.argtypes = [ctypes.c_char_p]
 
-#lib.dag_ray_fire
+_vec3 = ndpointer(dtype=float64, shape=(3,), flags='CONTIGUOUS')
 
-
-
-class listPOINTER:
-    def __init__(self, etype, length):
-        self.etype = etype
-        self.length = length
-
-    def from_param(self, param):
-        if isinstance( param, ndarray ) and len(param) == self.length:
-            return as_ctypes( param )
-        elif isinstance( param, (list,tuple) ) and len(param) == self.length:
-            return (self.etype * len(param))(*param)
-        else:
-            raise DagmcError( "Problem with listPOINTER" )
-
-
-#_vec3 = listPOINTER( ctypes.c_double, 3 )
-_vec3 = ndpointer( dtype=float64, shape=(3,), flags='CONTIGUOUS' )
-
-_returns_moab_errors( lib.dag_pt_in_vol )
-lib.dag_pt_in_vol.argtypes = [ EntityHandle, _vec3, ctypes.POINTER(ctypes.c_int),
-                               _vec3, ctypes.c_void_p ]
+_returns_moab_errors(lib.dag_pt_in_vol)
+lib.dag_pt_in_vol.argtypes = [EntityHandle, _vec3,
+                              ctypes.POINTER(ctypes.c_int),
+                              _vec3, ctypes.c_void_p]
 
 lib.dag_alloc_ray_history.restype = ctypes.c_void_p
 lib.dag_alloc_ray_history.argtypes = []
 
 lib.dag_dealloc_ray_history.restype = None
-lib.dag_dealloc_ray_history.argtypes = [ ctypes.c_void_p ]
+lib.dag_dealloc_ray_history.argtypes = [ctypes.c_void_p]
+
 
 @contextmanager
 def _ray_history():
     history = lib.dag_alloc_ray_history()
     yield history
-    lib.dag_dealloc_ray_history( history )
+    lib.dag_dealloc_ray_history(history)
 
-_returns_moab_errors( lib.dag_ray_fire )
-lib.dag_ray_fire.argtypes = [ EntityHandle, _vec3, _vec3,
-                              ctypes.POINTER( EntityHandle ), ctypes.POINTER( ctypes.c_double ), 
-                              ctypes.c_void_p, ctypes.c_double ]
+_returns_moab_errors(lib.dag_ray_fire)
+lib.dag_ray_fire.argtypes = [EntityHandle, _vec3, _vec3,
+                             ctypes.POINTER(EntityHandle),
+                             ctypes.POINTER(ctypes.c_double),
+                             ctypes.c_void_p, ctypes.c_double]
 
 
-_returns_moab_errors( lib.dag_next_vol )
-lib.dag_next_vol.argtypes = [ EntityHandle, EntityHandle, ctypes.POINTER( EntityHandle ) ]
+_returns_moab_errors(lib.dag_next_vol)
+lib.dag_next_vol.argtypes = [EntityHandle, EntityHandle,
+                             ctypes.POINTER(EntityHandle)]
 
 lib.vol_is_graveyard.restype = ctypes.c_int
 lib.vol_is_graveyard.argtypes = [EntityHandle]
@@ -93,9 +82,10 @@ lib.vol_is_graveyard.argtypes = [EntityHandle]
 lib.vol_is_implicit_complement.restype = ctypes.c_int
 lib.vol_is_implicit_complement.argtypes = [EntityHandle]
 
-_returns_moab_errors( lib.get_volume_metadata )
-lib.get_volume_metadata.argtypes = [ EntityHandle, ctypes.POINTER( ctypes.c_int ),
-                                     ctypes.POINTER( ctypes.c_double ), ctypes.POINTER( ctypes.c_double ) ]
+_returns_moab_errors(lib.get_volume_metadata)
+lib.get_volume_metadata.argtypes = [EntityHandle, ctypes.POINTER(ctypes.c_int),
+                                    ctypes.POINTER(ctypes.c_double),
+                                    ctypes.POINTER(ctypes.c_double)]
 
-_returns_moab_errors( lib.get_volume_boundary )
-lib.get_volume_boundary.argtypes = [ EntityHandle, _vec3, _vec3 ]
+_returns_moab_errors(lib.get_volume_boundary)
+lib.get_volume_boundary.argtypes = [EntityHandle, _vec3, _vec3]
