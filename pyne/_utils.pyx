a823412dcbd9dafa5f88874d5f5cc96eacee97d8
Anthony Scopatz <scopatz@gmail.com>
2012-05-15 00:16:22 -0500

    Updated fromstring functions.

--- pyne/_utils.pyx
+++ pyne/_utils.pyx
@@ -7,8 +7,38 @@ import numpy as np
 from std cimport atof, strtok, strcpy
 
 
-def fastfromstring(char * s, char * sep=" ", bint inplace=False):
-    """A quick replacement for numpy.fromstring() using the C standard
+def fromstring_split(char * s, sep=None, dtype=float):
+    """A replacement for numpy.fromstring() using the Python str.split() 
+    and np.array().
+
+    Parameters
+    ----------
+    s : str
+        String of data.
+    sep : str or None
+        String of separator characters, has the same meaning as in 
+        str.split().
+    dtype : np.dtype
+        Numpy dtype to cast elements enough.
+
+    Returns
+    -------
+    data : ndarry, 1d
+        Will always return a 1d array of dtype.  You must reshape to the 
+        appropriate shape.
+
+    See Also
+    --------
+    fromstring_token : May faster depending on the data. 
+
+    """
+    cdef list rawdata
+    rawdata = s.split(sep)
+    return np.array(rawdata, dtype=dtype)
+
+
+def fromstring_token(char * s, char * sep=" ", bint inplace=False, int maxsize=-1):
+    """A replacement for numpy.fromstring() using the C standard
     library atof() and strtok() functions.
 
     Parameters
@@ -22,11 +52,21 @@ def fastfromstring(char * s, char * sep=" ", bint inplace=False):
         Whether s should tokenized in-place or whether a copy should 
         be made.  If done in-place, the first instance of sep between 
         any tokens will replaced with the NULL character.
+    maxsize : int
+        Specifies the size of the array to pre-allocate.  If negative,
+        this will be set to the maximum possible number of elements, 
+        ie len(s)/2 + 1.
 
-    Notes
-    -----
-    Will always return a 1d float64 array.  You must cast and reshape 
-    to the appropriate type and shape.
+    Returns
+    -------
+    data : ndarry, 1d, float64
+        Will always return a 1d float64 array.  You must cast and reshape 
+        to the appropriate type and shape.
+
+    See Also
+    --------
+    fromstring_split : May faster depending on the data.
+
     """
     cdef char* cstring
     cdef char* cs
@@ -43,8 +83,10 @@ def fastfromstring(char * s, char * sep=" ", bint inplace=False):
         cs = <char *> malloc(I * sizeof(char))
         strcpy(cs, s)
 
-    I = (I / 2) + 1
-    data = np.empty(I, dtype=np.float64)
+    if maxsize < 0:
+        maxsize = (I / 2) + 1
+
+    data = np.empty(maxsize, dtype=np.float64)
     cdata = data
 
     i = 0
@@ -59,5 +101,3 @@ def fastfromstring(char * s, char * sep=" ", bint inplace=False):
 
     data = data[:i].copy()
     return data
-
-
