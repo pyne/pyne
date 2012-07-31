e936817716dd2421e712e91c4570c77830259e3f
Christopher Dembia <cld72@cornell.edu>
2012-07-31 00:35:15 -0400

    Finished putting in docstrings, about to remove stlconverters from build.

--- examples/mcnp.py
+++ examples/mcnp.py
@@ -3,6 +3,8 @@ enriched UO2 fuel pins surrounded by water.
 
 """
 
+# The first example creates an infinite lattice of an enriched UO2 pin
+# surrounded by a water moderaor.
 # Create the mcnp.Inp object
 
 # C
