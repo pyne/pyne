10c84b2f98111ee25a144eb575974fdfd1af011c
Katy Huff <katyhuff@gmail.com>
2012-08-10 11:51:41 -0500

    the data library builds and installs with cmake. Other pure cpp libs to follow.

--- cmake/PyneMacros.cmake
+++ cmake/PyneMacros.cmake
@@ -1,15 +1,13 @@
-macro add_lib_to_pyne( _name _source )
+macro( add_lib_to_pyne _name _source )
 # add the library
-add_library(${_name} ${_source})
-
+add_library(${_name}             ${_source})
 # add it to the list of pyne libraries
 set(PYNE_LIBRARIES ${PYNE_LIBRARIES} ${_name})
 endmacro()
 
-macro install_lib( _name )
+macro( install_lib _name )
 # install it and add it to cpack
 install(TARGETS ${_name}
   LIBRARY DESTINATION ${CMAKE_INSTALL_PREFIX}/lib
-  component libs
   )
 endmacro()
