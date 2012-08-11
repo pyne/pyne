0c9b4bf7bb00e623d9649061c59842d856f052a0
Christopher Dembia <cld72@cornell.edu>
2012-08-11 15:27:11 -0400

    Good progress on prototyping simulation.

--- pyne/simulation/inputfile.py
+++ pyne/simulation/inputfile.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 
-"""This module employs the modules `reactordef` and `material` to generate
+"""The ``inputfile`` module can be imported as such::
+
+    from pyne.simulation import inputfile
+
+Below is the reference for this module.
+
+
+
+This module employs the modules `reactordef` and `material` to generate
 plaintext input files for a general code. Support is provided for MCNPX, and
 support for Serpent is not complete but should be straightforward. The
 extension to other codes may require more effort.
@@ -12,11 +20,21 @@ extension to other codes may require more effort.
 import abc
 
 class InputFile(object):
-    """Create a more general name that encompasses reading an input file.
+    """Abstract base class for classes that take system and option definitions
+    to create an input file for a certain code (e.g. MCNPX, Serpent, MCODE,
+    etc.).
+
     """
     __metaclass__ = abc.ABCMeta
 
-    def __init__(self):
+    def __init__(self, systemdef, optionsdef):
+        """
+
+        Parameters
+        ----------
+        reactordef : pyne.ReactorDefinition
+
+        """
         pass
 
     @abc.abstractmethod
@@ -29,25 +47,19 @@ class InputFile(object):
         """Returns an infinite cylinder surface card."""
         return
 
-class JSONInput(InputFile):
-    """TODO ideally we can exchange file formats through something like this
-    intermediate."""
+#class JSONInput(InputFile):
+#    """TODO ideally we can exchange file formats through something like this
+#    intermediate."""
 
 class MCNPInput(InputFile):
     """Contains a write method for each type of surface.
     """
 
-    def __init__(self, reactordef, simulationdef):
-        """
-
-        Parameters
-        ----------
-        reactordef : pyne.ReactorDefinition
-
-        """
+    def __init__(self, systemdef, optionsdef):
+        pass
 
     def _cell(self, cell):
-        """Returns a cell card string."""
+        """Returns a cell card string given a Cell card."""
         return
 
     def _infinite_cylinder_surface(self, surface):
@@ -55,14 +67,12 @@ class MCNPInput(InputFile):
         return
 
 
-
-
 class SerpentInput(object):
     """Must find the cell used for a given material, and would need to create
     more than one material if necessary.
 
     """
-    def __init__(self, reactordef, simulationdef):
+    def __init__(self, systemdef, optionsdef):
         pass
 
 
