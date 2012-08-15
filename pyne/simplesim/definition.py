7eeb3d6c85932c7cefc0c54e5686f8d453a3ad3c
Christopher Dembia <cld72@cornell.edu>
2012-08-15 17:28:06 -0400

    Started work on SystemDefinition, realizing issues with Card class.

--- pyne/simplesim/definition.py
+++ pyne/simplesim/definition.py
@@ -35,9 +35,29 @@ class IDefinition(object):
         """Save object data to a JSON file."""
         return
 
+    def _unique(self, card_type, name):
+        """Checks that the name on a card has not already been used for another
+        card.
+
+        """
+        if card_type == "cell":
+            dict_to_check = self.cells
+        elif card_type == "surface":
+            dict_to_check = self.surfaces
+        elif card_type == "material":
+            dict_to_check = self.materials
+        elif card_type == "tally":
+            dict_to_check = self.tallies
+        else:
+            raise ValueError("The input ``card_type`` must be either "
+                    "'cell', 'surface', or 'material'.")
+        if name in dict_to_check:
+            raise Exception("The %s name %s has already been used for "
+                    "another %s" % (card_type, name, card_type))
+
 
 class SystemDefinition(IDefinition):
-    """This class creates a reactor definition as is done in MCNPX: homogeneous
+    """This class creates a system definition as is done in MCNPX: homogeneous
     regions in space in the reactor, called cells, are defined through the
     intersection, union, etc of surfaces and are filled by materials. The
     definition of materials is done using the `material` module of PyNE.
@@ -57,18 +77,32 @@ class SystemDefinition(IDefinition):
         self.materials = collections.OrderedDict()
         self.cells = collections.OrderedDict()
 
+    def add_cell(self, cell):
+        """
+
+        """
+        self._unique("cell", name)
+        # Check for the material.
+        if cell.material.name not in self.materials:
+            raise ValueError("Material %s is not found in this system
+            definition." % cell.material.name)
+        # Check for all required cells.
+        # Okay, all checks passed.
+        self.cell[cell.name] = cell
+
+    def add_material(self, material):
+        if material.name == None or material.name == '':
+            raise ValueError("The ``name`` property of the material cannot "
+                    "be empty.")
+        self._unique("material", name)
+        self.materials[material.name] = material
+
     def save(self, fname):
         """Saves definition to a JSON file. It is unlikely that the class will
         be amenable to json.dump()."""
         return
 
     def _open(self, fname):
-        return
-
-    def add_cell(self):
-        return
-
-    def add_cylinder(self):
         return
 
 
@@ -83,19 +117,11 @@ class SimulationDefinition(IDefinition):
 
     def __init__(self, systemdef, fname=None):
         """Creates a new options definition or loads one from a JSON file."""
-        self.sys(systemdef)
+        self.sys = systemdef
         if fname is not None:
             self._open(fname)
         else:
             self._create_new()
-
-    def _create_new(self):
-        """Initialize any attributes/properties."""
-
-    def save(self, fname):
-        """Saves definition to a JSON file. It is unlikely that the class will
-        be amenable to json.dump()."""
-        return
 
     def _open(self, fname):
         return
@@ -109,6 +135,14 @@ class SimulationDefinition(IDefinition):
     def add_tally(self, card):
         return
 
+    def _create_new(self):
+        """Initialize any attributes/properties."""
+
+    def save(self, fname):
+        """Saves definition to a JSON file. It is unlikely that the class will
+        be amenable to json.dump()."""
+        return
+
     @property
     def sys(self):
         return self._sys
@@ -116,3 +150,6 @@ class SimulationDefinition(IDefinition):
     @sys.setter
     def sys(self, value):
         self._sys = value
+
+class MCNPSimulation(SimulationDefinition):
+    pass
