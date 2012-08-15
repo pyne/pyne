865ddb1b267d735c3bc2accd436985f90b130221
Christopher Dembia <cld72@cornell.edu>
2012-08-15 14:55:46 -0400

    Uncommented the autosummary sphinx extension.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ sys.path.insert(0, os.path.abspath('../sphinxext'))
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.pngmath', 
-              'sphinx.ext.inheritance_diagram', 'numpydoc', #'sphinx.ext.autosummary',
+              'sphinx.ext.inheritance_diagram', 'numpydoc', 'sphinx.ext.autosummary',
               'ipython_console_highlighting', 'sphinx.ext.viewcode',
               ]
 
