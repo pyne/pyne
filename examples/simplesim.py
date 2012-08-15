374dfc2710fcf144e2c637140be0a7a9097b795a
Christopher Dembia <cld72@cornell.edu>
2012-08-15 00:05:28 -0400

    Removing CellSimple cards.

--- examples/simplesim.py
+++ examples/simplesim.py
@@ -37,8 +37,8 @@ rxr.add_surface(cellbound)
 rxr.add_cell(fuel)
 rxr.add_cell(coolant)
 
-#
-opts = simulationdef.OptionsDefinition(rxr)
+# 
+opts = simulationdef.SimulationDefinition(rxr)
 
 opts.add_card(cards.CriticalitySource(1000, 1, 1, 1))
 opts.add_card(cards.CriticalityPoints([[0, 0, 0]]))
@@ -58,6 +58,77 @@ for this_enrich in enrichments:
     inp.options.card[crit_source_name].cycles = 1000
     inp.write()
     
+
+
+
+
+"""
+
+"""
+# Create cards.
+channel = cards.AxisCylinder("channel", 'X', 2.54)
+leftbound = cards.Plane("leftbound", 'X', -500.0)
+rightbound = cards.Plane("rightbound", 'X', 500.0)
+polycyl = cards.AxisCylinder("polycyl", 'X', 17.54)
+tungstencyl = cards.AxisCylinder("tungstencyl", 'X', 27.54)
+coppercyl = cards.AxisCylinder("coppercyl", 'X', 28.04)
+shieldleft = cards.Plane("shieldleft", 'X', -25.0)
+shieldright = cards.Plane("shieldright", 'X', 25.0)
+aperturecyl = cards.AxisCylinder("aperturecyl", 'Z', 0.25)
+half = cards.Plane("half", 'Z', 0.0)
+gravecyl = cards.AxisCylinder("gravecyl", 'X', 33.04)
+
+# Make regions.
+pipemid = leftbound.pos & rightbound.neg & channel.neg
+polyshield = shieldleft.pos & shieldright.neg & 
+        channel.pos & polycyl.neg & 
+        aperturecyl.pos 
+tungstenshield = shieldleft.pos & shieldright.neg &
+        polycyl.pos & tungstencyl.neg &
+        aperturecyl.pos
+coppershield = shieldleft.pos & shieldright.neg &
+        tungstencyl.pos & coppercyl.neg &
+        aperturecyl.pos
+aperture = aperturecyl.neg & channel.pos & coppercyl.neg
+usefulvoid = (channel.pos & gravecyl.neg & 
+              leftbound.pos & shieldleft.neg)
+             |
+             (channel.pos & gravecyl.neg &
+              shieldright.pos & rightbound.neg)
+             |
+             (tungstencyl.pos & gravecyl.neg &
+              shieldleft.pos & shieldright.neg)
+uselessvoid = leftbound.neg | rightbound.pos |
+              (leftbound.pos & leftbound.neg &
+               gravecyl.pos)
+
+
+rxr.add_lattice
+
+rxr.add_universe
+cards.CellbyUniverse
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
