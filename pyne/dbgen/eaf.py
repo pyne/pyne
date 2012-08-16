5d249d7bb603d252a7f5caaaaeba70bb13a73583
Eric Relson <gertlex@gmail.com>
2012-08-16 11:59:29 -0500

    Updates to eaf.py; nuc_data_make functionality should be good to go.

--- pyne/dbgen/eaf.py
+++ pyne/dbgen/eaf.py
@@ -50,26 +50,33 @@ eaf_dtype = np.dtype([
 # Regular expression for parsing an individual set of EAF data
 eaf_info_pattern = \
     "(?P<iso>\d{5,7})\s*(?P<rxnum>\d{2,4})\s*(?P<ngrps>\d{1,3})" \
-    + "\s*(?P<parent>[a-zA-Z]{1,2}\s{0,3}\d{1,3})\s*" \
-    + "(?P<rxstr>\(N,[\w\s]{3}\))(?P<daugh>[a-zA-Z]{1,2}\s{0,3}\d{1,3})"
-eaf_bin_pattern = "(\d\.\d{5}E[-+]\d{2}\s*)"
+    + "\s*(?P<parent>[a-zA-Z]{1,2}\s{0,3}\d{1,3}[M ][12 ])" \
+    + "(?P<rxstr>\(N,[\w\s]{3}\))(?P<daugh>[a-zA-Z.]{1,2}\s{0,3}\d{1,3})"
+eaf_bin_pattern = "(.*?)(?P<xsec>(\d\.\d{5}E[-+]\d{2}\s*){1,175})"
 
 def parse_eaf_xsec(build_dir):
     """
     """
     
     #TODO: change eaf_file to something more universal
-    eaf_file = "/filespace/groups/cnerg/opt/FENDL2.0-A/fendlg-2.0_175"):
+    eaf_file = "/filespace/groups/cnerg/opt/FENDL2.0-A/fendlg-2.0_175"
 
     with open(eaf_file, 'r') as f:
         raw_data = f.read()
 
     eaf_data = list()
-    eaf_pattern = eaf_info_pattern + eaf_bin_pattern + "{1,75}"
+    eaf_pattern = eaf_info_pattern + eaf_bin_pattern 
+
+#    eaf_pattern = \
+#    "(?P<iso>\d{5,7})\s*(?P<rxnum>\d{2,4})\s*(?P<ngrps>\d{1,3})" \
+#    + "\s*(?P<parent>[a-zA-Z]{1,2}\s{0,3}\d{1,3}[M ][12 ])" \
+#    + "(?P<rxstr>\(N,[\w\s]{3}\))(?P<daugh>[a-zA-Z.]{1,2}\s{0,3}\d{1,3})" \
+#    + "(.*?)(?P<xsec>(\d\.\d{5}E[-+]\d{2}\s*){1,175})"
+
     # Iterate over all iso/rx combinations in file
-    for m in re.finditer(eaf_pattern, rawdata):
+    for m in re.finditer(eaf_pattern, raw_data, re.DOTALL):
         md = m.groupdict()
-    
+
         xsec_list = [float(x) for x in md['xsec'].split()]
         xsec_list += (175-len(xsec_list))*[0.0]
 
@@ -79,12 +86,13 @@ def parse_eaf_xsec(build_dir):
                   md['rxstr'],
                   md['daugh'],
                   xsec_list
-                  
                   )
         
         eaf_data.append(eafrow)
 
-    eaf_array = np.array(eaf_data, dtypes=eaf_dtype)
+    eaf_array = np.array(eaf_data, dtype=eaf_dtype)
+
+    print "Read in {0} sets of EAF data.".format(len(eaf_array))
 
     return eaf_array
 
@@ -98,20 +106,18 @@ def make_eaf_table(nuc_data, build_dir=""):
     db = tb.openFile(nuc_data, 'a', filters=BASIC_FILTERS)
 
     # Ensure that the appropriate file structure is present
-    if not hasattr(db.root, 'neutron')
+    if not hasattr(db.root, 'neutron'):
         neutron_group = db.createGroup('/', 'neutron', \
                 'Neutron Interaction Data')
 
     # Create xs group
     if not hasattr(db.root.neutron, 'eaf_xs'):
-    eaf_group = db.createGroup("/neutron", "eaf_xs", \
+        eaf_group = db.createGroup("/neutron", "eaf_xs", \
             "EAF 175-Group Neutron Activation Cross Section Data")
 
-
-    # Create xs table
-    eaf_table = db.createTable("/neutron/eaf_xs",
-            np.empty(0, dtypes=eaf_dtype),
-            "EAF Activation Cross Section Data [barns]",
+    eaf_table = db.createTable("/neutron/eaf_xs", "eaf_xs", \
+            np.empty(0, dtype=eaf_dtype), \
+            "EAF Activation Cross Section Data [barns]", \
             expectedrows=len(eaf_array))
 
     # Put eaf_array in the table
@@ -130,7 +136,7 @@ def make_eaf(args):
 
     # Check if the table already exists
     with tb.openFile(nuc_data, 'a', filters=BASIC_FILTERS) as f:
-        if hasattr(f.root, 'neutron') and hasttr(f.root.neutron, 'eaf_xs'):
+        if hasattr(f.root, 'neutron') and hasattr(f.root.neutron, 'eaf_xs'):
             return
 
     #
