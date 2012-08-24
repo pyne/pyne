7586489f67d816b04b000927df337cd5d9e3440a
Anthony Scopatz <scopatz@gmail.com>
2012-08-23 20:18:22 -0500

    Added a bunch of type checking functions.

--- pyne/tests/test_jsoncpp.py
+++ pyne/tests/test_jsoncpp.py
@@ -20,57 +20,88 @@ def test_strvalue():
     v = Value(s)
     assert_equal(len(v), 42)
     assert_equal(str(v), s)
+    assert_true(v.isstring())
+    assert_equal(v.type(), 4)
+    assert_equal(v.type_name(), 'string')
     
 def test_fltvalue():
     d = 42.0
     v = Value(d)
     assert_equal(float(v), d)
     assert_equal(int(v), 42)
+    assert_true(v.isfloat())
+    assert_equal(v.type(), 3)
+    assert_equal(v.type_name(), 'real')
 
 def test_intvalue():
     i = 42
     v = Value(i)
     assert_equal(float(v), 42.0)
     assert_equal(int(v), i)
+    assert_true(v.isint())
+    assert_equal(v.type(), 1)
+    assert_equal(v.type_name(), 'int')
     
 def test_truevalue():
     b = True
     v = Value(b)
     assert_true(v)
+    assert_true(v.isbool())
+    assert_equal(v.type(), 5)
+    assert_equal(v.type_name(), 'boolean')
 
 def test_falsevalue():
     b = False
     v = Value(b)
     assert_false(v)
+    assert_true(v.isbool())
+    assert_equal(v.type(), 5)
+    assert_equal(v.type_name(), 'boolean')
     
 def test_nonevalue():
     n = None
     v = Value(n)
     assert_true(v.isnull())
+    assert_true(v.isarray())
+    assert_true(v.isobject())
+    assert_equal(v.type(), 0)
+    assert_equal(v.type_name(), 'null')
 
 def test_arrvalue():
     a = [1, 2, 5, 3]
     v = Value(a)
     assert_equal(len(v), len(a))
     assert_equal([v[i] for i in range(len(a))], a)
+    assert_true(v.isarray())
+    assert_equal(v.type(), 6)
+    assert_equal(v.type_name(), 'array')
 
 def test_tuplevalue():
     a = (1, 2, 5, 3)
     v = Value(a)
     assert_equal(len(v), len(a))
     assert_equal(tuple([v[i] for i in range(len(a))]), a)
+    assert_true(v.isarray())
+    assert_equal(v.type(), 6)
+    assert_equal(v.type_name(), 'array')
 
 def test_setvalue():
     a = set([1, 2, 5, 3])
     v = Value(a)
     assert_equal(len(v), len(a))
     assert_equal(set([v[i] for i in range(len(a))]), a)
+    assert_true(v.isarray())
+    assert_equal(v.type(), 6)
+    assert_equal(v.type_name(), 'array')
 
 def test_mapvalue():
     m = {'name': 'Terry Jones', 'age': 42.0}
     v = Value(m)
     assert_equal(len(v), len(m))
     assert_equal(dict([(k, v[k]) for k in m.keys()]), m)
+    assert_true(v.isobject())
+    assert_equal(v.type(), 7)
+    assert_equal(v.type_name(), 'object')
 
 def test_badmapvalue():
     m = {'name': 'Terry Jones', 42: 42.0}
