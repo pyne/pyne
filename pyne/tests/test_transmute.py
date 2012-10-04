3c1d7256a2026e7c094a0f7a18a6d8427aab25dd
Matthew Klebenow <klebenowm@gmail.com>
2012-10-04 16:54:39 -0500

    Added test to check the scipy.linalg.expm execution

--- pyne/tests/test_transmute.py
+++ pyne/tests/test_transmute.py
@@ -9,13 +9,33 @@ from nose.tools import assert_equal, assert_not_equal, assert_raises, raises, \
 import os
 from pyne.material import Material
 import numpy  as np
+import scipy  as sp
 import tables as tb
-
 from pyne import transmute
+from scipy import linalg
 
 
-def test_decay1():
+"""def test_decay1():
     mat = Material({'C14': 1.0, 'N14': 0.0, 'C13': 0.0})
     obs = transmute.decay(mat, 1.0)
 
     assert False
+"""
+
+"""Tests _solve_decay_matrix calculating the matrix exponential"""
+def test_expm():
+    A = np.zeros((3,3))
+    np.fill_diagonal(A,[-1,-2,-3])
+    transA = transmute._solve_decay_matrix(A)
+    eA = linalg.expm(A)
+    tayA = linalg.expm3(A,q=20)
+    # Check the equivalence of _solve_decay_matrix
+    assert_equal(eA.all(),transA.all())
+    # Check convergence to high level Taylor Series solution
+    assert_almost_equal(tayA.all(),transA.all())
+
+#
+# Run as script
+#
+if __name__ == "__main__":
+    nose.main()
