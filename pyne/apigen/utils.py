4e96b7946b059531f60cdf116d91b5b33fc9bb61
Anthony Scopatz <scopatz@gmail.com>
2012-10-20 17:28:30 -0500

    fixed some bugs in cse_to_c()

--- pyne/apigen/utils.py
+++ pyne/apigen/utils.py
@@ -13,6 +13,8 @@ def cse_to_c(replacements, reduced_exprs, indent=2):
     rtn_pattern = re.compile('\s*?return (.*)')
     equ_pattern = re.compile('\s*?(\w+)\s*?=\s*?(\w.*)')
     repline_template = '{ind}double {name} = {expr}\n'
+    redline_template = '{ind}{name} = {expr}\n'
+    redrtnline_template = '{ind}return {expr}\n'
 
     for repsym, repexpr in replacements:
         repname = str(repsym)
@@ -26,11 +28,15 @@ def cse_to_c(replacements, reduced_exprs, indent=2):
         ccode += genline
 
     for redexpr in reduced_exprs:
-        gencode = codegen((repname, repexpr), "C", repname, header=False, empty=False)
+        gencode = codegen(("redname", redexpr), "C", "temp", header=False, empty=False)
         genexpr = gencode[0][1]
-        if repexpr.is_Equality:
-            genexpr = equ_pattern.search(genexpr).group(0)
+        if redexpr.is_Equality:
+            m = equ_pattern.search(genexpr)
+            genname = m.group(1)
+            genexpr = m.group(2)
+            genline = redline_template.format(ind=ws, name=genname, expr=genexpr)
         else:
-            genexpr = rtn_pattern.search(genexpr).group(0)
-        ccode += genexpr + "\n"
+            genexpr = rtn_pattern.search(genexpr).group(1)
+            genline = redrtnline_template.format(ind=ws, expr=genexpr)
+        ccode += genline
     return ccode
