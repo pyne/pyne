92b9a03e2a5e542daeb9547f28490706ac542bf6
Anthony Scopatz <scopatz@gmail.com>
2012-10-22 23:24:22 -0500

    updated to sym enr 10 and 15

--- pyne/apigen/main.py
+++ pyne/apigen/main.py
@@ -4,9 +4,9 @@ import enrich_multi_sym
 
 def main():
     debug = False
-    enrich_multi_sym.cgen_file(filename="cpp/enrichment_symbolic05", 
+    enrich_multi_sym.cgen_file(filename="cpp/enrichment_symbolic15", 
                                header_filename="cpp/enrichment_symbolic.h",
-                               max_ncomp=5, 
+                               max_ncomp=15, 
                                debug=debug)
 
 if __name__ == '__main__':
