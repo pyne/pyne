b4b53bab5519835b2a0017eb0ee78ef2821e6ebf
Matthew Klebenow <klebenowm@gmail.com>
2012-11-06 10:08:05 -0600

    Added implementation of get_daughters to transmute.py

--- pyne/tests/test_transmute.py
+++ pyne/tests/test_transmute.py
@@ -9,13 +9,38 @@ from nose.tools import assert_equal, assert_not_equal, assert_raises, raises, \
 import os
 from pyne.material import Material
 import numpy  as np
+import scipy  as sp
 import tables as tb
-
 from pyne import transmute
+from pyne.transmute import import_eaf_data
+from scipy import linalg
 
 
-def test_decay1():
+"""def test_decay1():
     mat = Material({'C14': 1.0, 'N14': 0.0, 'C13': 0.0})
     obs = transmute.decay(mat, 1.0)
 
     assert False
+"""
+
+"""Tests _solve_decay_matrix calculating the matrix exponential"""
+def test_expm():
+    A = np.zeros((3,3))
+    np.fill_diagonal(A,[-1,-2,-3])
+    transA = transmute._solve_decay_matrix(A)
+    eA = linalg.expm(A)
+    tayA = linalg.expm3(A,q=20)
+    # Check the equivalence of _solve_decay_matrix
+    assert_equal(eA.all(),transA.all())
+    # Check convergence to high level Taylor Series solution
+    assert_almost_equal(tayA.all(),transA.all())
+
+def test_import_eaf():
+    eaf_array = import_eaf_data
+    print(eaf_array)
+
+#
+# Run as script
+#
+if __name__ == "__main__":
+    nose.main()
