8290b84ca5887ebe9f08ea41f0d6e4d320d70aa6
Anthony Scopatz <scopatz@gmail.com>
2012-11-17 22:06:12 -0800

    stlconv seem to be working mostly ok

--- pyne/apigen/main.py
+++ pyne/apigen/main.py
@@ -1,13 +1,42 @@
 import argparse
 
-import enrich_multi_sym
-
 def main():
-    debug = False
-    enrich_multi_sym.cgen_file(filename="cpp/enrichment_symbolic15", 
-                               header_filename="cpp/enrichment_symbolic.h",
-                               max_ncomp=15, 
-                               debug=debug)
+    parser = argparse.ArgumentParser("Generates PyNE API")
+    parser.add_argument('--debug', action='store_true', default=False)
+    parser.add_argument('--enrich', action='store_true', default=False)
+    parser.add_argument('--stlwrap', action='store_true', default=False)
+    ns = parser.parse_args()
+
+    if ns.enrich:
+        print "generating symbolic enrichment cascades"
+        import enrich_multi_sym
+        enrich_multi_sym.cgen_file(filename="cpp/enrichment_symbolic15", 
+                                   header_filename="cpp/enrichment_symbolic.h",
+                                   max_ncomp=15, 
+                                   debug=ns.debug)
+    if ns.stlwrap:
+        print "generating C++ standard library wrappers & converters"
+        import stlwrap
+        template = [('py2c_set', 'int'),
+                    ('py2c_set', 'str'),
+                    ('py2c_map', 'int', 'int'),
+                    ('py2c_map', 'int', 'double'),
+                    ('py2c_map', 'str', 'int'),
+                    ('py2c_map', 'int', 'str'),
+                    ('py2c_map', 'str', 'double'),
+                    ('set', 'int'),
+                    ('set', 'str'),
+                    ('map', 'str', 'str'),
+                    ('map', 'str', 'int'),
+                    ('map', 'int', 'str'),
+                    ('map', 'str', 'double'),
+                    ('map', 'int', 'int'),
+                    ('map', 'int', 'double'),
+                    ('map', 'int', 'complex'),
+                    ]
+        stlwrap.genfiles(template, 
+                         fname='pyne/stlconverters', 
+                         testname="pyne/tests/test_stlconverters")
 
 if __name__ == '__main__':
     main()
