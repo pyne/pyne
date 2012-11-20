df05758f3c3c4cc462916cfb7d87c1901b76068e
Anthony Scopatz <scopatz@gmail.com>
2012-11-20 11:08:26 -0800

    works with cython v0.14

--- pyne/material.pyx
+++ pyne/material.pyx
@@ -1,7 +1,6 @@
 """Python wrapper for material library."""
 # Cython imports
 from libcpp.utility cimport pair as cpp_pair
-from libcpp.map cimport map as cpp_map
 from libcpp.set cimport set as cpp_set
 #from cython.operator cimport reference as ref
 from cython.operator cimport dereference as deref
@@ -15,8 +14,10 @@ import collections
 include "include/cython_version.pxi"
 IF CYTHON_VERSION_MAJOR == 0 and CYTHON_VERSION_MINOR >= 17:
     from libcpp.string cimport string as std_string
+    from libcpp.map cimport map as cpp_map
 ELSE:
     from pyne._includes.libcpp.string cimport string as std_string
+    from pyne._includes.libcpp.map cimport map as cpp_map
 cimport cpp_material
 cimport pyne.stlconverters as conv
 import pyne.stlconverters as conv
