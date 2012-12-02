cc66432e2de4c68c993cb28ef41bcb7b52f3a867
Anthony Scopatz <scopatz@gmail.com>
2012-12-02 17:14:23 -0600

    more doc updates.

--- cpp/nucname.h
+++ cpp/nucname.h
@@ -19,87 +19,110 @@ namespace pyne
 {
 namespace nucname 
 {
-  /*** Constructs the name to zz Dictionary ***/
-  typedef std::string name_t;
-  typedef int zz_t;
+  typedef std::string name_t; ///< name type
+  typedef int zz_t;           ///< Z number type
 
-  typedef std::map<name_t, zz_t> name_zz_t; 
-  typedef name_zz_t::iterator name_zz_iter; 
-  name_zz_t get_name_zz();
-  extern name_zz_t name_zz; 
+  typedef std::map<name_t, zz_t> name_zz_t; ///< name and Z num map type
+  typedef name_zz_t::iterator name_zz_iter; ///< name and Z num iter type
+  name_zz_t get_name_zz();  ///< Creates standard name to Z number mapping.
+  extern name_zz_t name_zz; ///< name to Z num map
 
-  /*** Constructs zz to name dictionary **/
-  typedef std::map<zz_t, name_t> zzname_t; 
-  typedef zzname_t::iterator zzname_iter; 
-  zzname_t get_zz_name();
-  extern zzname_t zz_name;
+  typedef std::map<zz_t, name_t> zzname_t;  ///< Z num to name map type
+  typedef zzname_t::iterator zzname_iter;   ///< Z num to name iter type
+  zzname_t get_zz_name();   ///< Creates standard Z number to name mapping.
+  extern zzname_t zz_name;  ///< Z num to name map
 
   /******************************************/
   /*** Define useful elemental group sets ***/
   /******************************************/
-  typedef std::set<name_t> name_group;
-  typedef name_group::iterator name_group_iter;
 
+  /// name grouping type (for testing containment)
+  typedef std::set<name_t> name_group;  
+  typedef name_group::iterator name_group_iter; ///< name grouping iter type
+
+  /// Z number grouping type (for testing containment)
   typedef std::set<zz_t> zz_group;
-  typedef zz_group::iterator zz_group_iter;
+  typedef zz_group::iterator zz_group_iter; ///< Z number grouping iter
 
-  zz_group name_to_zz_group (name_group);
+  /// Converts a name group to a Z number group.
+  /// \param eg a grouping of nuclides by name
+  /// \return a Z numbered group
+  zz_group name_to_zz_group (name_group eg);
 
-  extern name_t LAN_array[15];
-  extern name_group LAN;
-  extern zz_group lan;
+  extern name_t LAN_array[15];  ///< array of lanthanide names
+  extern name_group LAN;        ///< lanthanide name group
+  extern zz_group lan;          ///< lanthanide Z number group
 
-  extern name_t ACT_array[15];
-  extern name_group ACT;
-  extern zz_group act;
+  extern name_t ACT_array[15];  ///< array of actinide names
+  extern name_group ACT;        ///< actinide name group
+  extern zz_group act;          ///< actinide Z number group
 
-  extern name_t TRU_array[22];
-  extern name_group TRU;
-  extern zz_group tru;
+  extern name_t TRU_array[22];  ///< array of transuranic names
+  extern name_group TRU;        ///< transuranic name group
+  extern zz_group tru;          ///< transuranic Z number group
 
-  extern name_t MA_array[10];
-  extern name_group MA;
-  extern zz_group ma;
+  extern name_t MA_array[10];   ///< array of minor actinide names
+  extern name_group MA;         ///< minor actinide name group
+  extern zz_group ma;           ///< minor actinide Z number group
 
-  extern name_t FP_array[88];
-  extern name_group FP;
-  extern zz_group fp;
+  extern name_t FP_array[88];   ///< array of fission product names
+  extern name_group FP;         ///< fission product name group
+  extern zz_group fp;           ///< fission product Z number group
 
 
   /******************/
   /*** Exceptions ***/
   /******************/
 
+  /// Custom expection for declaring that a value does not follow a recognizable 
+  /// nuclide naming convention.
   class NotANuclide : public std::exception
   {
   public:
-    NotANuclide () {};
+    /// default constructor
+    NotANuclide () {}; 
+
+    /// default destructor
     ~NotANuclide () throw () {};
 
+    /// Constructor given previous and current state of nulide name
+    /// \param wasptr Previous state, typically user input.
+    /// \param nowptr Current state, as far as PyNE could get.
     NotANuclide(std::string wasptr, std::string nowptr)
     {
        nucwas = wasptr;
        nucnow = nowptr;
     };
 
+    /// Constructor given previous and current state of nulide name
+    /// \param wasptr Previous state, typically user input.
+    /// \param nowptr Current state, as far as PyNE could get.
     NotANuclide(std::string wasptr, int nowptr)
     {
       nucwas = wasptr;
       nucnow = pyne::to_str(nowptr);
     };
 
+    /// Constructor given previous and current state of nulide name
+    /// \param wasptr Previous state, typically user input.
+    /// \param nowptr Current state, as far as PyNE could get.
     NotANuclide(int wasptr, std::string nowptr)
     {
       nucwas = pyne::to_str(wasptr);
       nucnow = nowptr;
     };
 
+    /// Constructor given previous and current state of nulide name
+    /// \param wasptr Previous state, typically user input.
+    /// \param nowptr Current state, as far as PyNE could get.
     NotANuclide(int wasptr, int nowptr)
     {
       nucwas = pyne::to_str(wasptr);
       nucnow = pyne::to_str(nowptr);
     };
 
+    /// Generates an informational message for the exception
+    /// \return The error string
     virtual const char* what() const throw()
     {
       std::string NaNEstr ("Not a Nuclide! ");
@@ -115,42 +138,61 @@ namespace nucname
     };
 
   private:
-    std::string nucwas;
-    std::string nucnow;
+    std::string nucwas; ///< previous nuclide state
+    std::string nucnow; ///< current nuclide state  
   };
 
 
 
+  /// Custom expection for declaring that a value represents one or more nuclides 
+  /// in one or more namig conventions
   class IndeterminateNuclideForm : public std::exception
   {
   public:
+    /// default constructor
     IndeterminateNuclideForm () {};
+
+    /// default destuctor
     ~IndeterminateNuclideForm () throw () {};
 
+    /// Constructor given previous and current state of nulide name
+    /// \param wasptr Previous state, typically user input.
+    /// \param nowptr Current state, as far as PyNE could get.
     IndeterminateNuclideForm(std::string wasptr, std::string nowptr)
     {
        nucwas = wasptr;
        nucnow = nowptr;
     };
 
+    /// Constructor given previous and current state of nulide name
+    /// \param wasptr Previous state, typically user input.
+    /// \param nowptr Current state, as far as PyNE could get.
     IndeterminateNuclideForm(std::string wasptr, int nowptr)
     {
       nucwas = wasptr;
       nucnow = pyne::to_str(nowptr);
     };
 
+    /// Constructor given previous and current state of nulide name
+    /// \param wasptr Previous state, typically user input.
+    /// \param nowptr Current state, as far as PyNE could get.
     IndeterminateNuclideForm(int wasptr, std::string nowptr)
     {
       nucwas = pyne::to_str(wasptr);
       nucnow = nowptr;
     };
 
+    /// Constructor given previous and current state of nulide name
+    /// \param wasptr Previous state, typically user input.
+    /// \param nowptr Current state, as far as PyNE could get.
     IndeterminateNuclideForm(int wasptr, int nowptr)
     {
       nucwas = pyne::to_str(wasptr);
       nucnow = pyne::to_str(nowptr);
     };
 
+    /// Generates an informational message for the exception
+    /// \return The error string
     virtual const char* what() const throw()
     {
       std::string INFEstr ("Indeterminate nuclide form: ");
@@ -166,24 +208,33 @@ namespace nucname
     }
 
   private:
-    std::string nucwas;
-    std::string nucnow;
+    std::string nucwas; ///< previous nuclide state
+    std::string nucnow; ///< current nuclide state
   };
 
 
-  /********************/
-  /*** Current Form ***/
-  /********************/
-  std::string current_form(std::string);
-  std::string current_form(int);
+  /// \name Current Form Functions
+  /// \{
+  /// These functions compute the current form of the nuclide.
+  /// \param nuc a nuclide
+  /// \return a (string) flag of the nuclides current form.
+  std::string current_form(std::string nuc);
+  std::string current_form(int nuc);
+  /// \}
 
-
-  /************************/
-  /*** zzaaam functions ***/
-  /************************/
-  int zzaaam(int);
-  int zzaaam(char *);
-  int zzaaam(std::string);
+  /// \name ZZAAAM Form Functions
+  /// \{
+  /// The ZZAAAM nuclide naming convention is the canonical form for representing
+  /// nuclides in PyNE. This places the charge of the nucleus out front, then has 
+  /// three digits for the atomic mass number, and ends with a metastable flag 
+  /// (0 = ground, 1 = first excited state, 2 = second excited state, etc). 
+  /// Uranium-235 here would be expressed as ‘922350’.
+  /// \param nuc a nuclide
+  /// \return an integer nuclide identifier.
+  int zzaaam(int nuc);
+  int zzaaam(char * nuc);
+  int zzaaam(std::string nuc);
+  /// \}
 
 
   /************************/
