347524b75e8d050ee463b8000ed197f3d5eff1a6
Eric Relson <gertlex@gmail.com>
2012-12-04 15:13:48 -0600

    Revert poorly thought-out previous commit. (but fixed a few comments)

--- pyne/xs/data_source.py
+++ pyne/xs/data_source.py
@@ -525,9 +525,9 @@ class EAFDataSource(DataSource):
         ----------
         nuc : int
             Nuclide in zzaaam form.
-        rx : str 
+        rx : int or str 
             Reaction MT # in nnnm form.
-            OR: (eventually)
+            OR:
             Reaction key: 'gamma', 'alpha', 'p', etc.
 
         See Also
@@ -552,16 +552,13 @@ class EAFDataSource(DataSource):
         # Check if usable rx #
         if rx is None:
             return None
-        if str(rx) not in EAF_RX and 0 != rx
+        if str(rx) not in EAF_RX:
             msg = "the reaction '{rx}' is not valid.".format(rx=rx)
             raise IndexError(msg)
 
         # Grab data
         with tb.openFile(nuc_data, 'r') as f:
-            if rx == 0:
-                cond = "(nuc_zz == {0})".format(nuc)
-            else:
-                cond = "(nuc_zz == {0}) & (rxnum == '{1}')".format(nuc, rx)
+            cond = "(nuc_zz == {0}) & (rxnum == '{1}')".format(nuc, rx)
             node = f.root.neutron.eaf_xs.eaf_xs
             rows = [np.array(row['xs']) for row in node.where(cond)]
 
