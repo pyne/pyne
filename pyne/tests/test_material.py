ed30b3fd607018272c2bec97e4c3d2274d6e9c7e
Elliott Biondo <elliott.biondo@gmail.com>
2013-01-09 17:22:59 -0600

    this commit covers most of issue #78

--- pyne/tests/test_material.py
+++ pyne/tests/test_material.py
@@ -7,7 +7,8 @@ from nose.tools import assert_equal, assert_not_equal, assert_raises, raises, \
     assert_almost_equal, assert_true, assert_false, assert_in
 
 import os
-from pyne.material import Material, from_atom_frac, from_hdf5, from_text, MapStrMaterial, MultiMaterial
+from pyne.material import Material, from_atom_frac, from_hdf5, from_text, MapStrMaterial, MultiMaterial,\
+    read_mcnp_inp
 from pyne import jsoncpp 
 from pyne import data
 import numpy  as np
@@ -928,21 +929,25 @@ def test_write_mcnp_mass_fracs():
     leu = Material( nucvec={'U235': 0.04, 'U238': 0.96}, attrs={\
           'mat_number':2, 'table_ids':{'922350':'15c', '922380':'25c'},\
           'mat_name':'LEU', 'source':'Some URL', \
-          'comments': 'comments can go here'}, density = 19.1 )
+          'comments': \
+'this is a long comment that will definitly go over the 80 character limit, for science', \
+          'name':'leu'}, density = 19.1 )
 
     leu.write_mcnp_mass_fracs('mcnp_mass_fracs.txt')
 
     with open('mcnp_mass_fracs.txt') as f:
         written = f.read()
-    expected = (an'C density = 19.1\n'
+    expected = ('C name: leu\n'
+                'C density = 19.1\n'
                 'C source: Some URL\n'
-                'C comments:\n'
-                'C comments can go here\n'
+                'C comments: this is a long comment that will definitly go over the 80 character\n'
+                'C  limit, for science\n'
                 'm2\n'
                 '     92235.15c -4.0000E-02\n'
                 '     92238.25c -9.6000E-01\n')
     assert_equal(written, expected)
     os.remove('mcnp_mass_fracs.txt')
+
 
 def test_write_alara():
     if 'alara.txt' in os.listdir('.'):
@@ -951,64 +956,84 @@ def test_write_alara():
     leu = Material( nucvec={'U235': 0.04, 'U238': 0.96}, attrs={\
           'mat_number':2, 'table_ids':{'922350':'15c', '922380':'25c'},\
           'mat_name':'LEU', 'source':'Some URL', \
-          'comments': 'comments can go here'}, density = 19.1 )
+          'comments': \
+'this is a long comment that will definitly go over the 80 character limit, for science', \
+          'name':'leu'}, density = 19.1 )
 
     leu.write_alara('alara.txt')
 
     with open('alara.txt') as f:
         written = f.read()
-    expected = ('# source: Some URL\n'
-                '# comments:\n'
-                '# comments can go here\n'
+    expected = ('# name: leu\n'
+                '# source: Some URL\n'
+                '# comments: this is a long comment that will definitly go over the 80 character\n'
+                '#  limit, for science\n'
                 'm2 19.1 2\n'
                 '     92235 4.0000E-02 92\n'
                 '     92238 9.6000E-01 92\n')
     assert_equal(written, expected)
     os.remove('alara.txt')
-'''
+
+
 def test_read_mcnp() :
-    expected = [Material( nucvec={'U235': 0.04, 'U238': 0.96}, attrs={\
-          'mat_number':2, 'table_ids':{'922350':'15c', '922380':'25c'},\
-          'mat_name':'LEU', 'source':'Some URL', \
-          'comments': 'comments can go here'}, density = 19.1 ),
-    assert_equal(expected, read)
-'''
 
-    
+    expected_material = Material(nucvec={922350: 0.04, 922380: 0.96}, mass=-1.0, 
+    density=19.1, attrs={"comments":" first line of comments second line of \
+comments third line of comments forth line of comments","mat_number":"1",\
+    "name":" leu", "source":" Some http://URL.com",\
+    "table_ids":{"922350":"15c","922380":"25c"}})
+    expected_material.mass = -1.0 # to avoid reassignment to +1.0
 
+    expected_multimaterial =MultiMaterial({Material(\
+    {10000: 0.11190248274452597, 80000: 0.888097517255474}, -1.0, 0.9, 3, \
+    {"comments":" Here are comments the comments continue here are more \
+even more","mat_number":"2","name":" water","source":" internet",\
+    "table_ids":{"10000":"05c","80000":"06c"}}): 1,\
+     Material({10000: 0.11190248274452597, 80000: 0.888097517255474}, \
+    -1.0, 1.002158184090557, 3, {"comments":" Here are comments the comments \
+continue here are more even more","mat_number":"2","name":" water",\
+     "source":" internet","table_ids":{"10000":"05c","80000":"06c"}}): 1})
 
+    read_materials = read_mcnp_inp('mcnp_inp.txt')
 
+    assert_equal(expected_material, read_materials[0])
 
-'''
-def test_write_text():
-    if 'test_mat.txt' in os.listdir('.'):
-        os.remove('leu.txt')
+    assert_equal(expected_multimaterial._mats.keys()[0].comp,\
+                                        read_materials[1]._mats.keys()[0].comp)
+    assert_equal(expected_multimaterial._mats.keys()[0].mass,\
+                                        read_materials[1]._mats.keys()[0].mass)
+    assert_equal(expected_multimaterial._mats.keys()[0].density,\
+                                      read_materials[1]._mats.keys()[0].density)
+    assert_equal(expected_multimaterial._mats.keys()[0].atoms_per_mol,\
+                                read_materials[1]._mats.keys()[0].atoms_per_mol)
+    assert_equal(expected_multimaterial._mats.keys()[0].attrs,\
+                                      read_materials[1]._mats.keys()[0].attrs)
 
-    leu = Material({'U235': 0.04, 'U238': 0.96}, 42.0, 1.0, 1.0)
-    leu.write_text('leu.txt')
+    assert_equal(expected_multimaterial._mats.keys()[1].comp,\
+                                        read_materials[1]._mats.keys()[1].comp)
+    assert_equal(expected_multimaterial._mats.keys()[1].mass,\
+                                        read_materials[1]._mats.keys()[1].mass)
+    assert_equal(expected_multimaterial._mats.keys()[1].density,\
+                                      read_materials[1]._mats.keys()[1].density)
+    assert_equal(expected_multimaterial._mats.keys()[1].atoms_per_mol,\
+                                read_materials[1]._mats.keys()[1].atoms_per_mol)
+    assert_equal(expected_multimaterial._mats.keys()[1].attrs,\
+                                      read_materials[1]._mats.keys()[1].attrs)
 
-    with open('leu.txt') as f:
-        written = f.read()
-    expected = ("Mass    42\n"
-                "Density 1\n"
-                "APerM   1\n"
-                "U235    0.04\n"
-                "U238    0.96\n")
-    assert_equal(written, expected)
+def test_natural_elements() :
+    water = Material()
+    water.from_atom_frac({10000: 2.0, 80000: 1.0})
+    expected_comp = {10000: 0.11190248274452597, 80000: 0.888097517255474}
+    assert_equal(water.comp, expected_comp)
 
-    read_leu = from_text('leu.txt')
-    assert_equal(leu.mass, read_leu.mass)
-    assert_equal(leu.atoms_per_mol, read_leu.atoms_per_mol)
-    assert_equal(leu.comp, read_leu.comp)
+def test_mass_density_from_atom_density() :
+    ethanol = Material()
+    ethanol.from_atom_frac({'C':2, 'H':6, 'O':1})
+    atom_density_ethanol = 9.282542841E22 # atom density not molecule density
+    mass_density = ethanol.mass_density_from_atom_density(atom_density_ethanol)
+    expected_mass_density = 0.7890206250055379
+    assert_equal(mass_density, expected_mass_density)
 
-    os.remove('leu.txt')
-'''
-
-
-
-
-
-#
 # Run as script
 #
 if __name__ == "__main__":
