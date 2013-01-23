10241249a8340ade3d4b14e8bb00d22b5ec47bda
John Xia <john.danger.xia@gmail.com>
2013-01-23 14:39:58 -0600

    Fixed style errors and cleaned up.

--- pyne/tests/test_endf.py
+++ pyne/tests/test_endf.py
@@ -1,16 +1,22 @@
-import numpy as np
-from pyne.endf import Library
-from pyne.rx_data import double_spin_dict
-import nose 
-from nose.tools import assert_equal, assert_not_equal, assert_raises, raises, assert_in
 import warnings
+
+import numpy as np
+from numpy.testing import assert_array_equal # , assert_array_almost_equal
+
+from pyne.endf import Library
+from pyne.rxdata import DoubleSpinDict
+
+import nose 
+from nose.tools import assert_equal # , assert_not_equal, assert_raises, raises, assert_in
 
 
 library = Library('endftest_small.txt')   
 
+
 def test_mats():
     for mat_id in library.mats:
         assert_in(mat_id, [128, 131, 419])
+
 
 def test_get():
     obs = library.get(419, 4, 2)
@@ -20,8 +26,9 @@ def test_get():
                     2.123212e+6, 8.231231e-6,
                     -2.231211e+6, 8.123421e-6])    
     badkey = library.get(111, 1, 1)
-    assert(np.array_equal(exp, obs))
+    assert_array_equal(exp, obs)
     assert_equal(badkey, False)
+
 
 def test_unresolved_resonances_a():
     # Case A (ENDF Manual p.70)
@@ -44,11 +51,12 @@ def test_unresolved_resonances_a():
     new_obs_GNO = new_obs['GNO']
     new_exp_GNO = 7.088000e-1
 
-    assert(np.array_equal(exp_D, obs_D))
-    assert(np.array_equal(exp_GNO, obs_GNO))
+    assert_array_equal(exp_D, obs_D)
+    assert_array_equal(exp_GNO, obs_GNO)
 
-    assert(np.array_equal(new_exp_D, new_obs_D))
-    assert(np.array_equal(new_exp_GNO, new_obs_GNO))
+    assert_array_equal(new_exp_D, new_obs_D)
+    assert_array_equal(new_exp_GNO, new_obs_GNO)
+
 
 def test_unresolved_resonances_b():
     # Case B (ENDF Manual p. 70)
@@ -61,8 +69,9 @@ def test_unresolved_resonances_b():
     exp_419_GF = np.array([2.000000e3, 2.100000e3, 2.200000e3, 2.300000e3, 2.400000e3, 2.500000e3,
                         2.600000e3, 2.700000e3, 2.800000e3, 2.900000e3, 3.000000e3])
     
-    assert(np.array_equal(exp_ES, obs_ES))
-    assert(np.array_equal(exp_419_GF, obs_419_GF))
+    assert_array_equal(exp_ES, obs_ES)
+    assert_array_equal(exp_419_GF, obs_419_GF)
+
 
 def test_unresolved_resonances_c():
     # Case C (ENDF Manual p. 70)
@@ -75,18 +84,18 @@ def test_unresolved_resonances_c():
     obs_D = obs['D']
     exp_D = np.array([7.762320e3, 6.766400e3, 2.780300e3])
 
-    assert(np.array_equal(exp_ES, obs_ES))
-    assert(np.array_equal(exp_D, obs_D))
+    assert_array_equal(exp_ES, obs_ES)
+    assert_array_equal(exp_D, obs_D)
+
     
-def test_double_spin_dict():
-    subject = double_spin_dict({(3.499999999998, 2, 1):{'A':'a', 'B':'b'},
-                                (2.000000000012, 3, 4):{'C':'c', 'D':'d'}})
+def test_DoubleSpinDict():
+    subject = DoubleSpinDict({(3.499999999998, 2, 1):{'A':'a', 'B':'b'},
+                              (2.000000000012, 3, 4):{'C':'c', 'D':'d'}})
     subject.update({(3.500000000011,8,9):{'E':'e', 'F':'f'}})
     
     obs = subject[(3.48, 8, 9)]
     exp = {'E':'e', 'F':'f'}
     assert_equal(exp, obs)
-
 
 
 if __name__ == "__main__":
