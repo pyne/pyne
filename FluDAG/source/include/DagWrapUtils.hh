0105e2435495eaff047a46abb693676bd24ab409
Julie C. Zachman <zachman@engr.wisc.edu>
2013-02-05 17:07:34 -0600

    Add function declarations; update comments; add convenience inlines for writing to ostream

--- FluDAG/source/include/DagWrapUtils.hh
+++ FluDAG/source/include/DagWrapUtils.hh
@@ -14,10 +14,13 @@
 
 //Forward declarations
 
-// Common print utilities used in FGeometryInit.cc
+// Common print utilities used in /*FGeometryInit.cc*/ creatRegionAndMatFiles.cpp
 inline std::ostream& setw10(std::ostream& os) { return os << std::setw(10);}
 inline std::ostream& setscientific(std::ostream& os) { return os << std::setiosflags(std::ios::scientific);}
 inline std::ostream& setfixed(std::ostream& os) { return os << std::setiosflags(std::ios::fixed);}
 std::ostream& PrintHeader(std::ostream& os, const char* title);
+
+void PrintEntityRegionNames(std::ostream& os);
+void writeRegionLine(std::ostream& os, int iRegion, std::string name);
 
 #endif
