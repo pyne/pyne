2a97b3d87c320c3f823afac3542aca70a7758539
Julie C. Zachman <zachman@engr.wisc.edu>
2013-02-22 14:28:07 -0600

    Remove cruft; set flukareg=numVol

--- FluDAG/source/cpp/WrapInit.cc
+++ FluDAG/source/cpp/WrapInit.cc
@@ -7,89 +7,29 @@
 //
 //////////////////////////////////////////////////////////////////
 
-/*
-#include <stdlib.h>
-#include <stdio.h>
-#include <fstream>
-*/
 
 #include "DagWrapUtils.hh"
 #include "DagWrappers.hh"
 
 using namespace moab;
-#define DAG DagMC::instance()
 
 void jomiwr(int & nge, const int& lin, const int& lou, int& flukaReg)
 {
-//flag
-#ifdef DAGGEOMETRY_DEBUG
   std::cout << "================== JOMIWR =================" << std::endl;
-#endif 
   // return FLUGG code to fluka
   nge = 3;
 
-  //Geoinit Pointer
-#ifdef DAGGEOMETRY_DEBUG
-  std::cout << "\t *==> JOMIWR: Setting DAGMC::instance()..." << std::endl;
-#endif 
-//  FGeometryInit * ptrGeoInit=FGeometryInit::GetInstance();
-
+ // std::cout << "\t *==> JOMIWR: Setting DAGMC::instance()..." << std::endl;
   
-  //initialize geometry:construct detector and set world volume
-#ifdef DAGGEOMETRY_DEBUG
-  std::cout << "\t *==> JOMIWR: Setting the detector..." << std::endl;
-#endif 
-//  ptrGeoInit->setDetector();
-#ifdef DAGGEOMETRY_DEBUG
-  std::cout << "\t *==> JOMIWR: Setting mother volume..." << std::endl;
-#endif 
-//  ptrGeoInit->setMotherVolume(); 
-  
-  //close geometry for optimization
-#ifdef DAGGEOMETRY_DEBUG
-//  std::cout << "\t *==> JOMIWR: Closing geometry..." << std::endl;
-#endif 
-//  ptrGeoInit->closeGeometry();
-  
-  //initialize wrappers utility histories at the beginning of run and set flag
-#ifdef DAGGEOMETRY_DEBUG
-//  std::cout << "\t *==> JOMIWR: InitHistories..." << std::endl;
-#endif 
-//  ptrGeoInit->InitHistories();
-  
-  //initialize lattice array
-#ifdef DAGGEOMETRY_DEBUG
-//  std::cout << "\t *==> JOMIWR: Init lattice array..." << std::endl;
-#endif 
-//  ptrGeoInit->InitJrLtGeantArray();
-  
-  //initialize debug-array
-#ifdef DAGGEOMETRY_DEBUG
-//  std::cout << "\t *==> JOMIWR: Init debug array..." << std::endl;
-#endif 
-//  ptrGeoInit->InitHistArray();
   
   //create Fluka material cards in flukaMat.inp file
-#ifdef DAGGEOMETRY_DEBUG
-  std::cout << "\t *==> JOMIWR: Init fluka materials...moved to main jcz" << std::endl;
-#endif 
-//  ptrGeoInit->createFlukaMatFile();
-//    createFlukaMatFile();
+  // std::cout << "\t *==> JOMIWR: Init fluka materials...moved to main jcz" << std::endl;
+  //createFlukaMatFile();
   
-  //returns number of volumes + 1
-#ifdef DAGGEOMETRY_DEBUG
-  std::cout << "\t *==> JOMIWR: Returning..." << std::endl;
-#endif 
-  // G4PhysicalVolumeStore * pVolStore = G4PhysicalVolumeStore::GetInstance();
-  // int numVol = int(pVolStore->size());
+  //Original comment:  returns number of volumes + 1
   unsigned int numVol = DAG->num_entities(3);
-//   flukaReg = numVol + 1;
+  flukaReg = numVol;
 	
-#ifdef DAGGEOMETRY_DEBUG
-  std::cout << "Number of volumes + 1: " << flukaReg << std::endl;
+  std::cout << "Number of volumes: " << flukaReg << std::endl;
   std::cout << "================== Out of JOMIWR =================" << std::endl;
-#endif
 }
-
-
-
