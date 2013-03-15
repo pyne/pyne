bb68b5e662e66456a6c1e6ed37669596d73c0a86
Julie C. Zachman <zachman@engr.wisc.edu>
2013-03-15 08:17:57 -0500

    Initial placement.

---
+++ FluDAG/src/include/dagmc_utils.hpp
@@ -0,0 +1,29 @@
+#ifndef DAGMC_UTILS_H
+#define DAGMC_UTILS_H
+
+#include <iostream>
+#include <moab/Interface.hpp>
+using namespace moab;
+namespace moab
+{
+    class DagMC;
+}
+
+/**
+ * Generic halt-on-error error checking
+ */
+void chkerr( Interface* mbi, ErrorCode code, int line, const char* file );
+void chkerr( Interface& mbi, ErrorCode code, int line, const char* file );
+void chkerr( DagMC& dag, ErrorCode code, int line, const char* file );
+
+#define CHECKERR(M,C) chkerr(M,C,__LINE__,__FILE__)
+
+// features provided by obb_analysis.cpp
+
+// ErrorCode obbvis_create( DagMC& dag, std::vector<int> &volumes, int grid, std::string& filename );
+// ErrorCode obbstat_write( DagMC& dag, std::vector<int> &volumes,
+//                          std::vector<std::string> &properties, std::ostream& out );
+
+
+#endif /* DAGMC_UTILS_H */
+
