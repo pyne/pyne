1462140e3d70597752816db0dd7aebcbfdd1ca67
Julie C. Zachman <zachman@engr.wisc.edu>
2013-05-05 15:06:16 -0500

    Added documentation

--- FluDAG/src/cpp/DagWrappers.hh
+++ FluDAG/src/cpp/DagWrappers.hh
@@ -27,87 +27,104 @@
 #define isvhwr isvhwr_
 #define rg2nwr rg2nwr_
 
-// Wrapflgfwr
-
+// WrapFlgfwr.cc stubs this
 extern "C" void flgfwr(int& flkflg);
-// WrapDN
 
+// WrapDN.cc has been deleted.  The FLUKA internal function is used.
 extern "C" int idnrwr(const int & nreg, const int & mlat);
 
-// WrapG1
-
+// WrapG1.cpp has been deleted.  The function is now
+// defined in fluka_funcs.cpp.  It calls g1_fire.
 extern "C" void  g1wr(double& pSx, double& pSy, double& pSz, double* pV,
                       int& oldReg, const int& oldLttc, double& propStep,
                       int& nascFlag, double& retStep, int& newReg,
 	              double& saf, int& newLttc, int& LttcFlag,
                       double* sLt, int* jrLt);
 
-// WrapG1RT
+  /**
+   * g1_fire is called by fludag's implementation of g1wr.  It calls DAG->ray_fire(...).
+   * oldRegion region of start point
+   * point     start point
+   * dir       direction vector
+   * retStep   set to distance to next surface
+   * newRegion region ofter step
+  */
+  void g1_fire(int& oldRegion, double point[], double dir[],  double& retStep, int& newRegion);
 
+// WrapG1RT.cc has been deleted.  The function is now 
+// defined in fluka_funcs.cpp, as a stub.
 extern "C" void g1rtwr(void);
 
-// WrapIncrHist
+// WrapIncrHist.cc has been deleted.  The internal 
+// FLUKA form of this function is called.
+// extern "C" void conhwr(int& intHist, int* incrCount); 
 
-extern "C" void conhwr(int& intHist, int* incrCount); 
+// WrapIniHist.cc has been deleted.  The function is now
+// commented out in fluka_funcs.cpp
+// extern "C" void inihwr(int& intHist);                   
 
-// WrapIniHist
-
-extern "C" void inihwr(int& intHist);                   
-
-// WrapInit - calls createFlukaMatRegion(...)
-
+// WrapInit.cc - has been deleted, the function is now
+// defined in fluka_funcs.cpp
 extern "C" void jomiwr(int & nge, const int& lin, const int& lou,
                        int& flukaReg);
 
-// WrapLookDB
-
+// WrapLookDB.cc has been deleted, the function is now
+// defined in fluka_funcs.cpp.  It sets some of its return values
 extern "C" void lkdbwr(double& pSx, double& pSy, double& pSz,
                        double* pV, const int& oldReg, const int& oldLttc,
                	       int& newReg, int& flagErr, int& newLttc);
 
 // WrapLookFX
-
+// Stubbed in WrapLookFX.cc and linked in.
 extern "C" void lkfxwr(double& pSx, double& pSy, double& pSz,
                        double* pV, const int& oldReg, const int& oldLttc,
                        int& newReg, int& flagErr, int& newLttc);
 	    
-// WrapLookMG
-
+// WrapLookMG.cc stubs this function and is linked in.
 extern "C" void lkmgwr(double& pSx, double& pSy, double& pSz,
                        double* pV, const int& oldReg, const int& oldLttc,
 		       int& flagErr, int& newReg, int& newLttc);
 	    
-// WrapLookZ
-
+// WrapLookZ has been deleted.  This function is defined in
+// fluka_funcs.cc.  It is called by look(..).
 extern "C" void lkwr(double& pSx, double& pSy, double& pSz,
                      double* pV, const int& oldReg, const int& oldLttc,
 	             int& newReg, int& flagErr, int& newLttc);
 
-// WrapMag
+// Wrapper for lkwr clarifying which areguments are used.
+int look( double& posx, double& posy, double& posz, double* dir, int& region);
 
+// WrapMag.cc stubs this function and is linked in
 extern "C" void fldwr(const double& pX, const double& pY, const double& pZ,
                        double& cosBx, double& cosBy, double& cosBz, 
                        double& Bmag, int& reg, int& idiscflag);
 	    
-// WrapNorml
-
+// WrapNorml.cc has been removed.  This function is defined in fluka_funcs.
+//  It is called by normal(..).
 extern "C" void nrmlwr(double& pSx, double& pSy, double& pSz,
                        double& pVx, double& pVy, double& pVz,
 	               double* norml, const int& oldReg, 
 	               const int& newReg, int& flagErr);
+
+// Wrapper for nrmlwr clarifying which arguments are used
+int  normal (double& posx, double& posy, double& posz, double *norml, int& regionForSense);
 
 // WrapReg
 
 extern "C" void rgrpwr(const int& flukaReg, const int& ptrLttc, int& g4Reg,
                        int* indMother, int* repMother, int& depthFluka);
 
-// WrapSavHist
-	    
+// WrapSavHist.cc has been removed.  This function has been commented
+// out of an implementation in fluka_funcs.cpp, so the internal FLUKA 
+// version, if any, is called.
 extern "C" int isvhwr(const int& fCheck, const int& intHist);
 
-//Wrap region2name
-
+// WrapReg2name.cc defines this and is linked in.  It calls
+// region2name, which is defined in fluka_funcs
+// ToDo:  Remove WrapReg2name.cc and implement rg2nwr(..) in fluka_funcs.cpp
 extern "C" void rg2nwr(const int& mreg, char* Vname);
+// Defined in fluka_funcs, called by rg2nwr
+void region2name(int volindex, char * vname );
 
 #endif //DAGWRAPPERS_HH
 
