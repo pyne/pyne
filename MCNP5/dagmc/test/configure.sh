45a143ff0e51b001616f2d9337754dde323bf0ec
Julie C. Zachman <zachman@engr.wisc.edu>
2013-06-10 15:44:10 -0500

    Change test reference to local drive

--- MCNP5/dagmc/test/configure.sh
+++ MCNP5/dagmc/test/configure.sh
@@ -5,6 +5,6 @@ EXTRA_ARGS=$@
 rm -rf CMakeCache.txt
 
 cmake \
--D DAGMC_MESHTALLY_SOURCE=$HOME/research/alt_tallies/DAGMC/MCNP5/dagmc \
+-D DAGMC_MESHTALLY_SOURCE=$HOME/DAGMC/MCNP5/dagmc/test/ \
 $EXTRA_ARGS \
 ..
