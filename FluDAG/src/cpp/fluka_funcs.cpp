f60444db43d7dcd47e9366f9329420ad29d862af
Julie C. Zachman <zachman@engr.wisc.edu>
2013-06-14 08:21:52 -0500

    Pull f_idnr into fluka_funcs

--- FluDAG/src/cpp/fluka_funcs.cpp
+++ FluDAG/src/cpp/fluka_funcs.cpp
@@ -628,6 +628,36 @@ void f_g1rt(void)
     }
     return;
 }
+
+
+// Wrapper for setting DNEAR option on fluka side. Must return 0 
+// if user doesn't want Fluka to use DNEAR to compute the 
+// step (the same effect is obtained with the GLOBAL (WHAT(3)=-1)
+// card in fluka input), returns 1 if user wants Fluka always to 
+// use DNEAR (in this case, be sure that GEANT4 DNEAR is unique, 
+// coming from all directions!!!).
+//
+// modified 24.10.01: by I. Hrivnacova
+//   functions declarations separated from implementation
+//   (moved to Wrappers.hh);
+//
+///////////////////////////////////////////////////////////////////
+
+// #include "DagWrappers.hh"
+// #include "DagWrapUtils.hh"
+
+int f_idnr(const int & nreg, const int & mlat) 
+
+{
+	std::cerr<<"================== IDNRWR ================="<<std::endl;
+
+// returns 0 if user doesn't want Fluka to use DNEAR to compute the 
+// step (the same effect is obtained with the GLOBAL (WHAT(3)=-1)
+// card in fluka input), returns 1 if (be sure that GEANT4 DNEAR is unique, 
+// coming from all directions!!!) user wants Fluka always to use DNEAR.
+
+	return 0;
+}
 /**************************************************************************************************/
 /******                                End of FLUKA stubs                                  ********/
 /**************************************************************************************************/
