6856a1d2001894b6ac219735ee6ff4723f114469
Julie C. Zachman <zachman@engr.wisc.edu>
2013-06-20 14:21:56 -0500

    Incorporate WrapDN into fluka_funcs.cpp

--- FluDAG/src/cpp/fluka_funcs.cpp
+++ FluDAG/src/cpp/fluka_funcs.cpp
@@ -629,32 +629,15 @@ void f_g1rt(void)
     return;
 }
 
-
-// Wrapper for setting DNEAR option on fluka side. Must return 0 
-// if user doesn't want Fluka to use DNEAR to compute the 
-// step (the same effect is obtained with the GLOBAL (WHAT(3)=-1)
-// card in fluka input), returns 1 if user wants Fluka always to 
-// use DNEAR (in this case, be sure that GEANT4 DNEAR is unique, 
-// coming from all directions!!!).
-//
-// modified 24.10.01: by I. Hrivnacova
-//   functions declarations separated from implementation
-//   (moved to Wrappers.hh);
-//
-///////////////////////////////////////////////////////////////////
-
-// #include "DagWrappers.hh"
-// #include "DagWrapUtils.hh"
-
+// Set DNEAR option if needed
 int f_idnr(const int & nreg, const int & mlat) 
 
 {
-	std::cerr<<"================== IDNRWR ================="<<std::endl;
+	std::cerr<<"================== F_IDNR ================="<<std::endl;
 
 // returns 0 if user doesn't want Fluka to use DNEAR to compute the 
 // step (the same effect is obtained with the GLOBAL (WHAT(3)=-1)
-// card in fluka input), returns 1 if (be sure that GEANT4 DNEAR is unique, 
-// coming from all directions!!!) user wants Fluka always to use DNEAR.
+// card in fluka input), returns 1 if user wants Fluka always to use DNEAR.
 
 	return 0;
 }
@@ -830,7 +813,6 @@ void fludagwrite_assignma(std::string lfname)  // file with cell/surface cards
   int num_vols = DAG->num_entities(3);
   std::cout << __FILE__ << ", " << __func__ << ":" << __LINE__ << "_______________" << std::endl;
   std::cout << "\tnum_vols is " << num_vols << std::endl;
-  std::cout << "Graveyard list: " << std::endl;
   MBErrorCode ret;
   MBEntityHandle entity = 0;
   int id;
@@ -847,6 +829,7 @@ void fludagwrite_assignma(std::string lfname)  // file with cell/surface cards
   }
   // Preprocessing loop:  make a string, "props",  for each vol entity
   // This loop could be removed if the user doesn't care to see terminal output
+  std::cout << "Property list: " << std::endl;
   for (unsigned i=1; i<=num_vols; i++)
   {
      
