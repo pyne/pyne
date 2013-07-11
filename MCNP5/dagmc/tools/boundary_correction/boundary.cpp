11683db65f59a43fc7a2bde8c4036bdfe550a005
Kerry Dunn <klhaney@wisc.edu>
2013-07-11 15:25:56 -0500

    Issue #99: completes initial implementation of boundary correction tool

--- MCNP5/dagmc/tools/boundary_correction/boundary.cpp
+++ MCNP5/dagmc/tools/boundary_correction/boundary.cpp
@@ -7,6 +7,7 @@
 #include <vector>
 
 #include <DagMC.hpp>
+#include <moab/CartVect.hpp>
 #include <moab/Core.hpp>
 #include <moab/Interface.hpp>
 
@@ -67,20 +68,98 @@ void load_dagmc_instance(char* geometry_input_file)
 }
 
 // TODO write description for this method
-// returns -1.0 if distance to boundary is > max_distance
-double distance_to_boundary(double coords[3],
-                            double direction[3],
-                            double max_distance,
+// returns volume_ID of 0 if no valid volume is found and prints warning
+moab::EntityHandle find_volume_ID(const moab::CartVect& coords)
+{
+    // get number of 3D volumes from DAGMC instance
+    int num_volumes = dagmc->num_entities(3);
+
+    // iterate through volumes to find the one in which coords is located
+    moab::EntityHandle volume_ID = 0;
+    moab::CartVect direction(1.0, 0.0, 0.0);
+    int inside_volume = 0;
+    int volume_index = 1;
+    
+    while (inside_volume == 0 && volume_index <= num_volumes)
+    {
+        volume_ID = dagmc->entity_by_index(3, volume_index);
+        moab::ErrorCode dagmc_error = dagmc->point_in_volume(volume_ID,
+                                                             coords.array(),
+                                                             inside_volume,
+                                                             direction.array());
+
+        assert(dagmc_error == moab::MB_SUCCESS);
+
+        ++volume_index;
+    }
+
+    if (inside_volume == 0)
+    {
+        std::cerr << "Warning: coords (" << coords[0] << "," << coords[1] << ","
+                  << coords[2] << ") do not exist in any DAGMC volume.\n";
+        return 0;
+    }
+
+    return volume_ID;
+}
+
+// TODO write description for this method
+// returns -1.0 if distance to boundary is > max_distance or volume_ID is not valid
+double distance_to_boundary(double max_distance,
+                            const moab::CartVect& coords,
+                            const moab::CartVect& direction,
                             const moab::EntityHandle& volume_ID)
 {
-    // distance to the nearest boundary
-    double distance = 0.0;
+    assert(max_distance > 0.0);
+
+    // return if starting volume_ID is the graveyard
+     if (dagmc->has_prop(volume_ID, "graveyard")) return -1.0;
 
     // create RayHistory to store surface crossing history
     moab::DagMC::RayHistory surface_history;
 
-    // TODO call ray fire until either reach a distance greater than the
-    // max_distance or reach a reflecting or vacuum boundary
+    // call ray fire until a reflecting or vacuum boundary is found
+    moab::CartVect position = coords;
+    moab::EntityHandle current_volume = volume_ID;
+    moab::EntityHandle next_surface = 0;
+    bool found_boundary = false;
+    double distance = 0.0;
+    double next_surface_distance = 0.0;
+
+    do
+    {
+        // get distance to next boundary; ray_fire will only return next
+        // surface if less than or equal to max_distance
+        moab::ErrorCode dagmc_error = moab::MB_SUCCESS;
+
+        dagmc_error = dagmc->ray_fire(current_volume,
+                                      position.array(),
+                                      direction.array(),
+                                      next_surface,
+                                      next_surface_distance,
+                                      &surface_history,
+                                      max_distance);
+
+        assert(dagmc_error == moab::MB_SUCCESS);
+
+        // return if no intersection is found less than max_distance
+        if (next_surface == 0) return -1.0;
+
+        // otherwise update distance to boundary and current position
+        distance += next_surface_distance;
+        position += next_surface_distance * direction;
+
+        // update the current volume to the next volume
+        dagmc_error = dagmc->next_vol(next_surface, current_volume, current_volume);
+        assert(dagmc_error == moab::MB_SUCCESS);
+
+        if (dagmc->has_prop(next_surface, "spec.reflect") ||
+            dagmc->has_prop(current_volume, "graveyard"))
+        {
+            found_boundary = true;
+        }
+    }
+    while (found_boundary == false);
 
     return distance;
 }
@@ -89,33 +168,31 @@ double distance_to_boundary(double coords[3],
 // assumes node can only ever be near one boundary in each direction (lower OR upper)
 // if false is returned, then boundary and distance may have meaningless results
 bool node_near_boundary(const moab::EntityHandle& mesh_node,
-                        const std::vector<double>& bandwidth,
+                        const moab::CartVect& bandwidth,
                         int boundary[3],
                         double distance[3])
 {
-    bool boundary_point = false;
-    double direction[] = {0.0, 0.0, 0.0};
-    double coords[3];
-
     // get coordinates of the mesh node
-    moab::ErrorCode mb_error = mbi->get_coords(&mesh_node, 1, coords);
+    moab::CartVect coords;
+    moab::ErrorCode mb_error = mbi->get_coords(&mesh_node, 1, coords.array());
     assert(mb_error == moab::MB_SUCCESS);
 
     // determine volume in DAGMC geometry in which mesh node is located
-    moab::EntityHandle volume_ID;
-    // TODO add method to determine point in volume
-    // TODO (if no volume, print warning and return false)
-    // TODO call closest_to_location (if greater than bandwidth, return false)
+    moab::EntityHandle volume_ID = find_volume_ID(coords);
 
-    // mesh node may be near a boundary
+    if (volume_ID == 0) return false;
+
     // iterate through all directions x = 0, y = 1, and z = 2
+    bool boundary_point = false;
+    moab::CartVect direction(0.0);
+
     for (int i = 0; i < 3; ++i)
     {
         // compute distance to upper boundary
         direction[i] = 1.0;
-        distance[i] = distance_to_boundary(coords, 
+        distance[i] = distance_to_boundary(bandwidth[i],
+                                           coords,
                                            direction,
-                                           bandwidth[i],
                                            volume_ID);
 
         // test if mesh node is NOT within one bandwidth of upper boundary
@@ -123,9 +200,9 @@ bool node_near_boundary(const moab::EntityHandle& mesh_node,
         {
             // compute distance to lower boundary
             direction[i] = -1.0;
-            distance[i] = distance_to_boundary(coords,
+            distance[i] = distance_to_boundary(bandwidth[i],
+                                               coords,
                                                direction,
-                                               bandwidth[i],
                                                volume_ID);
 
             // test if mesh node is NOT within one bandwidth of lower boundary
@@ -164,11 +241,12 @@ int main(int argc, char* argv[])
 
     // read bandwidth vector from command line
     // TODO
-    std::vector<double> bandwidth(3, 0.1);
+    moab::CartVect bandwidth(0.1);
 
     // create new BOUNDARY and DISTANCE_TO_BOUNDARY tags
     int tag_size = 3;
     const std::vector<int> default_boundary(3, -1);
+    const moab::CartVect default_distance(-1.0);
     moab::ErrorCode mb_error = moab::MB_SUCCESS;
     moab::Tag boundary_tag, distance_tag;
 
@@ -183,7 +261,8 @@ int main(int argc, char* argv[])
     mb_error = mbi->tag_get_handle("DISTANCE_TO_BOUNDARY", tag_size,
                                    moab::MB_TYPE_DOUBLE,
                                    distance_tag,
-                                   moab::MB_TAG_SPARSE|moab::MB_TAG_CREAT);
+                                   moab::MB_TAG_DENSE|moab::MB_TAG_CREAT,
+                                   default_distance.array());
                                    
     assert(mb_error == moab::MB_SUCCESS);
 
@@ -193,7 +272,6 @@ int main(int argc, char* argv[])
     std::vector<moab::EntityHandle> mesh_nodes;
 
     mb_error = mbi->get_entities_by_type(mesh_set, moab::MBVERTEX, mesh_nodes);
-
     assert(mb_error == moab::MB_SUCCESS);
 
     // iterate through all mesh nodes
@@ -212,10 +290,10 @@ int main(int argc, char* argv[])
         if (boundary_point)
         {
             mb_error = mbi->tag_set_data(boundary_tag, &node, 1, boundary);
-            assert(mb_error = moab::MB_SUCCESS);
+            assert(mb_error == moab::MB_SUCCESS);
 
             mb_error = mbi->tag_set_data(distance_tag, &node, 1, distance);
-            assert(mb_error = moab::MB_SUCCESS);
+            assert(mb_error == moab::MB_SUCCESS);
         }
     }
 
@@ -224,10 +302,11 @@ int main(int argc, char* argv[])
 
     mb_error = mbi->write_file("output_mesh.vtk",
                                NULL, NULL, NULL, 0,
-                               output_tags, 1);
+                               output_tags, 2);
 
     // Memory management
     delete mbi;
+    // DAGMC has no destructor
 
     return 0;
 }
