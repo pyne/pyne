0fbc46830867580bbce039f3d9f92c27288aeb16
Matthew Klebenow <klebenowm@gmail.com>
2013-07-17 03:13:24 -0500

    Implemented nucname and transmute shorthands.

--- pyne/tests/test_transmute.py
+++ pyne/tests/test_transmute.py
@@ -15,9 +15,9 @@ from scipy import linalg
 
 from pyne.material import Material
 from pyne import nuc_data
-from pyne import nucname
+from pyne import nucname as nn
 from pyne import data
-from pyne import transmute
+from pyne import transmute as tm
 from pyne.xs.data_source import EAF_RX
 
 
@@ -26,7 +26,7 @@ def test_expm():
     A = np.zeros((3,3))
     t = 1.
     np.fill_diagonal(A,[-1,-2,-3])
-    transA = transmute._matrix_exp(A, t)
+    transA = tm._matrix_exp(A, t)
     eA = linalg.expm2(A)
     tayA = linalg.expm3(A,q=20)
     # Check the equivalence of _solve_decay_matrix
@@ -37,7 +37,7 @@ def test_check_phi():
     eaf_numEntries = 175
     # First check that None is properly converted
     nothing = None
-    phi = transmute._check_phi(None)
+    phi = tm._check_phi(None)
     assert_equal(phi.ndim, 2)
     assert_equal(phi.shape[0], eaf_numEntries)
     assert_equal(phi.shape[1], 1)
@@ -45,52 +45,52 @@ def test_check_phi():
         assert_equal(entry, 0)
     # Check that incorrect shape raises an exception
     phi = np.ones((175))
-    assert_raises(ValueError, transmute._check_phi, phi)
+    assert_raises(ValueError, tm._check_phi, phi)
     # Check that incorrect number of entries raises an exception
     phi = np.ones((50,1))
-    assert_raises(ValueError, transmute._check_phi, phi)
+    assert_raises(ValueError, tm._check_phi, phi)
     # Check that a negative entry raises an exception
     phi = np.ones((175,1))
     phi[123] = -1
-    assert_raises(ValueError, transmute._check_phi, phi)
+    assert_raises(ValueError, tm._check_phi, phi)
 
 """Tests correct application of the _get_daughters function"""
 def test_get_daughters():
-    nuc = nucname.zzaaam("O16")
+    nuc = nn.zzaaam("O16")
     data_table = tb.openFile(nuc_data)
     daughtersTest = [row['daughter'] for row in \
         data_table.root.neutron.eaf_xs.eaf_xs.where('nuc_zz == nuc')]
     data_table.close()
     for i in range(len(daughtersTest)):
-        daughtersTest[i] = transmute._convert_eaf(daughtersTest[i])
-    daughter_dict = transmute._get_daughters(nuc)
+        daughtersTest[i] = tm._convert_eaf(daughtersTest[i])
+    daughter_dict = tm._get_daughters(nuc)
     for daugh in daughter_dict.keys():
         assert(daugh in daughtersTest)
 
 """Tests conversion of EAF formatted nuc strings to zzaaam format"""
 def test_convert_eaf():
-    nuc1 = nucname.zzaaam('O16')
-    test1 = transmute._convert_eaf('O 16')
+    nuc1 = nn.zzaaam('O16')
+    test1 = tm._convert_eaf('O 16')
     assert_equal(nuc1, test1)
-    nuc2 = nucname.zzaaam('AU196')
-    test2 = transmute._convert_eaf('AU196G')
+    nuc2 = nn.zzaaam('AU196')
+    test2 = tm._convert_eaf('AU196G')
     assert_equal(nuc2, test2)
-    nuc3 = nucname.zzaaam('AU196M')
-    test3 = transmute._convert_eaf('AU196M1')
+    nuc3 = nn.zzaaam('AU196M')
+    test3 = tm._convert_eaf('AU196M1')
     assert_equal(nuc3, test3)
-    nuc4 = nucname.zzaaam('AU196') + 2
-    test4 = transmute._convert_eaf('AU196M2')
+    nuc4 = nn.zzaaam('AU196') + 2
+    test4 = tm._convert_eaf('AU196M2')
     assert_equal(nuc4, test4)
 
 """Tests correct implementation of the _get_decay function"""
 def test_get_decay():
     manual_dict = {}
-    nuc = nucname.zzaaam('O16')
+    nuc = nn.zzaaam('O16')
     children = data.decay_children(nuc)
     for child in children:
         branch = data.branch_ratio(nuc,child)
         manual_dict[child] = branch
-    transmute_dict = transmute._get_decay(nuc)
+    transmute_dict = tm._get_decay(nuc)
     assert_equal(manual_dict, transmute_dict)
 
 """Tests correct implementation of the _grow_matrix function"""
@@ -104,12 +104,12 @@ def test_grow_matrix():
                        [0.25,-0.3,0.,0.],
                        [0.,0.123,-1.2,0.],
                        [0.,0.,0.1848,-1.337]])
-    method = transmute._grow_matrix(orig, prod, dest)
+    method = tm._grow_matrix(orig, prod, dest)
     assert_true(np.array_equal(manual, method))
     orig = np.array([[-1.]])
     manual = np.array([[-1.,0.],
                        [0.1848,-1.337]])
-    method = transmute._grow_matrix(orig, prod, dest)
+    method = tm._grow_matrix(orig, prod, dest)
     assert_true(np.array_equal(manual, method))
 
 """Tests correct implementation of the _check_tol function"""
@@ -117,7 +117,7 @@ def test_check_tol():
     current = 1848.3
     tol = 1337.5
     N_ini = 1
-    assert_true(transmute._check_tol(current, tol))
+    assert_true(tm._check_tol(current, tol))
 
 """Tests corret implementation of the _tree_log() function"""
 def test_tree_log():
@@ -127,11 +127,11 @@ def test_tree_log():
     d2 = 2
     d11 = 1
     d20 = 0
-    nuc0 = nucname.zzaaam('O16')
-    nuc1 = nucname.zzaaam('O17')
-    nuc2 = nucname.zzaaam('O18')
-    nuc11 = nucname.zzaaam('He4')
-    nuc20 = nucname.zzaaam('C12')
+    nuc0 = nn.zzaaam('O16')
+    nuc1 = nn.zzaaam('O17')
+    nuc2 = nn.zzaaam('O18')
+    nuc11 = nn.zzaaam('He4')
+    nuc20 = nn.zzaaam('C12')
     N0 = 123.456
     N1 = 12.3456
     N2 = 1.23456
@@ -143,11 +143,11 @@ def test_tree_log():
     temp.append('   |--> HE4 (1111.0)\n')
     temp.append('--> C12 (12.0)\n')
     with open(filename, 'w') as tree:
-        transmute._tree_log(d0, nuc0, N0, tree)
-        transmute._tree_log(d1, nuc1, N1, tree)
-        transmute._tree_log(d2, nuc2, N2, tree)
-        transmute._tree_log(d11, nuc11, N11, tree)
-        transmute._tree_log(d20, nuc20, N20, tree)
+        tm._tree_log(d0, nuc0, N0, tree)
+        tm._tree_log(d1, nuc1, N1, tree)
+        tm._tree_log(d2, nuc2, N2, tree)
+        tm._tree_log(d11, nuc11, N11, tree)
+        tm._tree_log(d20, nuc20, N20, tree)
     with open(filename, 'r') as f:
         lines = f.readlines()
     for i in range(len(lines)):
@@ -157,43 +157,43 @@ def test_tree_log():
 """Tests correct implementation of a transmutation with zero flux on an
 isotope with a zero decay-constant."""
 def test_zero_flux():
-    nuc = nucname.zzaaam('FE56')
+    nuc = nn.zzaaam('FE56')
     t_sim = 100.
     phi = None
     tree = None
     tol = 1e-7
-    out = transmute.transmute_core(nuc, t_sim, phi, tree, tol)
+    out = tm.transmute_core(nuc, t_sim, phi, tree, tol)
     assert_equal(out[nuc], 1)
 
 """Tests that the root isotope is not being skipped"""
 def test_root_decrease():
-    nuc = nucname.zzaaam('FE56')
+    nuc = nn.zzaaam('FE56')
     t_sim = 100.
     phi = np.zeros((175,1))
     for i in np.arange(phi.shape[0]):
         phi[i] = 1e12
     tree = None
     tol = 1e-7
-    out = transmute.transmute_core(nuc, t_sim, phi, tree, tol)
+    out = tm.transmute_core(nuc, t_sim, phi, tree, tol)
     assert_true(out[nuc] < 1)
 
 """Tests that transmute_core and transmute agree"""
 def test_trans_v_transCore():
-    nuc = nucname.zzaaam('FE56')
+    nuc = nn.zzaaam('FE56')
     t_sim = 100.
     phi = np.zeros((175,1))
     for i in np.arange(phi.shape[0]):
         phi[i] = 1.0E+12
-    out_core = transmute.transmute_core(nuc, t_sim, phi)
+    out_core = tm.transmute_core(nuc, t_sim, phi)
     inp = {nuc: 1.0}
-    out = transmute.transmute(inp, t_sim, phi)
+    out = tm.transmute(inp, t_sim, phi)
     for key in out.keys():
         assert_true(key in out_core.keys())
         assert_equal(out[key], out_core[key])
 
 """Tests that transmute and transmute_spatial agree"""
 def test_trans_v_transSpat():
-    nuc = nucname.zzaaam('FE56')
+    nuc = nn.zzaaam('FE56')
     t_sim = 100.
     phi = np.zeros((175,1))
     for i in np.arange(phi.shape[0]):
@@ -201,8 +201,8 @@ def test_trans_v_transSpat():
     inp = {nuc: 1.0}
     inp2 = {nuc: 2.0}
     space = {1: (phi, inp), 2: (phi, inp2)}
-    out = transmute.transmute(inp, t_sim, phi)
-    space_out = transmute.transmute_spatial(space, t_sim)
+    out = tm.transmute(inp, t_sim, phi)
+    space_out = tm.transmute_spatial(space, t_sim)
     for key in out.keys():
         assert_true(key in space_out[1].keys())
         assert_true(key in space_out[2].keys())
@@ -211,9 +211,9 @@ def test_trans_v_transSpat():
 
 """Tests if decay is properly implemented"""
 def test_tm171_decay():
-    nuc = nucname.zzaaam('TM171')
+    nuc = nn.zzaaam('TM171')
     t_sim = 1.2119E+8 # Run for 3.843 years (approx 2 half lives)
-    out = transmute.transmute_core(nuc, t_sim, None)
+    out = tm.transmute_core(nuc, t_sim, None)
     # With no flux, the result should be pure decay
     assert_true(nuc in out.keys())
     tm_res = out[nuc]
