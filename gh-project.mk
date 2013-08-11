77754357545da3b80570017f93d2286f432f0b33
Paul P.H. Wilson <wilsonp@engr.wisc.edu>
2013-08-11 17:36:50 -0500

    Update for moved doc source

--- gh-project.mk
+++ gh-project.mk
@@ -1,7 +1,7 @@
 # Example for a Github hosted domain account: account.github.io
 #
 # GH_SOURCE_DIR = top-level directory of all the ReST source files
-GH_SOURCE_DIR = doc-src/source
+GH_SOURCE_DIR = doc-src
 # GH_SOURCE_BRANCH = repository branch that contains the source
 GH_SOURCE_BRANCH = master
 # GH_PUBLISH_BRANCH = repository branch that contains the rendered HTML
