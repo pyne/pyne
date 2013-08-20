f7950765f74043f49ad2086e89fccf17358180fe
elliottbiondo <Biondo@wisc.edu>
2013-08-20 14:25:23 -0500

    Added tests and documentation

--- pyne/stat_mesh.py
+++ pyne/stat_mesh.py
@@ -1,16 +1,7 @@
-#!/usr/bin/env python
-
-import sys
-import linecache
-import itertools
-
-from itaps import iBase, iMesh
-from collections import namedtuple, Iterable
-from r2s.scdmesh import ScdMesh, ScdMeshError
 
 class StatMesh(object):
     """This is a class to store and manipulate information on a MOAB mesh
-    containing statistically quantities (with an average and statistical error).
+    containing statistical quantities (with an average and statistical error).
     """
 
     def __init__():
