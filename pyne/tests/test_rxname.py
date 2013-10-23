9dee0d14b25d62e7c1ab28e88380e60efbcd6494
Anthony Scopatz <scopatz@gmail.com>
2013-10-23 02:56:02 -0500

    Added tests and python hooks

--- pyne/tests/test_rxname.py
+++ pyne/tests/test_rxname.py
@@ -135,9 +135,11 @@ def test_mt_not():
     assert_raises(RuntimeError, rxname.mt, "Waka waka")
     assert_raises(RuntimeError, rxname.mt, 0)
 
-
 def test_child():
-    assert_equal(rxname.child("U235", "absorption"), "U236")
+    assert_equal(rxname.child("U235", "absorption"), 922360000)
+
+def test_parent():
+    assert_equal(rxname.parent("U235", "absorption"), 922340000)
 
 
 
