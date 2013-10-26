837961bc6a905d992744094a711f7131a99f80c7
John Xia <john.danger.xia@gmail.com>
2013-10-26 13:49:55 -0500

    changed abundance_by_Z to module-level dict

--- pyne/data.pyx
+++ pyne/data.pyx
@@ -78,14 +78,13 @@ cdef conv._MapIntDouble natural_abund_map_proxy = conv.MapIntDouble(False)
 natural_abund_map_proxy.map_ptr = &cpp_data.natural_abund_map
 natural_abund_map = natural_abund_map_proxy
 
-def abundance_by_Z():
-    "Returns the natural abundances of all non-trivial nuclides sorted by Z."
-    cpp_data.natural_abund(<int>10000000)
-    abundance_by_Z = dict([(i, []) for i in range(1,119)])
-    for zas, abundance in natural_abund_map.items():
-        if 0.0 < abundance <= 1.0:
-            abundance_by_Z[zas/10000000].append((zas, abundance))
-    return abundance_by_Z
+# initialize natural_abund_map
+cpp_data.natural_abund(<int>10000000)
+
+abundance_by_Z = dict([(i, []) for i in range(1,119)])
+for zas, abundance in natural_abund_map.items():
+    if 0.0 < abundance <= 1.0:
+        abundance_by_Z[zas/10000000].append((zas, abundance))
 
 
 def natural_abund(nuc):
