80c3522fb5d051416057347e07a3dcb2721c8031
py1sl <py1sl@server>
2013-12-03 22:34:56 +0000

    PEP8ifing

--- pyne/gammaSpec.py
+++ pyne/gammaSpec.py
@@ -1,35 +1,32 @@
-"""
-    This module is for gamma spectrometry analysis
-    November 2013
+"""Purpose:
+
+  This module is for gamma spectrometry analysis
+  will have functions for activity calculation
+   
+  .. moduleauthor:: S Lilley
 
 """
 
-# import useful modules
 import numpy as np
-# import local modules
-import spectAnalysis
+import spectanalysis
 
 
-class gammaSpec(spectAnalysis.phSpectra):
-    """ gamma Spec class includes gamma specific functionality
-
-        inherits from PHspectra class in spectAnalysis
-
-    """
+class GammaSpec(spectanalysis.PhSpectra):
+    """ GammaSpec class includes gamma specific variables"""
 
     def __init__(self):
         """ define gamma spectra specific variables """    
-        spectAnalysis.phSpectra.__init__(self)
+        super(GammaSpec,self).__init__()
         self.real_time = 0
         self.live_time = 0
         self.dead_time = 0
-        self.det_ID = ""
+        self.det_id = ""
         self.det_descp = ""
         self.start_date = ""
         self.start_time = ""
-        self.calib_E_fit = []
-        self.calib_FWHM_fit = []
-        self.Eres = 0
+        self.calib_e_fit = []
+        self.calib_fwhm_fit = []
+        self.eres = 0
         self.file_name = ""
 
     def calc_dead_time(self):
@@ -39,10 +36,10 @@ class gammaSpec(spectAnalysis.phSpectra):
     def calc_ebins(self):
         """ Calculate the energy value for each channel."""
         for item in self.channels:
-            energy = self.calib_E_fit[0] + (self.calib_E_fit[1]*item)
-            + (self.calib_E_fit[2]*item*item)
-            self.Ebin.append(energy)
-        self.Ebin = np.array(self.Ebin)
+            energy = self.calib_e_fit[0] + (self.calib_e_fit[1]*item)
+            + (self.calib_e_fit[2]*item*item)
+            self.ebin.append(energy)
+        self.ebin = np.array(self.ebin)
 
     def print_parameters(self):
         """Print all the gamma spectrum parameters"""
@@ -50,83 +47,77 @@ class gammaSpec(spectAnalysis.phSpectra):
         print "The real time is:" + str(self.real_time)
         print "The live time is:" + str(self.live_time)
         print "The dead time is:" + str(self.dead_time)
-        print "Detector ID:" + self.det_ID
+        print "Detector ID:" + self.det_id
         print "Detector description:" + self.det_descp
         print "Start date:" + self.start_date
         print "Start time:" + self.start_time
         print "Start channel number:" + str(self.start_chan_num)
         print "Number of channels:" + str(self.num_channels)
-        print "Energy calibration fit:" + str(self.calib_E_fit)
-        print "FWHM calibration fit:" + str(self.calib_FWHM_fit)
+        print "Energy calibration fit:" + str(self.calib_e_fit)
+        print "FWHM calibration fit:" + str(self.calib_fwhm_fit)
         print "Spectrum:" + str(self.counts)
         print "File name:" + self.file_name
 
 
-def readspefile(spec_file_path):
+def read_spe_file(spec_file_path):
     """ Read spe file
 
-    Reads a gamma vision .spe file and assigns all the useful
-    parts to the correct variables.
-    arguments: a file path to read in
-    returns: gamma spectra object
+    
 
     """
 
-    print "Starting to read file:" + spec_file_path
     spectra = gammaSpec()
     spectra.file_name = spec_file_path
    
-    spec_file = open(spec_file_path, "r")
-    full_file_text = spec_file.read()
+    with open(spec_file_path, "r") as spec_file:
+        full_file_text = spec_file.read()
     file_split = full_file_text.split("\n")
     spec_file.close()
     inspec = False
 
     for item in file_split:
         line = item.split(":")
-        # it processes the spectrum into 2 lists 1 for channel numbers
+        # processes the spectrum into 2 lists 1 for channel numbers
         # the other for counts
-        if(inspec):
+        if (inspec):
             if (len(line) > 1):
                 spectra.channels.append(int(line[0]))
                 temp = line[1].strip()
                 spectra.counts.append(float(temp))
 
-        if(line[0] == "Spectrum name"):
+        if (line[0] == "Spectrum name"):
             spectra.spec_name = line[1]
-        elif(line[0] == "Detector ID"):
-            spectra.det_ID = line[1].strip()
-        elif(line[0] == "Detector description"):
+        elif (line[0] == "Detector ID"):
+            spectra.det_id = line[1].strip()
+        elif (line[0] == "Detector description"):
             spectra.det_descp = line[1].strip()
-        elif(line[0] == "Real Time"):
+        elif (line[0] == "Real Time"):
             spectra.real_time = float(line[1])
-        elif(line[0] == "Live Time"):
+        elif (line[0] == "Live Time"):
             spectra.live_time = float(line[1])
-        elif(line[0] == "Acquisition start date"):
+        elif (line[0] == "Acquisition start date"):
             spectra.start_date = line[1].strip()
-        elif(line[0] == "Acquisition start time"):
+        elif (line[0] == "Acquisition start time"):
             spectra.start_time = line[1].strip() + ":" + line[2] + ":"+line[3]
-        elif(line[0] == "Starting channel number"):
+        elif (line[0] == "Starting channel number"):
             spectra.start_chan_num = int(line[1].strip())
-        elif(line[0] == "Number of channels"):
+        elif (line[0] == "Number of channels"):
             spectra.num_channels = int(line[1].strip())
-        elif(line[0] == "Energy Fit"):
+        elif (line[0] == "Energy Fit"):
             temp = line[1].strip()
             temp = temp.split(" ")
-            spectra.calib_E_fit.append(float(temp[0]))
-            spectra.calib_E_fit.append(float(temp[2]))
-            spectra.calib_E_fit.append(float(temp[4]))
-        elif(line[0] == "FWHM Fit"):
+            spectra.calib_e_fit.append(float(temp[0]))
+            spectra.calib_e_fit.append(float(temp[2]))
+            spectra.calib_e_fit.append(float(temp[4]))
+        elif (line[0] == "FWHM Fit"):
             temp = line[1].strip()
             temp = temp.split(" ")
-            spectra.calib_FWHM_fit.append(float(temp[0]))
-            spectra.calib_FWHM_fit.append(float(temp[2]))
-            spectra.calib_FWHM_fit.append(float(temp[4]))
-        elif(line[0] == "SPECTRUM"):
+            spectra.calib_fwhm_fit.append(float(temp[0]))
+            spectra.calib_fwhm_fit.append(float(temp[2]))
+            spectra.calib_fwhm_fit.append(float(temp[4]))
+        elif (line[0] == "SPECTRUM"):
             inspec = True
 
-    print "Finished file read"
-    #convert to numpy arrays
     spectra.counts = np.array(spectra.counts)
     spectra.channels = np.array(spectra.channels)
     # calculate additional parameters
--- pyne/spectAnalysis.py
+++ pyne/spectAnalysis.py
@@ -1,59 +1,25 @@
+"""Purpose:
+
+  This module is for spectrometry analysis
+  will have functions for general spectrum processing
+   
+  .. moduleauthor:: S Lilley
+
 """
-    Spectra processing
 
-    Secondary capabilities include plotting of spectra, plotting of peaks
-    Nov 2013
-
-"""
-
-# import useful modules
-import matplotlib.pyplot as plt
 import numpy as np
-import math
 
 
-class phSpectra():
+class PhSpectra():
     """ pulse height spectra class"""
 
     def __init__(self):
         """Initialise phSpectra variables"""
         self.channels = []
-        self.Ebin = []
+        self.ebin = []
         self.counts = []
         self.spec_name = ""
         self.start_chan_num = 0
         self.num_channels = 0
 
 
-def plot_spectra(spect):
-    """ Create a standard spectrum pulse height plot"""
-    plt.plot(spect.Ebin, spect.counts)
-    plt.xlim(xmin=10)
-    plt.ylim(ymin=1)
-    plt.xlabel("Energy (KeV)")
-    plt.ylabel("Counts")
-    plt.yscale("log")
-    plt.show()
-
-
-def plot_peak(spec1, energy, spread=4):
-    """
-    Create a filled plot of a region of the spectra around a given energy
-    value.
-
-    Inputs: spec1=spectrum object from which to plot
-            energy=the energy to centre the plot on
-            spread (default=4KeV)= half the energy range to plot i.e the
-            number of KeV above and below the energy value to plot
-
-    Returns: nothing
-    Displays: matplotlib filled plot
-
-    """
-    plt.plot(spec1.Ebin, spec1.counts)
-    plt.ylim(ymin=1)
-    plt.xlabel("Energy (KeV)")
-    plt.ylabel("Counts")
-    plt.xlim(xmin=energy-spread, xmax=energy+spread)
-    plt.fill(spec1.Ebin, spec1.counts, "g")
-    plt.show()
