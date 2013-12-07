8aa216d296250a5fd1ee4ae5d0c66caf51239ea8
Cameron Bates <cameron.r.bates@gmail.com>
2013-12-07 00:32:42 -0800

    Use ctypes.cdll's LoadLibrary function so no path editing is required on win/mac/linux

--- pyne/__init__.py
+++ pyne/__init__.py
@@ -1,8 +1,15 @@
 import os
+import sys
+from ctypes import cdll
 
+lib = os.path.join(os.path.split(__file__)[0], 'lib')
+libtype = '.so'
 if os.name == 'nt':
-    p = os.environ['PATH'].split(';')
-    lib = os.path.join(os.path.split(__file__)[0], 'lib')
-    os.environ['PATH'] = ";".join([lib] + p)
-    
+    libtype = '.dll'
+elif sys.platform == 'darwin':
+    libtype = '.dylib'
+liblist = ['libpyne', 'libpyne_data', 'libpyne_material', 'libpyne_enrichemnt', 'libpyne_nucname', 'libpyne_rxname']
+for item in liblist:
+    cdll.LoadLibrary(os.path.join(lib, item + libtype))
+
 from .pyne_config import *
