9aea0abbcc8721650e277f179c046469bfd6e37a
Cameron Bates <cameron.r.bates@gmail.com>
2013-12-07 12:58:30 -0800

    Don't load libraries under linux as it's unecessary

--- pyne/__init__.py
+++ pyne/__init__.py
@@ -2,14 +2,17 @@ import os
 import sys
 from ctypes import cdll
 
+#Make sure libraries are loaded on windows and osx
 lib = os.path.join(os.path.split(__file__)[0], 'lib')
-libtype = '.so'
+libtype = None
 if os.name == 'nt':
     libtype = '.dll'
 elif sys.platform == 'darwin':
     libtype = '.dylib'
 liblist = ['libpyne', 'libpyne_data', 'libpyne_material', 'libpyne_enrichemnt', 'libpyne_nucname', 'libpyne_rxname']
-for item in liblist:
-    cdll.LoadLibrary(os.path.join(lib, item + libtype))
+
+if libtype is not None:
+    for item in liblist:
+        cdll.LoadLibrary(os.path.join(lib, item + libtype))
 
 from .pyne_config import *
