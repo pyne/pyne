9a9e666be16dabdbf7f2fb927f9acaaa94677fb6
Cameron Bates <cameron.r.bates@gmail.com>
2013-12-10 14:19:09 -0800

    initialiaze cv to 0,0 to get rid of warning

--- pyne/extra_types.pyx
+++ pyne/extra_types.pyx
@@ -1,5 +1,5 @@
 cdef complex_t py2c_complex(object pyv):
-    cdef complex_t cv = complex_t()
+    cdef complex_t cv = complex_t(0,0)
     pyv = complex(pyv)
     cv.re = pyv.real
     cv.im = pyv.imag
