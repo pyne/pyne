8489ad3f4fb7451e90d7dde631136330f75cc51a
py1sl <py1sl@server>
2013-12-26 23:08:53 +0000

    updated spectplots to work with new names

--- pyne/gui/spectplots.py
+++ pyne/gui/spectplots.py
@@ -3,14 +3,15 @@
 import matplotlib.pyplot as plt
 
 
-def plot_spectra(spect):
+def plot_spectrum(spect):
     """ Create a standard spectrum pulse height plot"""
-    plt.plot(spect.Ebin, spect.counts)
+    plt.plot(spect.ebin, spect.counts)
     plt.xlim(xmin=10)
     plt.ylim(ymin=1)
     plt.xlabel("Energy (KeV)")
     plt.ylabel("Counts")
     plt.yscale("log")
+    plt.title(spect.file_name)
     plt.show()
 
 
@@ -18,20 +19,12 @@ def plot_peak(spec1, energy, spread=4):
     """
     Create a filled plot of a region of the spectra around a given energy
     value.
-
-    Inputs: spec1=spectrum object from which to plot
-            energy=the energy to centre the plot on
-            spread (default=4KeV)= half the energy range to plot i.e the
-            number of KeV above and below the energy value to plot
-
-    Returns: nothing
-    Displays: matplotlib filled plot
-
     """
-    plt.plot(spec1.Ebin, spec1.counts)
+    plt.plot(spec1.ebin, spec1.counts)
     plt.ylim(ymin=1)
     plt.xlabel("Energy (KeV)")
     plt.ylabel("Counts")
     plt.xlim(xmin=energy-spread, xmax=energy+spread)
-    plt.fill(spec1.Ebin, spec1.counts, "g")
+    plt.fill(spec1.ebin, spec1.counts, "g")
+    plt.title(spec1.file_name + " " + str(energy))
     plt.show()
