ed2de766e54c340afa9e66741661e65c13bf7349
py1sl <py1sl@server>
2014-01-05 22:05:34 +0000

    updated tests, added efficiency functions

--- tests/test_spectrometry.py
+++ tests/test_spectrometry.py
@@ -5,23 +5,23 @@ from nose.tools import assert_equal
 import gammaspec
 
 gspec1 = gammaspec.read_spe_file("test.spe")
+eff_coeff=[-2.818615042612040000,-0.727352820018942000,-0.039579888648190400,
+           -0.059230525466409600,0.023772637347443000,0.032530647507267100]
 
-def test_read_times():
+
+def test_read_spe():
     assert_equal(gspec1.real_time, 300.0)
     assert_equal(gspec1.live_time, 274.0)
     assert_equal(gspec1.start_time, "12:18:05  ")
     assert_equal(gspec1.start_date, "15-Mar-2012")
     assert_equal(gspec1.dead_time, 26.0)
-    
-    
-def test_read_det():
     assert_equal(gspec1.det_id, "1")
     assert_equal(gspec1.det_descp, "DSPEC PRO 1")
-
-
-def test_read_channels():
-     assert_equal(gspec1.start_chan_num, 0)
-     assert_equal(gspec1.num_channels, 16384)
+    assert_equal(gspec1.start_chan_num, 0)
+    assert_equal(gspec1.num_channels, 16384)
+    
+def test_calib():
+    assert_equal(gammaspec.calc_e_eff(1, eff_coeff, 1), 0.059688551591347033)
 
 if __name__ == "__main__":
     nose.runmodule()
