56c70593ffcacd0468eb4924edb82a9c0e08456d
Anthony Scopatz <scopatz@gmail.com>
2014-01-06 19:24:51 -0800

    made it through dagmc

--- pyne/dagmc.pyx
+++ pyne/dagmc.pyx
@@ -393,54 +393,45 @@ def fire_one_ray(vol_id, xyz, uvw):
     else:
         return None
 
-"""\
 
 def ray_iterator_slow(init_vol_id, startpoint, direction, **kw):
-    ""Return an iterator for a ray in a single direction.
+    """Return an iterator for a ray in a single direction.
 
     The iterator will yield a series of tuples (vol,dist,surf), indicating the next
-    volume intersected, the distance to the next intersection (from the last intersection),
-    and the surface intersected.  Stops iterating when no further intersections are 
-    detected along the ray.  This is the only way to traverse volumes along a given ray.
+    volume intersected, the distance to the next intersection (from the last 
+    intersection), and the surface intersected.  Stops iterating when no further 
+    intersections are detected along the ray.  This is the only way to traverse 
+    volumes along a given ray.
 
     Keyword arguments:
-    yield_xyz: results will contain a fourth tuple element, being the xyz position of the 
-               intersection
+    yield_xyz: results will contain a fourth tuple element, being the xyz 
+               position of the intersection
     dist_limit: distance at which to consider the ray ended
-    ""
-
-    eh = bridge.EntityHandle(vol_id_to_handle[init_vol_id])
-    xyz = np.array(startpoint, dtype=np.np.float64)
-    uvw = np.array(direction, dtype=np.np.float64)
+    """
+    eh = EntityHandle(vol_id_to_handle[init_vol_id])
+    xyz = np.array(startpoint, dtype=np.float64)
+    uvw = np.array(direction, dtype=np.float64)
 
     use_dist_limit = ('dist_limit' in kw)
-    dist_limit = kw.get('dist_limit',0.0)
+    dist_limit = kw.get('dist_limit', 0.0)
 
-    with bridge._ray_history() as history:
-        
-        surf = bridge.EntityHandle(0)
-        dist_result = ctypes.c_double(0.0)
+    with _ray_history() as history:
         while eh != 0:
-
             if use_dist_limit and dist_limit <= 0:
                 break
-
-            bridge.lib.dag_ray_fire(eh, xyz, uvw, 
-                                     ctypes.byref(surf), ctypes.byref(dist_result),
-                                     history, dist_limit)
-
-            if surf.value == 0:
+            surf, dist_result = dag_ray_fire(eh, xyz, uvw, history, dist_limit)
+            if surf == 0:
                 break
 
             # eh = the new volume
-            bridge.lib.dag_next_vol(surf, eh, ctypes.byref(eh))
-            xyz += uvw * dist_result.value
+            eh = dag_next_vol(surf, eh)
+            xyz += uvw * dist_result
             if use_dist_limit:
-                dist_limit -= dist_result.value
+                dist_limit -= dist_result
 
-            newvol = vol_handle_to_id[eh.value]
-            dist = dist_result.value
-            newsurf = surf_handle_to_id[surf.value]
+            newvol = vol_handle_to_id[eh]
+            dist = dist_result
+            newsurf = surf_handle_to_id[surf]
             
             if kw.get('yield_xyz', False) :
                 yield (newvol, dist, newsurf, xyz)
@@ -449,45 +440,39 @@ def ray_iterator_slow(init_vol_id, startpoint, direction, **kw):
 
 
 def ray_iterator(init_vol_id, startpoint, direction, **kw):
-
-    eh = bridge.EntityHandle(vol_id_to_handle[init_vol_id])
-    xyz = np.array(startpoint, dtype=np.np.float64)
-    uvw = np.array(direction, dtype=np.np.float64)
-
+    cdef int i, x
+    eh = EntityHandle(vol_id_to_handle[init_vol_id])
+    xyz = np.array(startpoint, dtype=np.float64)
+    uvw = np.array(direction, dtype=np.float64)
     dist_limit = kw.get('dist_limit',0.0)
+    buf = RayBuffer()
 
-    surfs = ctypes.POINTER(bridge.EntityHandle)()
-    vols = ctypes.POINTER(bridge.EntityHandle)()
-    dists = ctypes.POINTER(ctypes.c_double)()
-    buf = ctypes.c_void_p()
-    x = ctypes.c_int(0)
-
-    bridge.lib.dag_ray_follow(eh, xyz, uvw, dist_limit, 
-                               ctypes.byref(x), ctypes.byref(surfs), 
-                               ctypes.byref(dists), ctypes.byref(vols), buf)
-    for i in range(x.value):
-        vol_id = vol_handle_to_id[vols[i].value]
-        surf_id = surf_handle_to_id[surfs[i].value]
+    x, surfs, dists, vols = dag_ray_follow(eh, xyz, uvw, dist_limit, buf)
+    for i in range(x):
+        vol_id = vol_handle_to_id[vols[i]]
+        surf_id = surf_handle_to_id[surfs[i]]
         if kw.get('yield_xyz', False):
             xyz += uvw * dists[i]
             yield (vol_id, dists[i], surf_id, xyz)
         else:
             yield (vol_id, dists[i], surf_id)
     
-    bridge.lib.dag_dealloc_ray_buffer(buf)
+    dag_dealloc_ray_buffer(buf)
+
 
 def tell_ray_story(startpoint, direction, output=sys.stdout, **kw):
-    ""Write a human-readable history of a ray in a given direction.
+    """Write a human-readable history of a ray in a given direction.
 
-    The history of the ray from startpoint in direction is written to the given output file.
+    The history of the ray from startpoint in direction is written 
+    to the given output file.
     The initial volume in which startpoint resides will be determined, and 
     the direction argument will be normalized to a unit vector.
 
     kw args are passed on to underlying call to ray_iterator
 
-    ""
-    xyz = np.array(startpoint, dtype=np.np.float64)
-    uvw = np.array(direction, dtype=np.np.float64) 
+    """
+    xyz = np.array(startpoint, dtype=np.float64)
+    uvw = np.array(direction, dtype=np.float64) 
     uvw /= norm(uvw)
 
     def pr(*args): 
@@ -499,15 +484,15 @@ def tell_ray_story(startpoint, direction, output=sys.stdout, **kw):
         if md['material'] == 0:
             notes.append('void')
         else:
-            notes.append('mat='+str(md['material']))
-            notes.append('rho='+str(md['rho']))
+            notes.append('mat=' + str(md['material']))
+            notes.append('rho=' + str(md['rho']))
         if volume_is_graveyard(v): 
             notes.append('graveyard')
         if volume_is_implicit_complement(v):
             notes.append('implicit complement')
         return '({0})'.format(', '.join(notes))
 
-    pr('Starting a ray at',xyz,'in the direction',uvw)
+    pr('Starting a ray at', xyz, 'in the direction', uvw)
     
     if 'dist_limit' in kw:
         pr('with a dist_limit of', kw['dist_limit'])
@@ -521,11 +506,13 @@ def tell_ray_story(startpoint, direction, output=sys.stdout, **kw):
 
     for (vol, dist, surf, xyz) in ray_iterator(first_volume, xyz, uvw, **kwargs):
 
-        pr('  next intersection at distance',dist,'on surface',surf)
+        pr('  next intersection at distance', dist, 'on surface', surf)
         pr('  new xyz =', xyz)
         pr('proceeding into volume', vol, vol_notes(vol))
 
     pr('No more intersections')
+
+"""\
 
 #### start util
 
