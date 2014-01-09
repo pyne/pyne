2e346f9d7c763cbfe3eaedbdd18c3fcbaf13c479
Anthony Scopatz <scopatz@gmail.com>
2014-01-09 12:43:09 -0600

    Added converter for phtn_src to hdf5

--- tests/test_alara.py
+++ tests/test_alara.py
@@ -3,8 +3,10 @@ import os
 import nose
 
 from nose.tools import assert_almost_equal
-from nose.tools import assert_equal
+from nose.tools import assert_equal, assert_true, with_setup
 from numpy.testing import assert_array_equal
+import numpy as np
+import tables as tb
 
 # mesh specific imports
 try:
@@ -16,7 +18,7 @@ except ImportError:
     pass
 
 from pyne.mesh import Mesh, StatMesh, MeshError
-from pyne.alara import flux_mesh_to_fluxin
+from pyne.alara import flux_mesh_to_fluxin, photon_source_to_hdf5
 
 thisdir = os.path.dirname(__file__)
 
@@ -88,3 +90,21 @@ def test_write_fluxin_multiple():
         expected = f.readlines()
 
     assert_equal(written, expected)
+
+def test_photon_source_to_hdf5():
+    filename = os.path.join(thisdir, "files_test_alara", "phtn_src") 
+    photon_source_to_hdf5(filename, chunkshape=(10,))
+    assert_true(os.path.exists(filename + '.h5'))
+    
+    with tb.openFile(filename + '.h5') as h5f:
+        obs = h5f.root.data[:]
+    
+    with open(filename, 'r') as f:
+        for line, row in zip(f, obs):
+            ls = line.strip().split('\t')
+            assert_equal(ls[0].strip(), row['nuc'])
+            assert_equal(ls[1].strip(), row['time'])
+            assert_array_equal(np.array(ls[2:], dtype=np.float64), row['phtn_src'])
+    
+    if os.path.isfile(filename + '.h5'):
+        os.remove(filename + '.h5')
