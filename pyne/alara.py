a9367e1a927322900947536c7d0508efb80fad83
Elliott Biondo <elliott.biondo@gmail.com>
2014-01-09 22:10:57 -0600

    Changed photon source to hdf5 function to include volume index

--- pyne/alara.py
+++ pyne/alara.py
@@ -75,7 +75,7 @@ def flux_mesh_to_fluxin(flux_mesh, flux_tag, fluxin="fluxin.out",
 
 def photon_source_to_hdf5(filename, chunkshape=(10000,)):
     """Converts a plaintext photon source file to an HDF5 version for 
-    quick later use.
+    quick later use. 
 
     Parameters
     ----------
@@ -84,6 +84,19 @@ def photon_source_to_hdf5(filename, chunkshape=(10000,)):
     chunkshape : tuple of int
         A 1D tuple of the HDF5 chunkshape.
 
+    Output
+    ------
+    A single HDF5 file named filename.h5 containg the table headings:
+
+    ve_idx : int
+        The volume element index assuming the volume elements appear in xyz
+        order (z changing fastest).
+    nuc : str
+        The nuclide name as it appears in the photon source file.
+    time : str
+        The decay time as it appears in the photon source file.
+    phtn_src : 1D array of floats
+        Contains the photon source density for each energy group.   
     """
     f = open(filename, 'r')
     header = f.readline().strip().split('\t')
@@ -91,6 +104,7 @@ def photon_source_to_hdf5(filename, chunkshape=(10000,)):
     G = len(header) - 2
 
     dt = np.dtype([
+        ('ve_idx', np.int64),
         ('nuc', 'S6'),
         ('time', 'S20'),
         ('phtn_src', np.float64, G),
@@ -102,15 +116,78 @@ def photon_source_to_hdf5(filename, chunkshape=(10000,)):
 
     chunksize = chunkshape[0]
     rows = np.empty(chunksize, dtype=dt)
+    ve_idx = 0
     for i, line in enumerate(f, 1):
         ls = line.strip().split('\t')
+
+        # Keep track of the ve_idx by delimiting by the last TOTAL line in a
+        # volume element.
+        if i != 1:
+            if ls[0]  != 'TOTAL' and old == 'TOTAL':
+                ve_idx += 1
+
         j = (i-1)%chunksize
-        rows[j] = (ls[0].strip(), ls[1].strip(), np.array(ls[2:], dtype=np.float64))
+        rows[j] = (ve_idx, ls[0].strip(), ls[1].strip(), np.array(ls[2:], dtype=np.float64))
+        # Save the isotope in order to keep track of ve_idx
+        old = ls[0].strip()
+
         if i%chunksize == 0:
             tab.append(rows)
             rows = np.empty(chunksize, dtype=dt)
+
     if i%chunksize != 0:
         tab.append(rows[:j+1])
 
     h5f.close()
     f.close()
+
+def photon_source_h5_to_mesh(photon_source_h5, mesh, tags):
+    """This function reads in an h5 file produced by photon_source_to_hdf5 and
+    tags the requested data to the mesh of a PyNE Mesh object. Any combinations
+    of isotopes and decay times are allowed. The photon source file is assumed
+    to be in xyz order (z changes fastest) if a stuctured mesh is supplied
+    and imesh.iterate() order if an unstructured mesh is supplied.
+
+    Parameters
+    ----------
+    photon_source_h5 : str
+        The path of the hdf5 version of the photon source file.
+    mesh : PyNE Mesh
+       The object containing the imesh instance to be tagged.
+    tags: dict
+        A dictionary were the keys are tuples with two values. The first is a
+        string denoting an isotope as it appears in the photon source file.
+        (e.g. 'h-1', 'u-235', 'TOTAL'). The second is a string denoting the 
+        decay time as it appears in the file (e.g. 'shutdown', '1 h' '3 d'. The
+        values are the requested tag names for the combination of isotope and
+        decay time. For example if one wanted tags for the photon source
+        densities from U235 at shutdown and from all isotopes at 1 hour, the
+        dictionary would be:
+
+        tags = {('u-235', 'shutdown') : 'tag1', ('TOTAL', '1, h') : 'tag2'}
+    """
+    
+    if mesh.structured:
+        ves = list(mesh.structured_iterate_hex("xyz"))
+    else:
+        ves = list(mesh.imesh.iterate(iBase.Type.region, iMesh.Topology.all))
+
+    with tb.openFile(photon_source_h5) as h5f:
+         data = h5f.root.data[:]
+
+    # find number of energy groups
+    num_e_groups = len(data[0]) - 3
+
+    # create a dict of tag handles for all keys of the tags dict
+    tag_handles ={}
+    for tag_name in tags.values():
+        tag_handles[tag_name] = mesh.imesh.createTag(tag_name, num_e_groups, float)
+
+    for row in data:
+       checklist = tags.keys()
+       if (row[0], row[1]) in tags.keys():
+           pass 
+
+
+
+    
