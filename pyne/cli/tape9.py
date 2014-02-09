a34efabcce78cc5fedb295ff571bdea08ba00a9c
Anthony Scopatz <scopatz@gmail.com>
2014-02-09 23:59:06 +0200

    init'd cli sub-package

---
+++ pyne/cli/tape9.py
@@ -0,0 +1,13 @@
+"""This is a command line interface for manipulating ORIGEN v2.2 TAPE9.INP files.
+"""
+import argparser
+
+
+from pyne.origen22 import write_tape9
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
