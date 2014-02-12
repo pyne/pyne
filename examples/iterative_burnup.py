b52e6beca6dde52d63e13218fa34a1df45902ba2
Elliott Biondo <elliott.biondo@gmail.com>
2014-02-11 22:48:44 -0600

    Progress on iterative burnup script

--- examples/iterative_burnup.py
+++ examples/iterative_burnup.py
@@ -1,26 +1,28 @@
 """Top level wrapper script for iterative burnup calculation.
 """
 from itaps import iMesh
+import subprocess
 
 from pyne.material import Material
 from pyne.mesh import Mesh
 from pyne.mcnp import Meshtal, Meshtally, mesh_to_geom
 from pyne.alara import mesh_to_fluxin, mesh_to_geom, num_density_to_mesh
 
-def burnup(mesh, irr_time, count)
+mcnp_data_cards = ("               ")
+
+def burnup(mesh, irr_time, power, count)
 
     geom = mcnp.mesh_to_geom(mesh, 
            title_card="Input for iteration {0}".format(count)
 
-    data_cards = data_cards
-
     with open("MCNP_inp_{0}".count, 'w') as f:
-        f.write(geom + data_cards)
+        f.write(geom + mcnp_data_cards)
 
     run_mcnp()
 
     meshtal = Meshtal("meshtal_{0}".format(count))
-    mesh_to_flux(meshtal, "n_flux", "fluxin_{0}".format(count), reverse=False)
+    normalize_to_power(meshtal, power)
+    mesh_to_fluxin(meshtal, "n_flux", "fluxin_{0}".format(count), reverse=False)
     alara.mesh_to_geom(mesh, "alara_geom_{0}".format(count), 
                        "alara_matlib_{0}".format(count))
     irr_blocks = irradiation_blocks("alara_matlib_{0}".format(count), "isolib", 
@@ -30,17 +32,33 @@ def burnup(mesh, irr_time, count)
     with open("alara_geom_{0}".format(count), 'a') as f:
         f.write(irr_blocks)
 
-    run_alara(NEED TO PIPE TO A FILE)
+    alara_out = subprocess.check_output("alara alara_geom_{0}".format(count))
+
     num_density_to_mesh("alara_out_{0}".format(count), shutdown, mesh)
 
     return mesh
+
+def normalize_to_power(mesh, power)
+    # calculate fission energy per source
+    e_per_s = 0
+    ves = mesh.structured_iterate_hex('xyz')
+    tag = mesh.mesh.getTagHandle("n_flux")
+    for ve in ves:
+       fluxes = tag[ve]
+       e_per_s += 
+ 
+    print("The normalization is {0}".format(norm))   
+
+def gen_reactor():
+
+
 
     
 
 def main( arguments = None ) :
 
     #Instantiate option parser
-    parser = OptionParser(usage='%prog [options]')
+    parser = OptionParser(usage='%prog <x_dim>  [options]')
 
     # 
     #parser.add_option('-o', dest='mesh_output', default='flux_mesh.h5m',\
@@ -48,9 +66,10 @@ def main( arguments = None ) :
 
     (opts, args) = parser.parse_args(arguments)
 
+
     step = 0
     while step < num_steps
-        mesh = burnup(mesh, irr_time, count)
+        mesh = burnup(mesh, irr_time, power, count)
         step += 1
 
 if __name__ == __main__:
