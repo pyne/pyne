d5a0232785035fac094bc3bfe7e3c2c51cec33f9
Elliott Biondo <elliott.biondo@gmail.com>
2014-02-13 16:50:05 -0600

    Iterative burnup script nearly done, needs mods to alara mesh_to_geom. Lots of testing to be done.

--- examples/iterative_burnup.py
+++ examples/iterative_burnup.py
@@ -2,75 +2,142 @@
 """
 from itaps import iMesh
 import subprocess
+from optparse import OptionParser
 
-from pyne.material import Material
+from pyne.material import Material, from_atom_frac
 from pyne.mesh import Mesh
-from pyne.mcnp import Meshtal, Meshtally, mesh_to_geom
-from pyne.alara import mesh_to_fluxin, mesh_to_geom, num_density_to_mesh
+from pyne import mcnp
+from pyne import alara
 
-mcnp_data_cards = ("               ")
+J_PER_MEV = 1.602177E-22
 
-def burnup(mesh, irr_time, power, count)
+mcnp_data_cards = (
+"""MODE:n
+IMP:n 1 999R 0
+KCODE 1000 0.8 0 10
+KSRC 1E-9 0 0
+FMESH4:n origin=-5,-5,-5
+      imesh = 5
+      iints = 10
+      jmesh = 5
+      jints = 10
+      kmesh = 5
+      kints = 10
+      $ lowest energy bin commented out, MCNP uses 0
+      emesh=  $ 1.00000E-11
+                 5.00000E-09 1.00000E-08 1.50000E-08 2.00000E-08
+                 2.50000E-08 3.00000E-08 3.50000E-08 4.20000E-08
+                 5.00000E-08 5.80000E-08 6.70000E-08 8.00000E-08
+                 1.00000E-07 1.52000E-07 2.51000E-07 4.14000E-07
+                 6.83000E-07 1.12500E-06 1.85500E-06 3.05900E-06
+                 5.04300E-06 8.31500E-06 1.37100E-05 2.26000E-05
+                 3.72700E-05 6.14400E-05 1.01300E-04 1.67000E-04
+                 2.75400E-04 4.54000E-04 7.48500E-04 1.23400E-03
+                 2.03500E-03 2.40400E-03 2.84000E-03 3.35500E-03
+                 5.53100E-03 9.11900E-03 1.50300E-02 1.98900E-02
+                 2.55400E-02 4.08700E-02 6.73800E-02 1.11100E-01
+                 1.83200E-01 3.02000E-01 3.88700E-01 4.97900E-01
+                 6.39279E-01 8.20850E-01 1.10803E+00 1.35335E+00
+                 1.73774E+00 2.23130E+00 2.86505E+00 3.67879E+00
+                 4.96585E+00 6.06500E+00 1.00000E+01 1.49182E+01
+                 1.69046E+01 2.00000E+01 2.50000E+01
+FMESH14:n origin=-5,-5,-5
+      imesh = 5
+      iints = 1
+      jmesh = 5
+      jints = 1
+      kmesh = 5
+      kints = 1
+FM14 -6 -8
+print
 
+""")
+
+def burnup(mesh, irr_time, power, count):
+
+    # create m
     geom = mcnp.mesh_to_geom(mesh, 
-           title_card="Input for iteration {0}".format(count)
+           title_card="Input for iteration {0}".format(count))
 
-    with open("MCNP_inp_{0}".count, 'w') as f:
+    with open("MCNP_inp_{0}".format(count), 'w') as f:
         f.write(geom + mcnp_data_cards)
 
-    run_mcnp()
+    # run mcnp
+    subprocess.call("mcnp5 i=MCNP_inp_{0} meshtal=meshtal_{0}".format(count), shell=True)
 
-    meshtal = Meshtal("meshtal_{0}".format(count))
+    # use mcnp out to create alara input files
+    meshtal = mcnp.Meshtal("meshtal_{0}".format(count))
     normalize_to_power(meshtal, power)
-    mesh_to_fluxin(meshtal, "n_flux", "fluxin_{0}".format(count), reverse=False)
+    alara.mesh_to_fluxin(meshtal.tally[4], "n_result", "fluxin_{0}".format(count), reverse=False)
     alara.mesh_to_geom(mesh, "alara_geom_{0}".format(count), 
                        "alara_matlib_{0}".format(count))
-    irr_blocks = irradiation_blocks("alara_matlib_{0}".format(count), "isolib", 
+    irr_blocks = alara.irradiation_blocks("alara_matlib_{0}".format(count), "isolib", 
                    "FEINDlib CINDER CINDER90 THERMAL", ["0 s"], 
                    "fluxin_{0}".format(count), irr_time)
 
     with open("alara_geom_{0}".format(count), 'a') as f:
         f.write(irr_blocks)
 
-    alara_out = subprocess.check_output("alara alara_geom_{0}".format(count))
-
-    num_density_to_mesh("alara_out_{0}".format(count), shutdown, mesh)
+    # run alara
+    p = subprocess.Popen(["alara", "alara_geom_{0}".format(count)], stdout=subprocess.PIPE)
+    alara_out, err = p.communicate()
+    # tag transmuted materials back to mesh
+    print alara_out
+    alara.num_density_to_mesh(alara_out.split('\n'), "shutdown", mesh)
 
     return mesh
 
-def normalize_to_power(mesh, power)
+def normalize_to_power(meshtal, power):
     # calculate fission energy per source
-    e_per_s = 0
-    ves = mesh.structured_iterate_hex('xyz')
-    tag = mesh.mesh.getTagHandle("n_flux")
+    flux_mesh = meshtal.tally[4]
+    e_mesh = meshtal.tally[14]
+
+    e = e_mesh.mesh.getTagHandle("n_result")[list(e_mesh.structured_iterate_hex())[0]]
+    norm = power/J_PER_MEV/e
+    ves = flux_mesh.structured_iterate_hex('xyz')
+    tag = flux_mesh.mesh.getTagHandle("n_result")
     for ve in ves:
-       fluxes = tag[ve]
-       e_per_s += 
+       tag[ve] = [x*norm for x in tag[ve]]
  
-    print("The normalization is {0}".format(norm))   
-
-def gen_reactor():
+    print("The normalization is {0} [n/s]".format(norm))   
 
 
+def gen_reactor(mesh, fuel, fuel_idx, mod):
 
-    
+    ves = mesh.structured_iterate_hex(mesh.structured_ordering)
+    for i, ve in enumerate(ves):
+       if i in fuel_idx:
+           mesh.mats[i] = fuel
+       else:
+           mesh.mats[i] = mod 
 
-def main( arguments = None ) :
+def main(arguments=None):
 
     #Instantiate option parser
-    parser = OptionParser(usage='%prog <x_dim>  [options]')
+    parser = OptionParser(usage='%prog <power> <time> <num_steps> [options]')
 
     # 
     #parser.add_option('-o', dest='mesh_output', default='flux_mesh.h5m',\
     #                  help = 'Name of mesh output file, default=%default')
 
     (opts, args) = parser.parse_args(arguments)
+    power = float(args[0])
+    time = float(args[1])
+    num_steps = int(args[2])
+    irr_time = str(time/num_steps) + ' s'
 
+    # create reactor that spans [[-5, 5], [-5, 5],[-5, 5]] with fuel volume
+    # elements near the center
+    fuel = from_atom_frac({'U235': 0.045, 'U238': 0.955, 'O16': 2.0}, density=10.7)
+    mod = from_atom_frac({'H1': 2.0, 'O16': 1.0}, density=1.0)
+    fuel_idx = range(399, 500)
+    coords = range(-5, 6)
+    mesh = Mesh(structured_coords = [coords, coords, coords], structured=True)
+    gen_reactor(mesh, fuel, fuel_idx, mod)
 
     step = 0
-    while step < num_steps
-        mesh = burnup(mesh, irr_time, power, count)
+    while step < num_steps:
+        mesh = burnup(mesh, irr_time, power, step)
         step += 1
 
-if __name__ == __main__:
-    main()
+main()
