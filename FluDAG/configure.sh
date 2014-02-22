f0bdd38836d1b75c8ca961454a7d066cb7be666f
Julie C. Zachman <zachman@engr.wisc.edu>
2014-02-21 20:11:01 -0600

    Undo merge mistake; fix config vars

--- FluDAG/configure.sh
+++ FluDAG/configure.sh
@@ -8,6 +8,6 @@ rm -rf CMakeCache.txt
 
 cmake \
 -D MOAB_HOME=$HOME/dagmc_bld/MOAB \
--D DAGMC_FLUDAG_SOURCE=$HOME/DAGMC/FluDAG/src \
+-D FLUDAG_SOURCE=$HOME/DAGMC/FluDAG/src \
 $EXTRA_ARGS \
 ../src
--- FluDAG/src/test/configure.sh
+++ FluDAG/src/test/configure.sh
@@ -7,7 +7,7 @@ EXTRA_ARGS=$@
 rm -rf CMakeCache.txt
 
 cmake \
--D DAGMC_FLUDAG_SOURCE=$HOME/DAGMC/FluDAG/src/ \
+-D FLUDAG_SOURCE=$HOME/DAGMC/FluDAG/src/ \
 -D MOAB_HOME=$HOME/dagmc_bld/MOAB   \
 -D GTEST_HOME=$HOME/DAGMC/gtest \
 $EXTRA_ARGS \
