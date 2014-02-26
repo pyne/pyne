421628214b802d2deaef6f1c761068b659e14e1f
py1sl <cccstevelilley@gmail.com>
2014-02-26 05:30:47 -0800

    added rectangular smoothing function

--- pyne/spectanalysis.py
+++ pyne/spectanalysis.py
@@ -22,3 +22,49 @@ class PhSpectrum(object):
         self.spec_name = spec_name
         self.start_chan_num = start_chan_num
         self.num_channels = num_channels
+
+def rect_smooth(spectrum, m):
+    """Rectangular smoothing function.
+
+    Parameters
+    ---------- 
+    spectrum: str 
+    a spectrum object,
+    m : int 
+    the smoothing width, must be an odd integer more than 3
+    
+    Returns
+    -------
+    smooth_spect: a spectrum object
+
+    """
+    if(m < 3):
+        raise ValueError('Error:Smoothing width less than 3')
+    if(m % 2 == 0):
+        raise ValueError('Error:Smoothing width not odd')
+
+    smooth_spec = spectrum
+    smooth_spec.counts = [] #reset counts
+    ext = (m - 1) / 2
+    i = 0
+    
+    # 3 stages of loops a small one at start and end to deal 
+    # with end cases and a main one for bulk of spectrum
+    while i < ext:
+        smooth_spec.counts.append(0)
+        i = i + 1
+    i = ext
+    while i < (len(spectrum.counts) - ext):
+        j = i - ext
+        sum_m = 0
+        while j <= (i + ext):
+            sum_m = sum_m + spectrum.counts[j]
+            j = j + 1
+        smooth_spec.counts.append(sum_m/m)
+        i = i + 1
+    while len(smooth_spec.counts) < (len(spectrum.counts)):
+        smooth_spec.counts.append(0)
+	
+    smooth_spec.spec_name=spectrum.spec_name +' smoothed'
+    return smooth_spec
+
