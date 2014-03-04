53a4330a99e83eb89747c1108ba483258348b8cb
opotowsky <arrielle.opotowsky@gmail.com>
2014-03-04 12:40:34 -0600

    parsing functionworks now; re-named several variables to reduce redundancies

--- pyne/dbgen/q_val.py
+++ pyne/dbgen/q_val.py
@@ -4,7 +4,6 @@ Module allows the grabbing of q_values (energy per disintegration) for the calcu
 
 from __future__ import print_function
 import csv
-import re
 
 import numpy as np
 import tables as tb
@@ -12,45 +11,53 @@ import tables as tb
 from pyne import nucname
 
 # Parses data from .csv
-def grab_q_value_data(location = 'q_val_actinides.csv'):
+def grab_q_values(location = 'q_val_actinides.csv'):
     """Parses data from three q_val csv files."""
 
-    # create dictionary
-    all_q_vals = {}
+    # create list
+    all_q_vals = []
     
     # grabs data row by row
     def read_row(row):
-        if re.match('Nuclide', row[0]):
-            pass
-        nuclide = nucname.id(join(row[0, 1]))
-        q_val = row[2]
-        gamma_frac = row[3]
-        all_q_vals.append(nuclide, q_val, gamma_frac)
+        if row[0] == 'Nuclide':
+            return
+        nuclide = nucname.id(''.join(row[0:2]).replace(' ', ''))
+        if len(row[2]) == 0:
+            q_val = 0.0
+        else:
+            q_val = float(row[2])
+        if len(row[3]) == 0:
+            gamma_frac = 0.0
+        else:   
+            gamma_frac = float(row[3])
+        entry = [nuclide, q_val, gamma_frac]
+        all_q_vals.append(entry)
     
     # opens .csv files and parses them
     with open(location, 'r') as f:
         reader = csv.reader(f)
         for row in reader:
             read_row(row)
-        
+            
+    return all_q_vals        
 
 #def make_q_value_table(q_data):
 
 
-def make_q_val():
+def make_q_value():
     """Controller function for adding q-values"""
-    q_data = args.q_data
-    if os.path.exists(q_data):
-        with tb.openFile(q_data, 'r') as f:
-            if '/q_val_data' in f:
-                print("skipping q_value data table creation; already exists.")
+    q_values = args.q_values
+    if os.path.exists(q_values):
+        with tb.openFile(q_values, 'r') as f:
+            if '/q_values' in f:
+                print("skipping q_value table creation; already exists.")
                 return
 
-    # Grab the q_value data
-    print("Grabbing q_value data...")
-    grab_q_value_data(os.path.join(os.path.split(__file__)[0], 
+    # Grab the q_values
+    print("Grabbing q_values...")
+    grab_q_values(os.path.join(os.path.split(__file__)[0], 
                               'q_val_actinides.csv')) 
 
     # Make q_value table once we have the array
 #    print("Making q_value table...")
-#    make_q_value_table(q_data)
+#    make_q_value_table(q_values)
