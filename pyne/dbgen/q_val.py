ac9043b53fd1559c5eb99d2bef2f761d24e407b3
opotowsky <arrielle.opotowsky@gmail.com>
2014-03-09 16:23:40 -0500

    first attempt at a function to make the table and add it to the nuc_data file

--- pyne/dbgen/q_val.py
+++ pyne/dbgen/q_val.py
@@ -10,15 +10,23 @@ import numpy as np
 import tables as tb
 
 from pyne import nucname
+from pyne.api import nuc_data
+#from pyne.api import BASIC_FILTERS
 
 # Parses data from .csv
 def grab_q_values(fname):
-    """Parses data from three q_val csv files."""
+    """Parses data from three q_val csv files.
     
-    # create list
-    all_q_vals = []
+    Parameters
+    ----------
+    fname : str
+        Path to q_value file.
+    """
+    
+    # Create list
+    all_q_values = []
         
-    # grabs data row by row
+    # Grabs data row by row
     def read_row(row):
         if row[0] == 'Nuclide' or len(row[0].strip()) == 0:
             return
@@ -32,20 +40,84 @@ def grab_q_values(fname):
         else:   
             gamma_frac = float(row[3])
         entry = [nuclide, q_val, gamma_frac]
-        all_q_vals.append(entry)
+        all_q_values.append(entry)
     
-    # opens .csv files and parses them
+    # Opens .csv files and parses them
     with open(fname, 'r') as f:
         reader = csv.reader(f)
         for row in reader:
             read_row(row)
-            
-    return all_q_vals        
 
-#def make_q_value_table(q_data):
+    return all_q_values         
 
+# Sorts and filters list of q_values
+def format_q_values(all_q_values):
+    """Filters the q_value data for multiple entries then sorts the nuclides.
+    
+    Parameters
+    ----------
+    all_q_values : list of lists
+        Array of q_values for all nuclides.
+    """
 
-def make_q_value():
+    distinct_all_q_values = []
+    
+    # Ensures only one entry per nuclide
+    for nuclide in all_q_values:
+        if not nuclide in distinct_all_q_values:
+            distinct_all_q_values.append(nuclide)
+    
+    # Sort in order of nuclide
+    distinct_all_q_values.sort(key=lambda nucid: nucid[0])
+ 
+    return distinct_all_q_values
+
+# Write q_value table to file
+def make_q_value_table(all_q_values, nuc_data, build_dir=""):
+    """Adds q_value table to the nuc_data.h5 library.
+
+    Parameters
+    ----------
+    all_q_values: list of lists
+        Array of q_values for all nuclides.
+    nuc_data : str
+        Path to nuclide data file.
+    build_dir : str
+        Directory to place q_value files in.
+    """
+
+    # Sort and filter the q_values
+    distinct_all_q_values = format_q_values(all_q_values)
+    
+    # Iterate over all nuclides in the q_value table
+    for row in distinct_all_q_values:
+
+        row = (nuclide['Nuclide'],
+               nuclide['Q_value [MeV per disintegration]'],
+               nuclide['Fraction of Q that comes from gammas'])
+
+        distinct_all_q_values.append(row)
+
+    # Converts to numpy array
+    q_value_array = np.array(distinct_all_q_values)
+
+    # Open the hdf5 file and create group
+    nuc_file = tb.openFile(nuc_data, 'a', filters=BASIC_FILTERS)
+    q_val_group = nuc_file.createGroup('/', 'q_values', 'Q_values for nuclides')
+
+    # Make a new table
+    q_value_table = nuc_file.createTable(q_val_group, np.empty(0), 
+                    'Nuclide, Q_value [MeV per disintegration],' 
+                    'Fraction of Q that comes from gammas')
+    q_value_table.append(q_value_array)
+
+    # Ensure that data was written to table
+    q_value_table.flush()
+
+    # Close the hdf5 file
+    q_values.close()
+
+def make_q_value(args):
     """Controller function for adding q-values"""
 #    q_values = args.q_values
 #    if os.path.exists(q_values):
@@ -54,13 +126,16 @@ def make_q_value():
 #                print("skipping q_value table creation; already exists.")
 #                return
 
+    nuc_data, build_dir = args.nuc_data, args.build_dir
+    
     # Grab the q_values
-    print("Grabbing q_values...")
+    print('Grabbing q_values...')
     q_value_files = ['q_val_actinides.csv', 'q_val_fissionproducts.csv', 
                      'q_val_light.csv']
+    all_q_values = []
     for fname in q_value_files:
-        grab_q_values(fname) 
-
-    # Make q_value table once we have the array
-#    print("Making q_value table...")
-#    make_q_value_table(q_values)
+        all_q_values += grab_q_values(fname)
+    
+    # Make the q_value table and write to file
+    print("Making q_value table...")
+    make_q_value_table(all_q_values, nuc_data, build_dir))     
