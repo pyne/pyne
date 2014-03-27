43fb9577c2adda684f710af0e4d4b150b541c7a0
Cameron Bates <cameron.r.bates@gmail.com>
2014-03-26 19:51:59 -0700

    remove unused imports

--- pyne/utils.py
+++ pyne/utils.py
@@ -1,8 +1,6 @@
 import os
 from distutils.dir_util import remove_tree
 
-from _utils import fromstring_split, fromstring_token, endftod, fromendf_tok
-
 time_conv_dict = {
           'as': 1e-18, 
 		  'attosec': 1e-18, 
