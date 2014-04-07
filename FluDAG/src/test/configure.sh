68eeb0aea7b64904ddd8fb9331c704db2a6607ce
Julie C. Zachman <zachman@engr.wisc.edu>
2014-04-07 17:09:11 -0500

    Updates per review

--- FluDAG/src/test/configure.sh
+++ FluDAG/src/test/configure.sh
@@ -6,4 +6,9 @@ EXTRA_ARGS=$@
 
 rm -rf CMakeCache.txt
 
-cmake -D MOAB_HOME=$HOME/dagmc_bld/MOAB  $EXTRA_ARGS  ..
+cmake \
+-D FLUDAG_SOURCE=$HOME/DAGMC/FluDAG/src/ \
+-D MOAB_HOME=$HOME/dagmc_bld/MOAB   \
+-D GTEST_HOME=$HOME/DAGMC/gtest \
+$EXTRA_ARGS \
+..
