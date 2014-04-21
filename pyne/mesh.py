aedc5e035ae5c23e546119bf83a706aa832f4b19
Joshua Howland <howland_josh@yahoo.com>
2014-04-20 21:25:18 -0700

    Fixed future import location accidental move

--- pyne/mesh.py
+++ pyne/mesh.py
@@ -1,7 +1,7 @@
+from __future__ import print_function, division
 import copy
 import itertools
 from collections import Iterable, Sequence
-from __future__ import print_function, division
 
 import numpy as np
 import tables as tb
