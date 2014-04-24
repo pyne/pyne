c556d4ff2bd171e7c8a0d54d01d06ab6e0d84f4d
rachelslaybaugh <r.slaybaugh@gmail.com>
2014-04-24 11:48:36 -0700

    fixed more grammar errors in enrichment description.

--- pyne/enrichment.pyx
+++ pyne/enrichment.pyx
@@ -1,9 +1,9 @@
 """The enrichment module contains tools for defining and manipulating 
 enrichment cascades.  The Cascade class is a simple container for storing 
-parameters that define and enrichment setup.  These include feed, product, 
+parameters that define an enrichment setup.  These include feed, product, 
 and tail materials, target enrichments, and separation factors.  The main 
-functions in this modules computes the total flow rate and separation factors
-from an initial cascade.  Other helper function compute relative flow rates 
+functions in this module compute the total flow rate and separation factors
+from an initial cascade.  Other helper functions compute relative flow rates 
 and nuclide-specific separation factors.
 """
 # Cython imports
