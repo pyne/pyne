1bed332d5b5351e51727fc75244646fbcf96df40
Andrew Davis <davisa@engr.wisc.edu>
2014-05-02 17:00:04 -0500

    added tests regarding when particles are defined as being inside, on and outside the surface/volume

--- Geant4/dagsolid/test/DagSolid_test.cpp
+++ Geant4/dagsolid/test/DagSolid_test.cpp
@@ -10,21 +10,37 @@
 class DagSolidTest : public ::testing::Test
 {
   protected:
+
+  virtual void SetUp()
+  {
+    DagMC* dagmc = DagMC::instance(); // create dag instance
+    
+    // dag_volumes
+    const char* h5mfilename = "test_geom.h5m";
+    dagmc->load_file(h5mfilename,0);
+    dagmc->init_OBBTree();
+    
+    // new volume
+    vol_1 = new DagSolid("vol_1",dagmc,1);
+  }
+
+  protected:
+  
+  DagSolid* vol_1;
+
 };
+
+/*
+ * Empty common setup function
+ */
+TEST_F(DagSolidTest,SetUp) {
+
+}
 
 /*
  * Point in volume test
  */
 TEST_F(DagSolidTest,point_in_volume) {
-  DagMC* dagmc = DagMC::instance(); // create dag instance
-
-  // dag_volumes
-  const char* h5mfilename = "/data/opt/DAGMC/Geant4/test/geometry/test_geom.h5m";
-  dagmc->load_file(h5mfilename,0);
-  dagmc->init_OBBTree();
-
-  // new volume
-  DagSolid* vol_1 = new DagSolid("vol_1",dagmc,1);
   // sample position
   G4ThreeVector position = G4ThreeVector(0.,0.,0.);
   // point in volume test
@@ -35,19 +51,12 @@ TEST_F(DagSolidTest,point_in_volume) {
   return;
 }
 
+
 /*
  * Point on surface
  */
 TEST_F(DagSolidTest,point_on_surface) {
-  DagMC* dagmc = DagMC::instance(); // create dag instance
 
-  // dag_volumes
-  const char* h5mfilename = "/data/opt/DAGMC/Geant4/test/geometry/test_geom.h5m";
-  dagmc->load_file(h5mfilename,0);
-  dagmc->init_OBBTree();
-
-  // new volume
-  DagSolid* vol_1 = new DagSolid("vol_1",dagmc,1);
   // sample position
   G4ThreeVector position = G4ThreeVector(50.,0.,0.);
   // point in volume test
@@ -59,18 +68,61 @@ TEST_F(DagSolidTest,point_on_surface) {
 }
 
 /*
+ * Point on surface tolerance, kCarTolerance/2.0 = 5.0e-9 
+ * of a surface should return outside
+ */
+TEST_F(DagSolidTest,point_out_outside_tolerance) {
+
+  // sample position
+  G4ThreeVector position = G4ThreeVector(50.+(1.0e-8/2.0),0.,0.);
+  // point in volume test
+  EInside inside = vol_1->Inside(position);
+
+  EXPECT_EQ(kOutside,inside);
+
+  return;
+}
+
+/*
+ * Point on surface tolerance, kCarTolerance/2.0 = 5.0e-9 
+ * of a surface should return outside
+ */
+TEST_F(DagSolidTest,point_out_surface_tolerance) {
+
+  // sample position
+  G4ThreeVector position = G4ThreeVector(50.+(9.99e-9/2.0),0.,0.);
+  // point in volume test
+  EInside inside = vol_1->Inside(position);
+
+  EXPECT_EQ(kSurface,inside);
+
+  return;
+}
+
+
+
+/*
+ * Point on surface tolerance, within kCarTolerance/2.0 = 5.0e-9 
+ * of a surface should return on Surface
+ */
+TEST_F(DagSolidTest,point_on_surface_tolerance) {
+
+  // sample position
+  G4ThreeVector position = G4ThreeVector(50.-(9.999e-9/2.0),0.,0.);
+  // point in volume test
+  EInside inside = vol_1->Inside(position);
+
+  EXPECT_EQ(kSurface,inside);
+
+  return;
+}
+
+
+/*
  * Point on out of volume
  */
 TEST_F(DagSolidTest,point_outside_volume) {
-  DagMC* dagmc = DagMC::instance(); // create dag instance
 
-  // dag_volumes
-  const char* h5mfilename = "/data/opt/DAGMC/Geant4/test/geometry/test_geom.h5m";
-  dagmc->load_file(h5mfilename,0);
-  dagmc->init_OBBTree();
-
-  // new volume
-  DagSolid* vol_1 = new DagSolid("vol_1",dagmc,1);
   // sample position
   G4ThreeVector position = G4ThreeVector(51.,0.,0.);
   // point in volume test
@@ -88,13 +140,6 @@ TEST_F(DagSolidTest,point_outside_volume) {
  * only for points external to a volume
  */
 TEST_F(DagSolidTest,test_2) {
-  DagMC* dagmc = DagMC::instance(); // create dag instance
-  const char* h5mfilename = "/data/opt/DAGMC/Geant4/test/geometry/test_geom.h5m";
-  dagmc->load_file(h5mfilename,0);
-  dagmc->init_OBBTree();
-
-  // new volume
-  DagSolid* vol_1 = new DagSolid("vol_1",dagmc,1);
 
   G4ThreeVector position = G4ThreeVector(-100.,0.,0.);
   double distance = vol_1->DistanceToIn(position);
@@ -110,18 +155,10 @@ TEST_F(DagSolidTest,test_2) {
  * ray fire test, distance to out
  */
 TEST_F(DagSolidTest,test_3) {
-  DagMC* dagmc = DagMC::instance(); // create dag instance
-  const char* h5mfilename = "/data/opt/DAGMC/Geant4/test/geometry/test_geom.h5m";
-  dagmc->load_file(h5mfilename,0);
-  dagmc->init_OBBTree();
-
-  // new volume
-  DagSolid* vol_1 = new DagSolid("vol_1",dagmc,1);
 
   G4ThreeVector position = G4ThreeVector(-100.,0.,0.);
   G4ThreeVector direction = G4ThreeVector(1.,0.,0.);
   double distance = vol_1->DistanceToOut(position,direction);
-  std::cout << distance << std::endl;
 
   // DistanceToOut normally only called from inside the volume
   // but in this case, we expect the ray to skip over the volume 
@@ -139,13 +176,6 @@ TEST_F(DagSolidTest,test_3) {
  *  when point is inside of volume
  */
 TEST_F(DagSolidTest,test_4) {
-  DagMC* dagmc = DagMC::instance(); // create dag instance
-  const char* h5mfilename = "/data/opt/DAGMC/Geant4/test/geometry/test_geom.h5m";
-  dagmc->load_file(h5mfilename,0);
-  dagmc->init_OBBTree();
-
-  // new volume
-  DagSolid* vol_1 = new DagSolid("vol_1",dagmc,1);
 
   G4ThreeVector position = G4ThreeVector(0.,0.,0.);
   double distance = vol_1->DistanceToOut(position);
@@ -162,13 +192,6 @@ TEST_F(DagSolidTest,test_4) {
  * points inside of a volume
  */
 TEST_F(DagSolidTest,test_5) {
-  DagMC* dagmc = DagMC::instance(); // create dag instance
-  const char* h5mfilename = "/data/opt/DAGMC/Geant4/test/geometry/test_geom.h5m";
-  dagmc->load_file(h5mfilename,0);
-  dagmc->init_OBBTree();
-
-  // new volume
-  DagSolid* vol_1 = new DagSolid("vol_1",dagmc,1);
 
   G4ThreeVector position = G4ThreeVector(0.,0.,0.);
   G4ThreeVector direction = G4ThreeVector(1.,0.,0.);
@@ -186,13 +209,6 @@ TEST_F(DagSolidTest,test_5) {
  * points inside of a volume
  */
 TEST_F(DagSolidTest,test_6) {
-  DagMC* dagmc = DagMC::instance(); // create dag instance
-  const char* h5mfilename = "/data/opt/DAGMC/Geant4/test/geometry/test_geom.h5m";
-  dagmc->load_file(h5mfilename,0);
-  dagmc->init_OBBTree();
-
-  // new volume
-  DagSolid* vol_1 = new DagSolid("vol_1",dagmc,1);
 
   // point inside cell looking out
   G4ThreeVector position = G4ThreeVector(0.,0.,0.);
@@ -215,13 +231,6 @@ TEST_F(DagSolidTest,test_6) {
  * points inside of a volume, point on surface
  */
 TEST_F(DagSolidTest,test_7) {
-  DagMC* dagmc = DagMC::instance(); // create dag instance
-  const char* h5mfilename = "/data/opt/DAGMC/Geant4/test/geometry/test_geom.h5m";
-  dagmc->load_file(h5mfilename,0);
-  dagmc->init_OBBTree();
-
-  // new volume
-  DagSolid* vol_1 = new DagSolid("vol_1",dagmc,1);
 
   // point inside cell looking out
   G4ThreeVector position = G4ThreeVector(50.,0.,0.);
@@ -243,13 +252,6 @@ TEST_F(DagSolidTest,test_7) {
  * point just outside of a volume, vnorm should be false
  */
 TEST_F(DagSolidTest, test_8 ) {
-  DagMC* dagmc = DagMC::instance(); // create dag instance
-  const char* h5mfilename = "/data/opt/DAGMC/Geant4/test/geometry/test_geom.h5m";
-  dagmc->load_file(h5mfilename,0);
-  dagmc->init_OBBTree();
-
-  // new volume
-  DagSolid* vol_1 = new DagSolid("vol_1",dagmc,1);
 
   // point inside cell looking out
   G4ThreeVector position = G4ThreeVector(51.,0.,0.);
@@ -265,3 +267,29 @@ TEST_F(DagSolidTest, test_8 ) {
   EXPECT_EQ(kInfinity,distance);
   return;
 }
+
+
+/*
+ * volume_test calculates the volume of the solid, test cube is 10*10*10 cm
+ * G4 works in mm, therefore expect 10*10*10*1000 = 1e6 cubic millimetres
+ */
+TEST_F(DagSolidTest, volume_test ) {
+
+  G4double volume =  vol_1->GetCubicVolume();
+  EXPECT_EQ(1.0e6,volume);
+  return;
+}
+
+
+/*
+ * surface_area_test determines if DAGMC returns correct surface area
+ * surface area currently is set to zero
+ */
+TEST_F(DagSolidTest, surface_area_test ) {
+
+  G4double surface_area = vol_1->GetSurfaceArea();
+  std::cout << surface_area << std::endl;
+  return;
+}
+
+
