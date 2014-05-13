b08d647934ecfa12bfe94dd01e90cb8da81b2768
Julie C. Zachman <zachman@engr.wisc.edu>
2014-05-13 13:36:51 -0500

    Add temporary c++ testing facility

---
+++ cpp/tests/test_createMaterial.cpp
@@ -0,0 +1,37 @@
+#include "material.h"
+
+int main (int argc, char* argv[])
+{
+   using namespace pyne;
+
+   Material test_mat;
+
+   // Define a comp_map by hand 
+   comp_map nucvec;
+   nucvec.insert(std::pair<int,double>(10010000, 1.0)); 
+   nucvec.insert(std::pair<int,double>(80160000, 1.0));   
+   nucvec.insert(std::pair<int,double>(691690000, 1.0));   
+   nucvec.insert(std::pair<int,double>(922350000, 1.0));   
+   nucvec.insert(std::pair<int,double>(922380000, 1.0));   
+   nucvec.insert(std::pair<int,double>(942390000, 1.0));   
+   nucvec.insert(std::pair<int,double>(942410000, 1.0));   
+   nucvec.insert(std::pair<int,double>(952420000, 1.0));   
+   nucvec.insert(std::pair<int,double>(962440000, 1.0));   
+
+   // Create a basic filled Material object
+   test_mat = Material(nucvec);
+
+   // Test if can call into the pyne library
+   comp_map::iterator it;
+   for (it=test_mat.comp.begin(); it != test_mat.comp.end(); ++it)
+   {
+       std::cout << it->first << ", " << it->second << std::endl;
+   }
+   std::cout << "density = " << test_mat.density << std::endl;
+   std::cout << "mass = " << test_mat.mass << std::endl;
+   std::cout << "atoms_per_molecule = " << test_mat.atoms_per_molecule << std::endl;
+
+   // Test if can call added functionality in pyne library
+   std::string write_mcnp_ret = test_mat.write_mcnp();
+   std::cout << write_mcnp_ret << "\nTaDA!" << std::endl;
+}
