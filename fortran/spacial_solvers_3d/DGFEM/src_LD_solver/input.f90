12049e0a0e1a9c116bb9820fd03c98bb9e3b7e26
josh <howland_josh@yahoo.com>
2014-05-27 21:03:07 -0700

    DGFEM/src_LD_solver converted to gfortran

--- fortran/spacial_solvers_3d/DGFEM/src_LD_solver/input.f90
+++ fortran/spacial_solvers_3d/DGFEM/src_LD_solver/input.f90
@@ -90,7 +90,7 @@ READ(7,104) phi_file
 INQUIRE(FILE = mtfile, EXIST = ex4)
 INQUIRE(FILE = xsfile, EXIST = ex1)
 INQUIRE(FILE = srcfile, EXIST = ex2)
-IF (ex1 == .FALSE. .OR. ex2 == .FALSE. .OR. ex4 == .FALSE.) THEN
+IF (ex1 .eqv. .FALSE. .OR. ex2 .eqv. .FALSE. .OR. ex4 .eqv. .FALSE.) THEN
    WRITE(8,'(/,3x,A)') "ERROR: File does not exist for reading."
    STOP
 END IF
@@ -126,7 +126,7 @@ CALL readmt(mtfile)
 ALLOCATE(ang(apo,3), w(apo))
 IF (qdtyp == 2) THEN
    INQUIRE(FILE=qdfile, EXIST=ex3)
-   IF (qdfile == '        ' .OR. ex3 == .FALSE.) THEN
+   IF (qdfile == '        ' .OR. ex3 .eqv. .FALSE.) THEN
       WRITE(8,'(/,3x,A)') "ERROR: illegal entry for the qdfile name."
       STOP
    END IF
