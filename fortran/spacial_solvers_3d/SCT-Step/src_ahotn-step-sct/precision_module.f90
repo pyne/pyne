914e7ac878793e734273f12cb4caacf88d04c98c
josh <howland_josh@yahoo.com>
2014-05-29 11:40:32 -0700

    Revert "Changed precision values in SCT-Step/precision_module.f90"
    This reverts commit c2caf4e34dff16020ec36080251db3575780705b.

--- fortran/spacial_solvers_3d/SCT-Step/src_ahotn-step-sct/precision_module.f90
+++ fortran/spacial_solvers_3d/SCT-Step/src_ahotn-step-sct/precision_module.f90
@@ -13,24 +13,24 @@ MODULE precision_module
 !                      peps         - Minimum distance of two points to be considered distinct points
 !
 INTEGER, PARAMETER :: pr = selected_real_kind(32,4000)
-REAL*8,  PARAMETER :: eps       = 1.0E-14 
-REAL*8,  PARAMETER :: peps      = 1.8E-14
+REAL(kind=pr),  PARAMETER :: eps       = 1.0E-14_pr 
+REAL(kind=pr),  PARAMETER :: peps      = 1.8E-14_pr
 !
 ! ** Useful definitions
 !
-REAL*8, PARAMETER :: zero    = 0.0 
-REAL*8, PARAMETER :: one     = 1.0 
-REAL*8, PARAMETER :: two     = 2.0
-REAL*8, PARAMETER :: three   = 3.0
-REAL*8, PARAMETER :: four    = 4.0
-REAL*8, PARAMETER :: half    = 0.5
-REAL*8, PARAMETER :: quart   = 0.25
-REAL*8, PARAMETER :: mone    = -1.0
-REAL*8, PARAMETER :: mtwo    = -2.0 
-REAL*8, PARAMETER :: ten     = 10.0
-REAL*8, PARAMETER :: hundred = 100.0
-REAL*8, PARAMETER :: large   = 100000000.0
-REAL*8, PARAMETER :: small   = 0.00000001
+REAL(kind=pr), PARAMETER :: zero    = 0.0_pr 
+REAL(kind=pr), PARAMETER :: one     = 1.0_pr 
+REAL(kind=pr), PARAMETER :: two     = 2.0_pr
+REAL(kind=pr), PARAMETER :: three   = 3.0_pr
+REAL(kind=pr), PARAMETER :: four    = 4.0_pr
+REAL(kind=pr), PARAMETER :: half    = 0.5_pr
+REAL(kind=pr), PARAMETER :: quart   = 0.25_pr
+REAL(kind=pr), PARAMETER :: mone    = -1.0_pr
+REAL(kind=pr), PARAMETER :: mtwo    = -2.0_pr 
+REAL(kind=pr), PARAMETER :: ten     = 10.0_pr
+REAL(kind=pr), PARAMETER :: hundred = 100.0_pr
+REAL(kind=pr), PARAMETER :: large   = 100000000.0_pr
+REAL(kind=pr), PARAMETER :: small   = 0.00000001_pr
 !
 END MODULE
 
