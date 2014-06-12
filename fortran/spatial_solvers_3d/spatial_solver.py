6407c41922c0ef1b9857af63664cebec83766ab4
josh <howland_josh@yahoo.com>
2014-06-12 16:04:09 -0700

    input test script now working.   Added nessesary files

---
+++ fortran/spatial_solvers_3d/spatial_solver.py
@@ -0,0 +1,43 @@
+from AHOTN.src_LN.input import input;
+
+def ahotn(inputdict):
+	print "in ahotan"
+	print inputdict['material_id']
+	try:
+		input("test title in",
+		inputdict['spatial_order'],
+		inputdict['spatial_method'],
+		inputdict['quadrature_order'],
+		inputdict['quadrature_type'],
+		inputdict['nodes_xyz'][0],
+		inputdict['nodes_xyz'][1],
+		inputdict['nodes_xyz'][2],
+		inputdict['num_groups'],
+		inputdict['num_materials'],
+		inputdict['x_cells_widths'],
+		inputdict['y_cells_widths'],
+		inputdict['z_cells_widths'],
+		inputdict['x_boundry_conditions'][0],
+		inputdict['x_boundry_conditions'][1],
+		inputdict['y_boundry_conditions'][0],
+		inputdict['y_boundry_conditions'][1],
+		inputdict['z_boundry_conditions'][0],
+		inputdict['z_boundry_conditions'][1],
+		inputdict['material_id'],
+		inputdict['quadrature_file'],
+		inputdict['xs_file'],
+		inputdict['srcfile'],
+		inputdict['converge_critical'],
+		inputdict['max_iterations'],
+		inputdict['moments_converged'],
+		inputdict['converge_tolerence'],
+		inputdict['ichk'],
+		inputdict['ichk_tolerence'],
+		inputdict['momp'],
+		inputdict['momsum'],
+		inputdict['mompt'],
+		inputdict['qdflx'])
+	except:
+		print "FUCK"
+	print "INPUT WORKED!"
+	print inputdict['qdflx']
---
+++ fortran/spatial_solvers_3d/test_script.py
@@ -0,0 +1,5 @@
+import spatial_solver
+from dictionary_populate_test import populate
+
+a = populate()
+spatial_solver.ahotn(a)
