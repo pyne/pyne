16e8385b083e2bd14a590cb752b40163eb5cc6a2
josh <howland_josh@yahoo.com>
2014-06-22 14:27:59 -0700

    moved AHOTN source directory

--- fortran/spatial_solvers_3d/dictionary_populate_test.py
+++ fortran/spatial_solvers_3d/dictionary_populate_test.py
@@ -3,7 +3,7 @@
 def populate():
 		solver_dict = {
 		'solver':'AHOTN',
-		'solver_type':'LL',
+		'solver_type':'LN',
 		'spatial_order':1,
 		'spatial_method':0,
 		'angular_quadrature_order':4,  #WORKS
