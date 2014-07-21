c030380b1dac5b3ce53aa40fb21e2e9b24d58caa
josh <howland_josh@yahoo.com>
2014-07-21 13:40:32 -0700

    Updated ahot nosetests

--- tests/ahot/test_ahot.py
+++ tests/ahot/test_ahot.py
@@ -4,19 +4,36 @@ simply run this file in python. '''
 import pyne.spatialsolver
 from dictionary_populate_test import populate, populate_with_warnings
 
-def test_dgfem_dense():
-	a = populate("DGFEM","DENSE")
-	pyne.spatialsolver.solve(a)
+def test_ahotn_ln():
+  a = populate("AHOTN","LN")
+  pyne.spatialsolver.solve(a)
 
-#ALL supported configurations without warnings
-#a = populate("AHOTN","LN")
-#a = populate("AHOTN","LL")
-#a = populate("AHOTN","NEFD")
-#a = populate("DGFEM","LD")
-#a = populate("DGFEM","DENSE")
-#a = populate("DGFEM","LAGRANGE")
-#Supported configurations with ALL POSSIBLE warnings
-#a = populate_with_warnings("AHOTN")
-#a = populate_with_warnings("DGFEM")
+#NOTE: Before multiple tests can be ran, a python callback needs to be added in the wrapped
+#solver codes so that the following tests dont begin until the prior test has terminated in
+#the fortran runtime environment.
 
-#Call solve to excecute.  Output written to file called fort.8
+#def test_ahotn_ll():
+#  a = populate("AHOTN","LL")
+#  pyne.spatialsolver.solve(a)
+
+#def test_ahotn_nefd():
+#  a = populate("AHOTN","NEFD")
+#  pyne.spatialsolver.solve(a)
+
+#def test_dgfem_ld():
+#  a = populate("DGFEM","LD")
+#  pyne.spatialsolver.solve(a)
+
+#def test_dgfem_dense():
+#  a = populate("DGFEM","DENSE")
+#  pyne.spatialsolver.solve(a)
+
+#def test_dgfem_lagrange():
+#  a = populate("DGFEM","LAGRANGE")
+#  pyne.spatialsolver.solve(a)
+
+#TODO:
+#Add tests with supported warning configurations?
+  #a = populate_with_warnings("AHOTN")
+  #a = populate_with_warnings("DGFEM")
+#Add result verifications to each solver?
