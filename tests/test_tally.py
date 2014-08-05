2d445349370e44799aa593f6a7b18f1dce3dfea6
Andrew Davis <davisa@engr.wisc.edu>
2014-08-05 16:46:28 -0500

    updating test_tally for new signatures

--- tests/test_tally.py
+++ tests/test_tally.py
@@ -39,27 +39,72 @@ def test_tally5():
     tally = Tally()
     assert_equal(tally.entity_id,-1)
 
-###############################################################################
-# tests the constructor
 def test_tally6():
-    tally = Tally("flux","photon",12,"volume","volume 12")
-    assert_equal(tally.tally_type,"flux")
+    tally = Tally()
+    assert_equal(tally.tally_name,"")
 
 def test_tally7():
-    tally = Tally("flux","photon",12,"volume","volume 12")
-    assert_equal(tally.particle_name,"photon")
+    tally = Tally()
+    assert_equal(tally.entity_size,-1.0)
 
+
+###############################################################################
+# tests the constructor
 def test_tally8():
-    tally = Tally("flux","photon",12,"volume","volume 12")
-    assert_equal(tally.entity_type,"volume")
+    tally = Tally("Flux","photon",12,"Volume","Volume 12","Photon Flux in Cell 12",35.0)
+    assert_equal(tally.tally_type,"Flux")
 
 def test_tally9():
-    tally = Tally("flux","photon",12,"volume","volume 12")
-    assert_equal(tally.entity_name,"volume 12")
+    tally = Tally("Flux","photon",12,"Volume","Volume 12","Photon Flux in Cell 12",35.0)
+    assert_equal(tally.particle_name,"photon")
 
 def test_tally10():
-    tally = Tally("flux","photon",12,"volume","volume 12")
+    tally = Tally("Flux","photon",12,"Volume","Volume 12","Photon Flux in Cell 12",35.0)
+    assert_equal(tally.entity_type,"Volume")
+
+def test_tally11():
+    tally = Tally("Flux","photon",12,"Volume","Volume 12","Photon Flux in Cell 12",35.0)
+    assert_equal(tally.entity_name,"Volume 12")
+
+def test_tally12():
+    tally = Tally("Flux","photon",12,"Volume","Volume 12","Photon Flux in Cell 12",35.0)
     assert_equal(tally.entity_id,12)
+
+def test_tally13():
+    tally = Tally("Flux","photon",12,"Volume","Volume 12","Photon Flux in Cell 12",35.0)
+    assert_equal(tally.tally_name,"Photon Flux in Cell 12")
+
+def test_tally14():
+    tally = Tally("Flux","photon",12,"Volume","Volume 12","Photon Flux in Cell 12",35.0)
+    assert_equal(tally.entity_size,35.0)
+
+################################################################################
+# tests the save abilty
+def test_tally15():
+    tally = Tally("Flux","photon",12,"Volume","Volume 12","Photon Flux in Cell 12",35.0)
+    tally.write_hdf5("test_tally.h5","tally")
+    
+    new_tally = Tally()
+    new_tally.from_hdf5("test_tally.h5","tally",0)
+    assert_equal(tally.tally_type,new_tally.tally_type)
+
+def test_tally16():
+    tally = Tally("Flux","photon",12,"Volume","Volume 12","Photon Flux in Cell 12",35.0)
+   
+    new_tally = Tally()
+    new_tally.from_hdf5("test_tally.h5","tally",0)
+
+    assert_equal(tally.particle_name,new_tally.particle_name)
+
+def test_tally17():
+    tally = Tally("Flux","photon",12,"Volume","Volume 12","Photon Flux in Cell 12",35.0)
+   
+    new_tally = Tally()
+    new_tally.from_hdf5("test_tally.h5","tally",0)
+
+    assert_equal(tally.entity_type,new_tally.entity_type)
+
+
 
 # Run as script
 #
