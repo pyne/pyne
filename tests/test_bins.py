9cb7771704b7fc6a890e67d0850b43353d2e5aa7
Anthony Scopatz <scopatz@gmail.com>
2014-08-12 10:39:21 -0700

    cleaned up cache API

--- tests/test_bins.py
+++ tests/test_bins.py
@@ -4,8 +4,8 @@ import warnings
 
 import nose 
 
-from nose.tools import assert_equal, assert_not_equal, assert_raises, raises, assert_in, \
-                       assert_true
+from nose.tools import assert_equal, assert_not_equal, assert_raises, raises, \
+    assert_in, assert_true
 
 from numpy.testing import assert_array_equal, assert_array_almost_equal
 
