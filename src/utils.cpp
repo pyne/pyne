d17a2b93832d01a5f94c204586bc22262770b8d6
Cameron Bates <cameron.r.bates@gmail.com>
2014-09-01 15:20:58 -0700

    add fast version back with ability to switch

--- src/utils.cpp
+++ src/utils.cpp
@@ -77,8 +77,69 @@ double pyne::to_dbl (std::string s) {
   return strtod( s.c_str(), NULL );
 }
 
-double pyne::endftod (char * s) {
+double pyne::endftod_cpp (char * s) {
+  // Converts string from ENDF only handles "E-less" format but is 5x faster
+  int pos, mant, exp;
+  double v, dbl_exp;
+
+  mant = exp = 0;
+  if (s[2] == '.') {
+    // Convert an ENDF float
+    if (s[9] == '+' or s[9] == '-') {
+      // All these factors of ten are from place values.
+      mant = s[8] + 10 * s[7] + 100 * s[6] + 1000 * s[5] + 10000 * s[4] + \
+             100000 * s[3] + 1000000 * s[1] - 1111111 * '0';
+      exp = s[10] - '0';
+      // Make the right power of 10.
+      dbl_exp = exp & 01? 10.: 1;
+      dbl_exp *= (exp >>= 1) & 01? 100.: 1;
+      dbl_exp *= (exp >>= 1) & 01? 1.0e4: 1;
+      dbl_exp *= (exp >>= 1) & 01? 1.0e8: 1;
+      // Adjust for powers of ten from treating mantissa as an integer.
+      dbl_exp = (s[9] == '-'? 1/dbl_exp: dbl_exp) * 1.0e-6;
+      // Get mantissa sign, apply exponent.
+      v = mant * (s[0] == '-'? -1: 1) * dbl_exp;
+    }
+    else {
+      mant = s[7] + 10 * s[6] + 100 * s[5] + 1000 * s[4] + 10000 * s[3] + \
+             100000 * s[1] - 111111 * '0';
+      exp = s[10] + 10 * s[9] - 11 * '0';
+      dbl_exp = exp & 01? 10.: 1;
+      dbl_exp *= (exp >>= 1) & 01? 100.: 1;
+      dbl_exp *= (exp >>= 1) & 01? 1.0e4: 1;
+      dbl_exp *= (exp >>= 1) & 01? 1.0e8: 1;
+      dbl_exp *= (exp >>= 1) & 01? 1.0e16: 1;
+      dbl_exp *= (exp >>= 1) & 01? 1.0e32: 1;
+      dbl_exp *= (exp >>= 1) & 01? 1.0e64: 1;
+      dbl_exp = (s[8] == '-'? 1/dbl_exp: dbl_exp) * 1.0e-5;
+      v = mant * (s[0] == '-'? -1: 1) * dbl_exp;
+    }
+  }
+
+  // Convert an ENDF int to float; we start from the last char in the field and
+  // move forward until we hit a non-digit.
+  else {
+    v = 0;
+    mant = 1; // Here we use mant for the place value about to be read in.
+    pos = 10;
+    while (s[pos] != '-' and s[pos] != '+' and s[pos] != ' ' and pos > 0) {
+      v += mant * (s[pos] - '0');
+      mant *= 10;
+      pos--;
+    }
+    v *= (s[pos] == '-'? -1: 1);
+  }
+  return v;
+}
+
+double pyne::endftod_f (char * s) {
   return endftod_(s,12);
+}
+
+double (*pyne::endftod)(char * s) = &pyne::endftod_f;
+
+void pyne::use_fast_endftod() {
+  pyne::endftod = &pyne::endftod_cpp;
 }
 
 std::string pyne::to_upper(std::string s) {
