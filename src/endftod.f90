6abc31eeb5f8303728acb7db98967203a19d8afd
Cameron Bates <cameron.r.bates@gmail.com>
2014-09-04 12:33:51 -0700

    revert to gcc 4.2.1 compatible types

--- src/endftod.f90
+++ src/endftod.f90
@@ -1,10 +1,10 @@
 !> @brief Simple Fortran function to convert ENDF-6 values to doubles.
 !! @param str 11-charachter string in ENDF format
 function endftod(str) result(val)
-    use, intrinsic :: ISO_C_BINDING
+    integer, parameter:: dp=kind(0.d0)
     integer :: reason
-    real(C_DOUBLE) :: val
-    character (kind=C_CHAR, len=11) :: str
+    real(dp) :: val
+    character (len=11) :: str
     read( str, "(E11.0)", iostat=reason) val
     if (reason > 0) then
         print *, "Something went wrong converting: ", str
