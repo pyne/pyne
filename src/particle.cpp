c3b66ee9b262e567ee0b3c9d690fdd261793dc85
Andrew Davis <davisa@engr.wisc.edu>
2014-09-17 14:29:49 -0500

    first working version of particle class

--- src/particle.cpp
+++ src/particle.cpp
@@ -25,6 +25,7 @@ std::string pyne::particle::_names[NUM_PARTICLES] = {
   "KaonZeroShort",
   "KaonZero",
   "AntiKaonZero",
+  // light baryons
   "Neutron",
   "AntiNeutron",
   "Proton",
@@ -67,7 +68,7 @@ int pyne::particle::_pdcids[NUM_PARTICLES] = {
   -311,
   // light baryons
   2112,
-  -2122,
+  -2112,
   2212,
   -2212,
   // strange Baryons
@@ -85,6 +86,9 @@ int pyne::particle::_pdcids[NUM_PARTICLES] = {
 std::set<std::string> pyne::particle::names(pyne::particle::_names,
 					    pyne::particle::_names+NUM_PARTICLES);
 
+std::set<int> pyne::particle::pdc_nums(pyne::particle::_pdcids,
+					       pyne::particle::_pdcids+NUM_PARTICLES);
+
 std::map<std::string,int> pyne::particle::altnames;
 std::map<int,std::string> pyne::particle::id_name;
 std::map<std::string,int> pyne::particle::name_id;
@@ -93,7 +97,7 @@ std::map<std::string,std::string> pyne::particle::docs;
 void * pyne::particle::_fill_maps() {
   using std::make_pair;
 
-  std::string pyne::particle::_docs[NUM_PARTICLES] = {
+  std::string _docs[NUM_PARTICLES] = {
     // leptons
     "Electron",
     "Positron",
@@ -118,9 +122,10 @@ void * pyne::particle::_fill_maps() {
     "Kaon Zero Short",
     "Kaon Zero",
     "Anti Kaon Zero",
+    // light baryons
     "Neutron",
     "Anti Neutron",
-    "Proton",
+    "I am a Proton",
     "Anti Proton",
     // strange baryons
     "Lambda",
@@ -155,23 +160,138 @@ void * pyne::particle::_fill_maps() {
   altnames["X-Ray"] = name_id["Photon"];
 };
 
+void * pyne::particle::_ = pyne::particle::_fill_maps();
+
+// is hydrogen 
+bool pyne::particle::_is_hydrogen(int s) {
+  if( s == name_id["Proton"] )
+    return true;
+  if( pyne::particle::_is_hydrogen(pyne::nucname::name(s)) )
+      return true;
+  return false;
+}
+
+bool pyne::particle::_is_hydrogen(char *s) {
+  return pyne::particle::_is_hydrogen(std::string(s));
+}
+
+bool pyne::particle::_is_hydrogen(std::string s) {
+  // check std name
+  if( name_id[s] == name_id["Proton"] )
+    return true;
+  if( altnames[s] == name_id["Proton"])
+    return true;
+  if(pyne::nucname::name(s).find("H1") != std::string::npos)
+    return true;
+  return false;
+}
+// heavy ion
+bool pyne::particle::is_heavy_ion(int s) {
+  return pyne::particle::is_heavy_ion(std::string(id_name[s]));
+};
+
+bool pyne::particle::is_heavy_ion(char *s) {
+  return pyne::particle::is_heavy_ion(std::string(s));
+};
+
+bool pyne::particle::is_heavy_ion(std::string s) {
+  if(pyne::nucname::isnuclide(s))
+    if(pyne::particle::_is_hydrogen(s))
+      return false;
+    else
+      return true;
+  return false;
+};
+
+// is valid functions
+bool pyne::particle::is_valid(int s) {
+  if(pyne::nucname::isnuclide(s))
+    return true;
+  else 
+    return pyne::particle::is_valid(std::string(id_name[s]));
+};
+
+bool pyne::particle::is_valid(char *s) {
+  return pyne::particle::is_valid(std::string(s));
+};
+
+bool pyne::particle::is_valid(std::string s) {
+  // check std name
+  if(0 < names.count(s))
+    return true;
+  // check alternative name
+  if(0 < altnames.count(s))
+    return true;
+  // check if is a heavy ion
+  if( pyne::nucname::isnuclide(s) )
+    return true;
+  else
+    return false;
+};
+
+// pdc functions
+int pyne::particle::pdc_number(int s) {
+  if ( 0 < pdc_nums.count(s))
+    return s;
+  else
+    return 0;
+}
+int pyne::particle::pdc_number(char *s) {
+  return pyne::particle::pdc_number(std::string(s));
+}
+
+int pyne::particle::pdc_number(std::string s) {
+  if(pyne::nucname::isnuclide(s))
+    {
+      if(pyne::particle::_is_hydrogen(s))
+	return name_id["Proton"];
+      if( pyne::particle::is_heavy_ion(s) )
+	return 0;
+    }
+
+  if (0 < pdc_nums.count(name_id[s]))
+    return name_id[s];
+  if (0 < pdc_nums.count(altnames[s]))
+    return altnames[s];
+  return 0;
+}
+
 // name functions
+std::string pyne::particle::name(int s) {
+  if(pyne::nucname::isnuclide(s))
+    return pyne::particle::name(pyne::nucname::name(s));
+  return pyne::particle::name(id_name[s]);
+};
+
 std::string pyne::particle::name(char *s) {
   return pyne::particle::name(std::string(s));
 };
 
 std::string pyne::particle::name(std::string s) {
+  // check if is a hydrogen
+  if(pyne::nucname::isnuclide(s))
+    {
+      if(pyne::particle::_is_hydrogen(s))
+	return "Proton";
+      if( pyne::particle::is_heavy_ion(s) )
+	return s;
+    }
   // check std name
   if(0 < names.count(s))
     return s;
   // check alternative name
   if(0 < altnames.count(s))
     return id_name[altnames[s]];
-  // check if is a heavy ion
-  if( pyne::nucname::isnuclide(s) )
-    return s
+  // check for heavy ion
   else
-    throw NotAParticle(s,"???");
+    throw NotAParticle(s);
+};
+
+// describe functions
+std::string pyne::particle::describe(int s) {
+  if(pyne::nucname::isnuclide(s))
+    return pyne::particle::describe(pyne::nucname::name(s));
+  return pyne::particle::describe(id_name[s]);
 };
 
 std::string pyne::particle::describe(char *s) {
@@ -179,42 +299,22 @@ std::string pyne::particle::describe(char *s) {
 };
 
 std::string pyne::particle::describe(std::string s) {
+  // check if is a hydrogen
+  if(pyne::nucname::isnuclide(s))
+    {
+      if(pyne::particle::_is_hydrogen(s))
+	return docs[pyne::particle::name(s)];
+      if( pyne::particle::is_heavy_ion(s) )
+	return "Is a heavy ion";
+    }
   // check std name
   if(0 < names.count(s))
     return docs[s];
   // check alternative name
   if(0 < altnames.count(s))
-    return docs[altnames[s]];
+    return docs[id_name[altnames[s]]];
   // check if is a heavy ion
-  if( pyne::nucname::isnuclide(s) )
-    return s
   else
-    throw NotAParticle(s,"???");
+    throw NotAParticle(s);
 };
-
-
-// functions
-bool pyne::particle::is_anti(int pdc_num) {
-  if ( pdc_num < 0 )
-    return true;
-  else if ( pdc_num > 0 )
-    return false;
-  else
-    throw NotAPdcNum(int pdcnum);
-}
-
-
-bool pyne::particle::is_boson(int pdc_num) {
-  if ( pdc_num >= 21 && pdc_num <= 37 )
-    return true;
-  else
-    return false;
-}
-
-bool pyne::particle::is_lepton(int pdc_num) {
-  if ( pdc_num >= 11 && pdc_num <= 18 )
-    return true;
-  else
-    return false;
-}
 
--- src/particle.h
+++ src/particle.h
@@ -1,21 +1,118 @@
-#define NUM_PARTICLES = 32
+/// \brief Converts betweeen naming/numbering conventions for particle types
 
 // defines the primary particle types that are allowed by most monte carlo codes
 // some monte carlo codes allow us to score so called "heavy ions", in fact we
 // define heavy ions to be particles with more than one neutron or proton
 
+#include <string>
+#include <map>
+#include <set>
+
+#ifndef PYNE_IS_AMALGAMATED
+  #include "nucname.h"
+#endif
+
+/// Number of pure particle types currently supported
+#define NUM_PARTICLES 32
+
 namespace pyne 
 {
 namespace particle
 {
+  extern int _pdcids[NUM_PARTICLES]; ///
+  /// set of Particle Data Centre integer id numbers
+  extern std::string _docs[NUM_PARTICLES];
+  /// set of doc strings that describe the particle types
   extern std::string _names[NUM_PARTICLES];
+  /// set of name strings that are the particle types
   extern std::set<std::string> names;
+  /// set of valid names
+  extern std::set<int> pdc_nums;
+  /// set of valid pdc numbers
   extern std::map<std::string,int> name_id;
+  /// map of name to pdc number
   extern std::map<int,std::string> id_name;
+  /// map of pdc number to name
   extern std::map<std::string,std::string> docs;
+  /// map of name to doc string
   extern std::map<std::string,int> altnames;
+  /// map of alternative name to pdc number
 
+  /// \name is_hydrogen functions
+  /// \{
+  /// Returns whether or not the given particle is hydrogen or not, for
+  /// example, Protons (Hydrogen) are both valid nucids and fundamental 
+  /// pdc types, all the following identify as hydrogen, Proton, Hydrogen,
+  /// Protium, "H1", "1H", 100001000, PDC(2212)
+  /// \param n Integer PDC number or nucid
+  /// \param s String valid particle name, altname or nucid
+  bool _is_hydrogen(int n);
+  bool _is_hydrogen(char *s);
+  bool _is_hydrogen(std::string s);
+  /// \}
 
+  /// \name is_heavy_ion functions
+  /// \{
+  /// Returns whether or not the given particle is a heavy ion
+  /// or not. Heavy ions are not covered by the PDC scheme, therefore
+  /// the pyne::nucname class is used.
+  /// \param n Integer PDC number or nucid
+  /// \param s String valid particle name, altname or nucid
+  bool is_heavy_ion(int s);
+  bool is_heavy_ion(char *s);
+  bool is_heavy_ion(std::string s);
+  ///\}
+
+  /// \name is_valid functions
+  /// \{
+  /// Returns whether or not the the given particle is a valid particle
+  /// in the PyNE particle class. All PDC numbers, names, altnames nucnames
+  /// are valid particle types
+  /// \param n Integer PDC number or nucid
+  /// \param s String valid particle name, altname or nucid
+  bool is_valid(int n);
+  bool is_valid(char *s);
+  bool is_valid(std::string s);
+  ///\}
+
+  /// \name pdc_number functions
+  /// \{
+  /// Returns the PDC number of the particle given, if a valid pdc particle, 
+  /// will return the number, for heavy ions will return 0.
+  /// \param n Integer PDC number or nucid
+  /// \param s String valid particle name, altname or nucid
+  int pdc_number(int n);
+  int pdc_number(char *s);
+  int pdc_number(std::string s);
+  ///\}
+
+  /// \name name functions
+  /// \{
+  /// Returns the pyne::particle name of the particle given, 
+  /// if a valid pdc particle number, name or nucname. Raises
+  /// exception if not a valid name
+  /// \param n Integer PDC number or nucid
+  /// \param s String valid particle name, altname or nucid
+  std::string name(int s);
+  std::string name(char *s);
+  std::string name(std::string s);
+  ///\}
+
+  /// \name describe functions
+  /// \{
+  /// Returns a long string that describes the particle, if given
+  /// a valid particle name, otherwise raises exception
+  /// \param n Integer PDC number or nucid
+  /// \param s String valid particle name, altname or nucid
+  std::string describe(int s);
+  std::string describe(char *s);
+  std::string describe(std::string s);
+
+  /// A helper function to set the contents of the variables in this library.
+  void * _fill_maps();  
+  extern void * _;  ///< A dummy variable used when calling #_fill_maps().
+
+  /// Custom excpeption for failed particle types
   class NotAParticle : public std::exception
   {
   public:
@@ -25,11 +122,14 @@ namespace particle
     /// Default destructor
     ~NotAParticle () throw () {};
 
+    /// Constructor for raising the exception
+    /// Spits out the particle name as input
     NotAParticle(std::string particle_name)
-      {
-	part_name = particle_name;
-      }
+    {
+       part_name = particle_name;
+    };
 
+    /// raises error message
     virtual const char* what() const throw()
     {
       std::string pname ("Not a valid particle name ");
@@ -39,7 +139,7 @@ namespace particle
     };
 
     private:
-      std::string part_name;  
+       std::string part_name;  /// the particle name
 
 
   };
