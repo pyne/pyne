ab095dd364538369652b599d138487c93f3de43f
Andrew Davis <davisa@engr.wisc.edu>
2014-09-24 12:04:16 -0500

    updated particle tests for mcnp/fluka ids

--- tests/test_particle.py
+++ tests/test_particle.py
@@ -10,7 +10,8 @@ from nose.tools import assert_equal, assert_not_equal, assert_raises, raises, \
 
 from pyne.utils import VnVWarning
 warnings.simplefilter("ignore", VnVWarning)
-from pyne.particle import is_valid,name,is_heavy_ion,pdc_number
+from pyne.particle import is_valid,name,is_heavy_ion,pdc_number,mcnp,mcnp6, \
+    fluka
 
 
 def test_is_valid():
@@ -26,6 +27,7 @@ def test_is_valid():
 def test_name():
     assert_equal(name("Proton"),"Proton")
     assert_equal(name("Neutron"),"Neutron")
+    assert_equal(name(2112),"Neutron")
     assert_equal(name("Hydrogen"),"Proton")
     assert_equal(name("Protium"),"Proton")
     assert_equal(name(10010000),"Proton")
@@ -48,6 +50,32 @@ def test_pdc_number():
     assert_not_equal(pdc_number("Proton"),-2212)
     assert_equal(pdc_number("Hydrogen"),2212)
     assert_equal(pdc_number("22Na"),0)
+
+def test_mcnp_id():
+    assert_equal(mcnp("Neutron"),"N")
+    assert_equal(mcnp(2112),"N")
+    assert_equal(mcnp("Photon"),"P")
+    assert_equal(mcnp("Gamma"),"P")
+    assert_equal(mcnp("Electron"),"E")
+
+def test_mcnp6_id():
+    assert_equal(mcnp6("Neutron"),"N")
+    assert_equal(mcnp6(2112),"N")
+    assert_equal(mcnp6("Photon"),"P")
+    assert_equal(mcnp6("Gamma"),"P")
+    assert_equal(mcnp6("Electron"),"E")
+    assert_equal(mcnp6("Proton"),"H")
+    assert_equal(mcnp6("Hydrogen"),"H")
+
+def test_fluka_id():
+    assert_equal(fluka("Neutron"),"NEUTRON")
+    assert_equal(fluka(2112),"NEUTRON")
+    assert_equal(fluka("Photon"),"PHOTON")
+    assert_equal(fluka("Gamma"),"PHOTON")
+    assert_equal(fluka("Electron"),"ELECTRON")
+    assert_equal(fluka("Beta-"),"ELECTRON")
+    assert_equal(fluka("Proton"),"PROTON")
+    assert_equal(fluka("Hydrogen"),"PROTON")
 
 
 # Run as script
