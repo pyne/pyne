2e4170da0f158f22e2351340200935915c12fadf
Elliott Biondo <elliott.biondo@gmail.com>
2014-11-17 00:16:44 -0600

    Adding css file.

--- docs/conf.py
+++ docs/conf.py
@@ -164,7 +164,9 @@ html_favicon = '../img/pyne_icon.ico'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
+
 html_static_path = ['_static']
+html_style = "pyne.css"
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
