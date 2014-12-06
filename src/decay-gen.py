3bafd37102d085fb7ebf94ca76abad73085f2814
Anthony Scopatz <scopatz@gmail.com>
2014-12-05 23:49:19 -0600

    massages satbility

--- src/decay-gen.py
+++ src/decay-gen.py
@@ -155,7 +155,8 @@ def k_a(chain):
     if np.isnan(dc).any():
         # NaNs are bad, mmmkay.  Nones mean we should skip
         return None, None  
-    ends_stable = (dc[-1] == 0.0)  # check if last nuclide is a stable species
+    #ends_stable = (dc[-1] == 0.0)  # check if last nuclide is a stable species
+    ends_stable = (dc[-1] < 1e-16)  # check if last nuclide is a stable species
     # compute cij -> ci in prep for k
     cij = dc[:, np.newaxis] / (dc[:, np.newaxis] - dc)
     if ends_stable:
