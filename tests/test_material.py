903252a0eb69ced3aa292bcad985c2b0a7f2bde6
opotowsky <arrielle.opotowsky@gmail.com>
2014-12-09 18:48:10 -0600

    adding unit test

--- tests/test_material.py
+++ tests/test_material.py
@@ -204,9 +204,10 @@ class TestMaterialMethods(TestCase):
     def test_decay_heat(self):
         mat = Material({922350000: 0.05, 922380000: 0.95}, 15)
         obs = mat.decay_heat()
-        exp = {922350000: 0.0, 922380000: 0.0}
+        exp = {922350000: 4.48963565256e-14, 922380000: 1.2123912039e-13}
         assert_equal(set(obs), set(exp))
-        assert_equal(set(obs.values()), set(exp.values()))
+        for key in exp:
+            assert_almost_equal(obs[key], exp[key])
 
 
     def test_molecular_mass(self):
