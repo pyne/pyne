0c7bcf12aec5dfa935b9f8881d7871c47c6c4f62
Kalin Kiesling <krkiesling@gmail.com>
2015-01-09 10:20:57 -0600

    refactored and added commenting

--- pyne/partisn.py
+++ pyne/partisn.py
@@ -51,121 +51,72 @@ if HAVE_PYTAPS:
     from pyne.mesh import Mesh, StatMesh, MeshError, IMeshTag
 
 
-#class PartisnRead(object):
-""" This class reads all necessary attributes from a material-laden 
-geometry file, a pre-made PyNE mesh object, and the nuclear data cross 
-section library, and any optional inputs that are necessary for creating a 
-PARTISN input file. Supported are 1D, 2D, and 3D geometries.
-
-Parameters
-----------
-    mesh : mesh object, a premade mesh object that conforms to the geometry. 
-        Bounds of the mesh must correspond to the desired PartiSn fine mesh. 
-        One fine mesh per coarse mesh will be created. Can be 1-D, 2-D, or 3-D.
-        Only Cartesian based geometries are currently supported.
-    hdf5 : file, a material-laden dagmc geometry file.
-    nucdata : file, nuclear data cross section library.
-                note: only BXSLIB format is currently supported.
-    nuc_names : dict, pyne element/isotope names to bxslib name assignment,
-                keys are pyne nucids (int) and values are bxslib names (str)
-    datapath : str, optional, The path in the heirarchy to the data table 
-            in an HDF5 file. (for MaterialLibrary)
-                default = material_library/materials
-    nucpath : str, optional, The path in the heirarchy to the 
-            nuclide array in an HDF5 file. (for MaterialLibrary)
-                default = material_library/nucid
-"""
-
-def read_hdf5_mesh(mesh, hdf5, nucdata, nuc_names, **kwargs):
-    dagmc.load(hdf5)
-    # optional inputs
-    datapath = kwargs['datapath'] if 'datapath' in kwargs else '/material_library/materials'
-    nucpath = kwargs['nucpath'] if 'nucpath' in kwargs else '/material_library/nucid'
+def write_partisn_input(mesh, hdf5, ngroup, isn, nmq, **kwargs):
+    """This class reads all necessary attributes from a material-laden 
+    geometry file, a pre-made PyNE mesh object, and the nuclear data cross 
+    section library, and any optional inputs that are necessary for creating a 
+    PARTISN input file. It then writes a PARTISN text input file.
     
-    # Read the materials from the hdf5 and convert to correct naming convention
-    mat_lib = _get_materials(hdf5, datapath, nucpath, nuc_names)
-    xs_names = _read_bxslib(nucdata)
-
-    return mat_lib, xs_names
-
- 
-def _read_bxslib(nucdata):
-    # read entire file
-    binary_file = _BinaryReader(nucdata, mode='rb')
-    record = binary_file.get_fortran_record()
-    #print(binary_file)
-    ##print(record.get_double())
-    #print(record.get_string(28))   
+    Notes:
+        This does not write out all necessary inputs for the solver and cross
+        section library (block 3 and 5). There is no assumed cross section 
+        library type.
+    
+    Parameters:
+    -----------
+        mesh : PyNE mesh object, a premade mesh object that conforms to the 
+            geometry. Bounds of the mesh must correspond to the desired PartiSn
+            fine mesh intervals. One fine mesh per coarse mesh will be created. 
+            Can be 1-D, 2-D, or 3-D.
+            Note: Only Cartesian meshes are currently supported.
+        hdf5 : file path, a material-laden dagmc geometry file.
+        ngroup : int, the number of energy groups in the cross section library
+        isn : int, S_n order to be used MIGHT NOT KEEP AS AN INPUT
+        nmq : int, the number of moments in a P_n expansion of the source
         
-    bxslib = open(nucdata, 'rb')
-    string = ""
-    edits = ""
-    xs_names=[]
-    # 181st byte is the start of xsnames
-    bxslib.seek(180)
-    done = False
-    while not done:
-        for i in range(0,8):
-            bytes = bxslib.read(1)
-            pad1=struct.unpack('s',bytes)[0]
-            if '\x00' in pad1:
-                done = True
-                return xs_names
-            string += pad1
-        xs_names.append(string.strip(" "))
-        string=""
+    Optional Parameters:
+    --------------------
+        data_hdf5path : str, the path in the heirarchy to the data table in an 
+            HDF5 file. (for MaterialLibrary)
+            default = material_library/materials
+        nuc_hdf5path : str, the path in the heirarchy to the nuclide array in 
+            an HDF5 file. (for MaterialLibrary)
+            default = material_library/nucid
+        names_dict : dict, pyne element/isotope names to bxslib name assignment,
+            keys are pyne nucids (int) and values are bxslib names (str)
+                Example: names_dict[250550000] ='mn55'
     
-
-def _get_materials(hdf5, datapath, nucpath, nuc_names):
-    # reads material properties from the loaded dagmc_geometry
-    
-    # set of exception nuclides for collapse_elements
-    mat_except = Set(nuc_names.keys())
-    
-    # collapse isotopes into elements
-    mats = MaterialLibrary(hdf5, datapath=datapath, nucpath=nucpath)
-    mats_collapsed = {}
-    for mat_name in mats.keys():
-        mats_collapsed[mat_name] = mats[mat_name].collapse_elements(mat_except)      
-    
-    # convert mass fraction to atom density in units [at/b-cm]
-    mat_lib = {}
-    comp_list = {}
-    for mat_name, comp in mats_collapsed.iteritems():
-        atom_dens_dict = comp.to_atom_dens()
-        for nucid, dens in atom_dens_dict.iteritems():
-            comp_list[nucid] = dens*10.**-24 # convert from [at/cc] to [at/b-cm]
-        mat_lib[mat_name] = comp_list
-
-    return mat_lib
-
-
-
-def write_partisn_input(mesh, mat_lib, ngroup, isn, xs_names, nmq, hdf5, input_file):
-    """This function writes out the necessary information to a text partisn 
-    input file.
-    
-    Parameters
-    ----------
-        coord_sys : list of str, indicator of either 1-D, 2-D, or 3-D Cartesian
-            geometry. 
-                1-D: [i]
-                2-D: [i ,j]
-                3-D: [i, j, k]
-                where i, j, and k are either "x", "y", or "z".
-        bounds : dict of list of floats, coarse mesh bounds for each dimension. 
-            Dictionary keys are the dimension "x", "y" or "z" for Cartesian. 
-            Must correspond to a 1:1 fine mesh to coarse mesh interval.
-        mat_lib : dict of dicts, keys are names of PyNE materials whose keys 
-            are bxslib names and their value is atomic density in units 
-            [at/b-cm].
-        zones : dict of dict of lists, first dict key is PartiSn zone number 
-            (int). Inner dict keys are "cell" with a list of cell numbers (int) 
-            as values and "vol_frac" with a list of corresponding cell volume 
-            fractions (float).
-        xs_names : list of str, names of isotope/elements from the bxslib
-    
+    Output:
+    -------
+        PARTISN Input file. 
+            Note: read comments generated in file. Not all variables will be 
+            assigned that are necessary.
     """
+    
+    # Load the geometry
+    dagmc.load(hdf5)
+    
+    # Read optional inputs:
+    
+    # hdf5 paths
+    if 'data_hdf5path' in kwargs:
+        data_hdf5path = kwargs['data_hdf5path']  
+    else:
+        data_hdf5path = '/material_library/materials'
+    
+    if 'nuc_hdf5path' in kwargs:
+        nuc_hdf5path = kwargs['nuc_hdf5path']
+    else:
+         nuc_hdf5path = '/material_library/nucid'
+    
+    # Dictionary of hdf5 names and cross section library names
+    # Assumes PyNE naming convention in the cross section library if no dict
+    # provided.
+    if 'names_dict' in kwargs:
+        nuc_names = kwargs['names_dict']
+    else:
+        # read a function
+        pass
     
     # Initialize dictionaries for each PARTISN block
     block01 = {}
@@ -174,14 +125,16 @@ def write_partisn_input(mesh, mat_lib, ngroup, isn, xs_names, nmq, hdf5, input_f
     block04 = {}
     block05 = {}
     
-    #############################
-    #   Set block01 variables   #
-    #############################
+    # Set input variables
     
     block01['IGEOM'], bounds = _get_coord_sys(mesh)
     block01['NGROUP'] = ngroup
     block01['ISN'] = isn
+    
+    xs_names = _get_xs_names(nuc_names)
     block01['NISO'] = len(xs_names)
+    
+    mat_lib = _get_material_lib(hdf5, data_hdf5path, nuc_hdf5path, nuc_names)
     block01['MT'] = len(mat_lib)
     
     block02['ZONES'], zones = _get_zones(mesh, hdf5, bounds)
@@ -189,32 +142,52 @@ def write_partisn_input(mesh, mat_lib, ngroup, isn, xs_names, nmq, hdf5, input_f
     
     for key in bounds.keys():
         if key == 'x':
-            block01['IM'] = len(bounds[key]) - 1
+            n = len(bounds[key]) - 1
+            block01['IM'] = n
             block01['IT'] = block01['IM']
-        elif key == 'y':
-            block01['JM'] = len(bounds[key]) - 1
-            block01['JT'] = block01['JM']
-        elif key == 'z':
-            block01['KM'] = len(bounds[key]) - 1
-            block01['KT'] = block01['KM']
-    
-    #############################
-    #   Set block02 variables   #
-    #############################
-    
-    # fine intervals are 1 by default
-    for key in bounds.keys():
-        if key == 'x':
             block02['XMESH'] = bounds[key]
             block02['XINTS'] = 1
+            block05['SOURCX'] = np.zeros(shape=(n, nmq), dtype=float)
+            block05['SOURCX'][:,0] = 1.0
         elif key == 'y':
+            n = len(bounds[key]) - 1
+            block01['JM'] = n
+            block01['JT'] = block01['JM']
             block02['YMESH'] = bounds[key]
             block02['YINTS'] = 1
+            block05['SOURCY'] = np.zeros(shape=(n, nmq), dtype=float)
+            block05['SOURCY'][:,0] = 1.0
         elif key == 'z':
+            n = len(bounds[key]) - 1
+            block01['KM'] = n
+            block01['KT'] = block01['KM']
             block02['XZMESH'] = bounds[key]
             block02['ZINTS'] = 1
+            block05['SOURCZ'] = np.zeros(shape=(n, nmq), dtype=float)
+            block05['SOURCZ'][:,0] = 1.0
+    
+    block03['NAMES'] = xs_names
+    
+    mat_xs_names = _nucid_to_xs(mat_lib, xs_names, nuc_names)
+    block04['MATLS'] = mat_xs_names
+    
+    block04['ASSIGN'] = zones
+    
+    block05['IEVT'] = 0 # default? 0 = source
+    block05['SOURCE'] = np.zeros(shape=(ngroup, nmq), dtype=float)
+    block05['SOURCE'][:,0] = 1.0
+    
+    title = _title(hdf5)
+    
+    _write_input(title, block01, block02, block03, block04, block05)
     
     
+def _get_xs_names(nuc_names):
+    xs_names = []
+    for name in nuc_names.items():
+        xs_names.append(name)
+    
+    return xs_names
 
 
 def _get_coord_sys(mesh):
@@ -250,27 +223,51 @@ def _get_coord_sys(mesh):
     return IGEOM, bounds
 
 
+def _get_material_lib(hdf5, data_hdf5path, nuc_hdf5path, nuc_names):
+    # reads material properties from the loaded dagmc_geometry
+    
+    # set of exception nuclides for collapse_elements
+    mat_except = Set(nuc_names.keys())
+    
+    # collapse isotopes into elements
+    mats = MaterialLibrary(hdf5, datapath=data_hdf5path, nucpath=nuc_hdf5path)
+    mats_collapsed = {}
+    for mat_name in mats.keys():
+        mats_collapsed[mat_name] = mats[mat_name].collapse_elements(mat_except)      
+    
+    # convert mass fraction to atom density in units [at/b-cm]
+    mat_lib = {}
+    comp_list = {}
+    for mat_name, comp in mats_collapsed.iteritems():
+        atom_dens_dict = comp.to_atom_dens()
+        for nucid, dens in atom_dens_dict.iteritems():
+            # convert from [at/cc] to [at/b-cm]
+            comp_list[nucid] = dens*10.**-24
+        mat_lib[mat_name] = comp_list
+
+    return mat_lib
 
 
 def _get_zones(mesh, hdf5, bounds):
-
-    dg = dagmc.discretize_geom(mesh)
-    mat_assigns = dagmc.materials_to_cells(hdf5)
     
-    # Create dictionary of each voxel's info    
+    # Descretize the geometry and get cell fractions
+    dg = dagmc.discretize_geom(mesh)
+    
+    # Reorganize dictionary of each voxel's info with the key the voxel number 
+    # and values of cell and volume fraction   
     voxel = {}
     for i in dg:
-        idx = i[0]
+        idx = i[0]  # voxel number
         if idx not in voxel.keys():
             voxel[idx] = {}
             voxel[idx]['cell'] = []
             voxel[idx]['vol_frac'] = []
-            #voxel[idx]['rel_error'] = []
-            
         voxel[idx]['cell'].append(i[1])
         voxel[idx]['vol_frac'].append(i[2])
-        #voxel[idx]['rel_error'].append(i[3])
 
+    # get material to cell assignments
+    mat_assigns = dagmc.materials_to_cells(hdf5)
+    
     # Replace cell numbers with materials, eliminating duplicate materials
     # within single zone definition
     zones = {}
@@ -323,6 +320,7 @@ def _get_zones(mesh, hdf5, bounds):
             else:
                 voxel_zone[i] = y
     
+    # Put zones into format for PARTISN input
     if 'x' in bounds.keys():
         im = len(bounds['x']) - 1
     else:
@@ -348,74 +346,23 @@ def _get_zones(mesh, hdf5, bounds):
     return ZONES, zones_mats
     
 
-def _block03(xs_names):
-    block03 = {}
-    
-    block03['LIB'] = 'BXSLIB' # default
-    block03['NAMES'] = xs_names
-    #block03['MAXORD'] = 
-    
-    # Figure out the rest of this block later
-    
-    return block03
+def _nucid_to_xs(mat_lib, xs_names, nuc_names):
 
-
-def _block04(mat_lib, xs_names, nuc_names, zones):
-    block04 = {}
-    
-    # replace nucids with bsxlib names
-    mat_bxslib = {}
+    # replace nucids with xs library names
+    mat_xs_names = {}
     for mat in mat_lib.keys():
-        mat_bxslib[mat] = {}
+        mat_xs_names[mat] = {}
         for nucid in mat_lib[mat].keys():
             if nucid in nuc_names.keys():
                 name = nuc_names[nucid]
-                mat_bxslib[mat][name] = mat_lib[mat][nucid]
+                mat_xs_names[mat][name] = mat_lib[mat][nucid]
             else:
-                warn("Nucid {0} does not exist in nuc_names dictionary.".format(nucid))
-                mat_bxslib[mat]["{0}".format(nucid)] = mat_lib[mat][nucid]
-    
-    block04['MATLS'] = mat_bxslib
-    block04['ASSIGN'] = zones
-    
-    
-    
-    return block04
+                warn("Nucid {0} does not exist in the hdf5 geometry.".format(nucid))
+                mat_xs_names[mat]["{0}".format(nucid)] = mat_lib[mat][nucid]
+
+    return mat_xs_names
 
 
-def _block05(ngroup, bounds, nmq):
-    block05 = {}
-    # need volumetric source def here
-    # Calculation/Solver inputs
-    
-    block05['IEVT'] = 0 # default? 0 = source
-
-    # create source definition
-    # Volumetric source option 3
-    for i in bounds.keys():
-        if i == 'x':
-            it = len(bounds[i]) - 1
-            block05['SOURCX'] = np.zeros(shape=(it, nmq), dtype=float)
-            block05['SOURCX'][:,0] = 1.0
-        elif i == 'y':
-            jt = len(bounds[i]) - 1
-            block05['SOURCY'] = np.zeros(shape=(jt, nmq), dtype=float)
-            block05['SOURCY'][:,0] = 1.0
-        elif i == 'z':
-            kt = len(bounds[i]) - 1
-            block05['SOURCZ'] = np.zeros(shape=(kt, nmq), dtype=float)
-            block05['SOURCZ'][:,0] = 1.0
-    
-    block05['SOURCE'] = np.zeros(shape=(ngroup, nmq), dtype=float)
-    block05['SOURCE'][:,0] = 1.0
-    
-    return block05
-    
-
-def _write(title, block01, block02, block03, block04, block05):
-    pass
-    
-    
 def _title(hdf5):
     
     if "/" in hdf5:
@@ -428,3 +375,8 @@ def _title(hdf5):
     title = [name, dt]
     
     return title
+
+
+def _write_input(title, block01, block02, block03, block04, block05):
+    pass
+    
