f9785f6a8c10f7d9615ff2be4b3a9762e9e5e5c9
Anthony Scopatz <scopatz@gmail.com>
2015-02-10 16:29:44 -0600

    fix for dose_type str

--- pyne/material.pyx
+++ pyne/material.pyx
@@ -643,8 +643,12 @@ cdef class _Material:
             For a Material mat
         """
         cdef conv._MapIntDouble nucvec_proxy = conv.MapIntDouble()
+        cdef std_string dosetype
+        if not isinstance(dose_type, bytes):
+            dose_type = dose_type.encode()
+        dosetype = std_string(<char *> dose_type)
         nucvec_proxy.map_ptr = new cpp_map[int, double](
-                self.mat_pointer.dose_per_g(dose_type, source))
+                self.mat_pointer.dose_per_g(dosetype, source))
         return nucvec_proxy
 
 
