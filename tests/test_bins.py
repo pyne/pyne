25e983f1762537fa1cf726d55291bfd8330276f5
Anthony Scopatz <scopatz@gmail.com>
2015-04-06 17:21:34 -0500

    fixed bins error where memory beyond the array buffer was being read.

--- tests/test_bins.py
+++ tests/test_bins.py
@@ -52,7 +52,7 @@ def test_pointwise_linear_collapse():
         [np.array([1.0, 2.0]), np.array([-1.0, 2.0]), np.array([3.0, 6.0]), [5.5]],
         [np.array([0.0, 1.0]), np.array([0.0, 2.0]), np.array([0.0, 2.0]), [0.5]],
         [np.array([0.5, 1.5]), np.array([0.0, 2.0]), np.array([0.0, 2.0]), [1.0]],
-        [np.array([0.0, 1.0]), np.array([0.0, 0.75]), np.array([0.0, 1.0]), [3.0/8.0]],
+        [np.array([0.0, 1.0]), np.array([0.0, 0.75]), np.array([0.0, 1.0]), [2.0/3.0]],
         [np.array([1.0, 2.0]), np.array([0.0, 2.0]), np.array([4.0, 6.0]), [5.5]],
         ]
     for x_g, x, y, exp in cases:
