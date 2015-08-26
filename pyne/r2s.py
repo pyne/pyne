a1661b35f50aec6817891fe2ad55c04b62fe28eb
Elliott Biondo <elliott.biondo@gmail.com>
2015-08-26 16:06:13 -0500

    Minor removal

--- pyne/r2s.py
+++ pyne/r2s.py
@@ -2,7 +2,7 @@ from os.path import isfile
 from warnings import warn
 from pyne.utils import QAWarning
 
-from pyne.mesh import Mesh, IMeshTag
+from pyne.mesh import Mesh
 from pyne.mcnp import Meshtal
 from pyne.alara import mesh_to_fluxin, record_to_geom, photon_source_to_hdf5, \
                        photon_source_hdf5_to_mesh
