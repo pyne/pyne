d28c226a921fab74f5bead07ed3e71698085e4ae
Anthony Scopatz <scopatz@gmail.com>
2017-09-05 23:04:53 -0400

    more cleanup of setup.py and main cmake

--- cmake/ReplicatePythonSourceTree.cmake
+++ cmake/ReplicatePythonSourceTree.cmake
@@ -1,11 +1,15 @@
 # Note: when executed in the build dir, then CMAKE_CURRENT_SOURCE_DIR is the
 # build dir.
-file( COPY setup.py pyne DESTINATION "${CMAKE_ARGV3}"
-    FILES_MATCHING PATTERN "*.py" 
-                   PATTERN "*.pyw" 
-                   PATTERN "*.csv" 
-                   PATTERN "*.txt" 
-                   PATTERN "*.inp" 
-                   PATTERN "*.html" 
-                   PATTERN "*.pxi"
-                   PATTERN "*.mas12")
+file(COPY scripts DESTINATION "${CMAKE_ARGV3}")
+file(COPY setup.py setup_sub.py pyne DESTINATION "${CMAKE_ARGV3}"
+     FILES_MATCHING PATTERN "*.py"
+                    PATTERN "*.pyw"
+                    PATTERN "*.csv"
+                    PATTERN "*.txt"
+                    PATTERN "*.inp"
+                    PATTERN "*.html"
+                    PATTERN "*.pxi"
+                    PATTERN "*.pyi"
+                    PATTERN "*.pxd"
+                    PATTERN "*.mas12"
+                    )
