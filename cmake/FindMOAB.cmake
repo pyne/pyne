ebe40cd838c48707b6597e46ad81d155d1b7d2e9
Lucas John Jacobson <ljacobson64@gmail.com>
2017-10-25 15:42:56 -0500

    Don't error if MOAB not found

--- cmake/FindMOAB.cmake
+++ cmake/FindMOAB.cmake
@@ -5,7 +5,6 @@
 #  MOAB_FOUND - system has MOAB
 #  MOAB_INCLUDE_DIRS - the MOAB include directory
 #  MOAB_LIBRARIES - Link these to use MOAB
-#  MOAB_DEFINITIONS - Compiler switches required for using MOAB
 
 find_path(MOAB_CMAKE_CONFIG NAMES MOABConfig.cmake
           HINTS ${MOAB_ROOT}
@@ -13,16 +12,21 @@ find_path(MOAB_CMAKE_CONFIG NAMES MOABConfig.cmake
           PATH_SUFFIXES lib Lib cmake cmake/MOAB
           NO_DEFAULT_PATH)
 
-message(STATUS "Found MOAB in ${MOAB_CMAKE_CONFIG}")
+if (MOAB_CMAKE_CONFIG)
+  message(STATUS "Found MOAB in ${MOAB_CMAKE_CONFIG}")
 
-include(${MOAB_CMAKE_CONFIG}/MOABConfig.cmake)
+  include(${MOAB_CMAKE_CONFIG}/MOABConfig.cmake)
 
-string(STRIP "${DAGMC_LIBRARIES}" DAGMC_LIBRARIES)
-string(REGEX REPLACE ";" " " DAGMC_LIBRARIES "${DAGMC_LIBRARIES}")
-string(REGEX REPLACE "[ ]+" " " DAGMC_LIBRARIES "${DAGMC_LIBRARIES}")
-separate_arguments(DAGMC_LIBRARIES)
-list(REMOVE_DUPLICATES DAGMC_LIBRARIES)
+  string(STRIP "${DAGMC_LIBRARIES}" DAGMC_LIBRARIES)
+  string(REGEX REPLACE ";" " " DAGMC_LIBRARIES "${DAGMC_LIBRARIES}")
+  string(REGEX REPLACE "[ ]+" " " DAGMC_LIBRARIES "${DAGMC_LIBRARIES}")
+  separate_arguments(DAGMC_LIBRARIES)
+  list(REMOVE_DUPLICATES DAGMC_LIBRARIES)
 
-message(STATUS "  MOAB_INCLUDE_DIRS: ${MOAB_INCLUDE_DIRS}")
-message(STATUS "  MOAB_LIBRARY_DIRS: ${MOAB_LIBRARY_DIRS}")
-message(STATUS "  DAGMC_LIBRARIES: ${DAGMC_LIBRARIES}")
+  message(STATUS "  MOAB_INCLUDE_DIRS: ${MOAB_INCLUDE_DIRS}")
+  message(STATUS "  MOAB_LIBRARY_DIRS: ${MOAB_LIBRARY_DIRS}")
+  message(STATUS "  DAGMC_LIBRARIES: ${DAGMC_LIBRARIES}")
+else ()
+  set(MOAB_FOUND 0)
+  message(STATUS "MOAB not found. Not building pyne with MOAB.")
+endif ()
