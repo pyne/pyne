f65c99a4189c34592815bd1d4708ddea3bf9be63
Lucas John Jacobson <ljacobson64@gmail.com>
2017-10-25 21:53:53 -0500

    Get MOAB rpath working again

--- cmake/FindMOAB.cmake
+++ cmake/FindMOAB.cmake
@@ -2,9 +2,13 @@
 #
 # Once done this will define
 #
-#  MOAB_FOUND - system has MOAB
-#  MOAB_INCLUDE_DIRS - the MOAB include directory
-#  MOAB_LIBRARIES - Link these to use MOAB
+# MOAB_FOUND        - boolean indicating that MOAB is found
+# MOAB_INCLUDE_DIRS - include directories from which to pick up MOAB includes
+# MOAB_LIBRARY_DIRS - location of MOAB libraries
+# MOAB_LIBRARIES    - libraries need to link to MOAB
+# DAGMC_LIBRARIES   - MOAB_LIBRARIES plus DAGMC library
+# MOAB_CXX, MOAB_CC, MOAB_F77, MOAB_FC - compilers used to compile MOAB
+# MOAB_CXXFLAGS, MOAB_CFLAGS, MOAB_FORTRAN_FLAGS - compiler flags used to compile MOAB
 
 find_path(MOAB_CMAKE_CONFIG NAMES MOABConfig.cmake
           HINTS ${MOAB_ROOT}
@@ -17,15 +21,12 @@ if (MOAB_CMAKE_CONFIG)
 
   include(${MOAB_CMAKE_CONFIG}/MOABConfig.cmake)
 
-  string(STRIP "${DAGMC_LIBRARIES}" DAGMC_LIBRARIES)
-  string(REGEX REPLACE ";" " " DAGMC_LIBRARIES "${DAGMC_LIBRARIES}")
-  string(REGEX REPLACE "[ ]+" " " DAGMC_LIBRARIES "${DAGMC_LIBRARIES}")
-  separate_arguments(DAGMC_LIBRARIES)
-  list(REMOVE_DUPLICATES DAGMC_LIBRARIES)
+  if (NOT MOAB_LIBRARY_DIRS)
+    get_filename_component(MOAB_LIBRARY_DIRS ${MOAB_INCLUDE_DIRS}/../lib ABSOLUTE)
+  endif ()
 
   message(STATUS "  MOAB_INCLUDE_DIRS: ${MOAB_INCLUDE_DIRS}")
   message(STATUS "  MOAB_LIBRARY_DIRS: ${MOAB_LIBRARY_DIRS}")
-  message(STATUS "  DAGMC_LIBRARIES: ${DAGMC_LIBRARIES}")
 else ()
   set(MOAB_FOUND 0)
   message(STATUS "MOAB not found. Not building pyne with MOAB.")
