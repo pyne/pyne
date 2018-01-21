5e2535da468124a41b07adf4280f5fc611522268
xoviat <xoviat@users.noreply.github.com>
2018-01-21 12:09:35 -0600

    Extract library functions into cpp

---
+++ src/complex_windows.cpp
@@ -0,0 +1,40 @@
+#include <complex>
+
+
+struct _Dcomplex {
+    double real;
+    double imag;
+};  
+
+
+extern "C" _Dcomplex
+__divdc3(double __a, double __b, double __c, double __d)
+{
+    using namespace std::complex_literals;
+
+    std::complex<double> e = __a + 1i*__b;
+    std::complex<double> f = __c + 1i*__d;
+
+    std::complex<double> g = e * f;
+
+    return _Dcomplex{
+        g.real(),
+        g.imag()
+    };
+}
+
+extern "C" _Dcomplex
+__muldc3(double __a, double __b, double __c, double __d)
+{
+    using namespace std::complex_literals;
+
+    std::complex<double> e = __a + 1i*__b;
+    std::complex<double> f = __c + 1i*__d;
+
+    std::complex<double> g = e / f;
+
+    return _Dcomplex{
+        g.real(),
+        g.imag()
+    };
+}
