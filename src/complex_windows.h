3773f7aa8123840501f593c8af9fb85e04e83634
Anthony Scopatz <scopatz@gmail.com>
2018-02-23 14:23:56 -0500

    removed complex windows

--- src/complex_windows.h
+++
@@ -1,9 +0,0 @@
-#ifndef PYNE_4ECBRTN5INH4DBYJIJVWAWRNRU
-#define PYNE_4ECBRTN5INH4DBYJIJVWAWRNRU
-#if defined(__clang__) && defined(_MSC_VER)
-/* clang-cl */
-#define complex _Complex
-#define _Complex_I  (0.0F +  1.0iF)
-#define I _Complex_I
-#endif
-#endif  // PYNE_4ECBRTN5INH4DBYJIJVWAWRNRU
