846ada509e1fae40b618baba81f8dbc914299ef3
zxkjack123 <zxkjack123@163.com>
2018-03-03 18:15:16 -0600

    test for all defined units of to_sec, test for undefined unit

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -13,8 +13,68 @@ import numpy as np
 
 
 def test_to_sec():
+    # as
+    assert_equal(2e-18, utils.to_sec(2, 'AS'))
+    assert_equal(2e-18, utils.to_sec(2, 'attosec'))
+    assert_equal(2e-18, utils.to_sec(2, 'attosecond'))
+    assert_equal(2e-18, utils.to_sec(2, 'attoseconds'))
+    # fs
+    assert_equal(2e-15, utils.to_sec(2, 'FS'))
+    assert_equal(2e-15, utils.to_sec(2, 'femtosec'))
+    assert_equal(2e-15, utils.to_sec(2, 'femtosecond'))
+    assert_equal(2e-15, utils.to_sec(2, 'femtoseconds'))
+    # ps
+    assert_equal(2e-12, utils.to_sec(2, 'PS'))
+    assert_equal(2e-12, utils.to_sec(2, 'picosec'))
+    assert_equal(2e-12, utils.to_sec(2, 'picosecond'))
+    assert_equal(2e-12, utils.to_sec(2, 'picoseconds'))
+    # ns
+    assert_equal(2e-9, utils.to_sec(2, 'NS'))
+    assert_equal(2e-9, utils.to_sec(2, 'nanosec'))
+    assert_equal(2e-9, utils.to_sec(2, 'nanosecond'))
+    assert_equal(2e-9, utils.to_sec(2, 'nanoseconds'))
+    # us
+    assert_equal(2e-6, utils.to_sec(2, 'US'))
+    assert_equal(2e-6, utils.to_sec(2, 'microsec'))
+    assert_equal(2e-6, utils.to_sec(2, 'microsecond'))
+    assert_equal(2e-6, utils.to_sec(2, 'microseconds'))
+    # ms
+    assert_equal(2e-3, utils.to_sec(2, 'MS'))
+    assert_equal(2e-3, utils.to_sec(2, 'millisec'))
+    assert_equal(2e-3, utils.to_sec(2, 'millisecond'))
+    assert_equal(2e-3, utils.to_sec(2, 'milliseconds'))
+    # s
+    assert_equal(2.0, utils.to_sec(2, 'S'))
+    assert_equal(2.0, utils.to_sec(2, 'sec'))
+    assert_equal(2.0, utils.to_sec(2, 'second'))
+    assert_equal(2.0, utils.to_sec(2, 'seconds'))
+    # m
     assert_equal(120.0, utils.to_sec(2, 'M'))
-
+    assert_equal(120.0, utils.to_sec(2, 'min'))
+    assert_equal(120.0, utils.to_sec(2, 'minute'))
+    assert_equal(120.0, utils.to_sec(2, 'minutes'))
+    # h
+    assert_equal(7200.0, utils.to_sec(2, 'H'))
+    assert_equal(7200.0, utils.to_sec(2, 'hour'))
+    assert_equal(7200.0, utils.to_sec(2, 'hours'))
+    # d
+    assert_equal(172800.0, utils.to_sec(2, 'D'))
+    assert_equal(172800.0, utils.to_sec(2, 'day'))
+    assert_equal(172800.0, utils.to_sec(2, 'days'))
+    # w
+    assert_equal(1209600.0, utils.to_sec(2, 'W'))
+    assert_equal(1209600.0, utils.to_sec(2, 'week'))
+    assert_equal(1209600.0, utils.to_sec(2, 'weeks'))
+    # y
+    assert_equal(63115200.0, utils.to_sec(2, 'Y'))
+    assert_equal(63115200.0, utils.to_sec(2, 'year'))
+    assert_equal(63115200.0, utils.to_sec(2, 'years'))
+    # c
+    assert_equal(6311520000.0, utils.to_sec(2, 'C'))
+    assert_equal(6311520000.0, utils.to_sec(2, 'century'))
+    assert_equal(6311520000.0, utils.to_sec(2, 'centuries'))
+    # undifined unit trigs ValueError
+    assert_raises(ValueError, utils.to_sec, 2, 'month')
 
 def test_to_barns():
     assert_equal(3E3, utils.to_barns(3, 'KB'))
