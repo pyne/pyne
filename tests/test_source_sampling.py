67870ee853edfeeee67062e12954922b94a1cb44
zxkjack123 <zxkjack123@163.com>
2018-03-22 15:24:46 -0500

    clean up

--- tests/test_source_sampling.py
+++ tests/test_source_sampling.py
@@ -45,31 +45,16 @@ def test_single_tet_tag_names_map():
     tag_names = {"src_tag_name": "src"}
     e_bounds = np.array([0, 1])
     sampler = Sampler(filename, tag_names, e_bounds, 0)
+
     # src_tag_name not given
     tag_names = {}
     assert_raises(ValueError, Sampler, filename, tag_names, e_bounds, 0)
     assert_raises(ValueError, Sampler, filename, tag_names, e_bounds, 1)
+
     # bias_tag_name not given
     tag_names = {"src_tag_name": "src"}
     assert_raises(ValueError, Sampler, filename, tag_names, e_bounds, 2)
 
-@with_setup(None, try_rm_file('tet.h5m'))
-def test_single_tet_r2s_using_subvoxel_source():
-    """This test tests uniform sampling within a single tetrahedron. This is
-    done by dividing the tetrahedron in 4 smaller tetrahedrons and ensuring
-    that each sub-tet is sampled equally.
-    """
-    seed(1953)
-    m = Mesh(structured=True,
-             structured_coords=[[0, 3, 3.5], [0, 1], [0, 1]],
-             mats = None)
-    m.src = IMeshTag(2, float)
-    m.src[:] = [[2.0, 1.0], [9.0, 3.0]]
-    e_bounds = np.array([0, 0.5, 1.0])
-    m.bias = IMeshTag(2, float)
-    m.bias[:] = [[1.0, 2.0], [3.0, 3.0]]
-    filename = "sampling_mesh.h5m"
- 
     # subvoxel r2s source.h5m used for r2s calculation
     cell_fracs = np.zeros(2, dtype=[('idx', np.int64),
                                     ('cell', np.int64),
