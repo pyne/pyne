056dc341e0bb3e1091536489e25fdb5f6da6f50b
moatazharb <mharb@wisc.edu>
2018-04-12 05:08:24 +0000

    going back to treat clean as bool. this is more safe as only False will keep files otherwise it will delete them

--- pyne/alara.py
+++ pyne/alara.py
@@ -1039,7 +1039,7 @@ def calc_eta(data_dir, mats, neutron_spectrum, flux_magnitudes, irr_times,
                # tot = background and sup != background, eta = inf >> ste = 1e6
                eta[m, dt] = 1E6
                
-    if clean == 'True':
+    if clean:
         shutil.rmtree(run_dir)  
     return eta
 
@@ -1096,6 +1096,6 @@ def calc_T(data_dir, mats, neutron_spectrum, irr_times,
                 T[m, dt, n, :] = [
                     float(x) / (neutron_spectrum[n] * flux_magnitudes[0]) for x in l[3:]]
                 i += 1
-    if clean == 'True':
+    if clean:
         shutil.rmtree(run_dir)
     return T
