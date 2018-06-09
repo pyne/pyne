ffd450784fea137f2d486e962ddb8226f8e1ab52
tyler <tylerkyi6@gmail.com>
2018-06-09 11:18:09 -0500

    tests srim fixed

--- tests/test_srim_read.py
+++ tests/test_srim_read.py
@@ -5,15 +5,14 @@ from nose.tools import assert_equal, assert_true, assert_almost_equal, assert_ra
 from pyne import srim_read as sr
 
 
-sr_read1 = sr.read_srim_output('Helium')
-
 def test_read_srim_output():
-    assert_equal(sr_read1.bragg_corr,-0.83)
-    assert_equal(sr_read1.sr_read1.filename,'SRIM Outputs\\Helium in Mylar (ICRU-222)')
-    assert_equal(sr_read1.ion,'Helium')
-    assert_equal(sr_read1.sr_read1.ion_mass,4.003)
-    assert_equal(sr_read1.date,'November 15, 2017')
-    assert_equal(sr_read1.stopping_units,' MeV / (mg/cm2) ')
-    assert_equal(sr_read1.target_density_grams,'1.3970E+00 g/cm3')
-    assert_equal(sr_read1.target_density_atoms,'9.6311E+22 atoms/cm3')
+    assert_equal(sr.read_srim_output('Helium').bragg_corr,-0.83)
+    assert_equal(sr.read_srim_output('Helium').filename,'SRIM Outputs\\Helium in Mylar (ICRU-222)')
+    assert_equal(sr.read_srim_output('Helium').ion,'Helium')
+    assert_equal(sr.read_srim_output('Helium').ion_mass,4.003)
+    assert_equal(sr.read_srim_output('Helium').date,'November 15, 2017')
+    assert_equal(sr.read_srim_output('Helium').stopping_units,' MeV / (mg/cm2) ')
+    assert_equal(sr.read_srim_output('Helium').target_density_grams,'1.3970E+00 g/cm3')
+    assert_equal(sr.read_srim_output('Helium').target_density_atoms,'9.6311E+22 atoms/cm3')
+
 
