f02904f56343889df329ca1d9b0b5b33d9450690
zxkjack123 <zxkjack123@163.com>
2018-10-08 10:48:06 -0500

    fix bug in move to next voxel, direction could be -1

--- tests/test_vta.py
+++ tests/test_vta.py
@@ -116,9 +116,15 @@ def test_ray_voxel_traverse_3D():
     B = np.array([2, 2, 2])
     results = vta._ray_voxel_traverse(bounds, A, B)
     exp_ans = set([(0, 0, 0), (0, 0, 1), (0, 1, 1), (1, 1, 1)])
-#    for i in range(len(exp_ans)):
-#        assert_array_equal(results[i], exp_ans[i])
     assert_equal(exp_ans, results)
+
+    # reverse the ray direction
+    A = np.array([2, 2, 2])
+    B = np.array([0.3, 0.4, 0.5])
+    results = vta._ray_voxel_traverse(bounds, A, B)
+    exp_ans = set([(0, 0, 0), (0, 0, 1), (0, 1, 1), (1, 1, 1)])
+    assert_equal(exp_ans, results)
+
 
 def test_facet_voxel_rraverse():
     x_bound = [0, 1, 2, 3, 4, 5, 6]
