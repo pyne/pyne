4d3a7c8d50f2215c808932214add5a102c3f13b5
zxkjack123 <zxkjack123@163.com>
2018-10-09 10:24:52 -0500

    add a test

--- tests/test_vta.py
+++ tests/test_vta.py
@@ -64,6 +64,13 @@ def test_ray_voxel_traverse_3D():
     exp_ans = set()
     assert_equal(exp_ans, results)
 
+    # ray on the outside boundary surface of the mesh
+    A = np.array([0.0, 0.0, 0.0])
+    B = np.array([0.0, 0.0, 2.0])
+    results = vta._ray_voxel_traverse(bounds, A, B)
+    exp_ans = set()
+    assert_equal(exp_ans, results)
+
 def test_facet_voxel_rraverse():
     x_bound = [0, 1, 2, 3, 4, 5, 6]
     y_bound = [0, 1, 2, 3, 4, 5, 6]
@@ -72,7 +79,6 @@ def test_facet_voxel_rraverse():
     A = np.array([0.5, 0.5, 0.5])
     B = np.array([5.5, 0.5, 0.5])
     C = np.array([3, 5.5, 0.5])
-#    import pdb; pdb.set_trace()
     results = vta._facet_voxel_traverse(A, B, C, bounds)
     exp_ans = set([(2, 5, 0), (2, 4, 0), (2, 3, 0), (1, 3, 0),
                (1, 2, 0), (1, 1, 0), (0, 1, 0), (0, 0, 0),
