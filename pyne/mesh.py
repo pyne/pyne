05ba61c891724bfcf31f88be76e5d4bfbcd58b1b
Paul P.H. Wilson <paul.wilson@wisc.edu>
2019-01-30 20:08:06 -0600

    Add a docstring for StatMesh

--- pyne/mesh.py
+++ pyne/mesh.py
@@ -1433,6 +1433,20 @@ class Mesh(object):
                  size=1, dtype=float)
 
 class StatMesh(Mesh):
+    """This class extends the basic Mesh class by modifying the standard
+    mathematical operations that are performed on multiple meshes.
+
+    A StatMesh assumes that each value being operated upon also has a
+    statistical error associaed with it, and forces operations on the
+    statistical error as well.
+
+    For example, when to quantities are added together, c = a + b, the
+    statistical error of c is found by combining the statistical errors of a
+    and b.
+
+    """
+
+    
     def __init__(self, mesh=None, structured=False,
                  structured_coords=None, structured_set=None, mats=()):
 
