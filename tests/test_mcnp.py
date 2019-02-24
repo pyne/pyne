c6f9c91fe2f115d468a19c2cab15ea369caf7886
Matthew Koziol <matthewkoziol0@gmail.com>
2019-02-23 19:03:08 -0600

    Fix another test

--- tests/test_mcnp.py
+++ tests/test_mcnp.py
@@ -556,7 +556,7 @@ def test_read_mcnp():
 
     read_materials = mats_from_inp('mcnp_inp.txt')
     assert_equal(expected_material, read_materials[1])
-    assert_equal(
+    assert_almost_equal(
         list(expected_multimaterial._mats.keys())[0].comp,
         list(read_materials[2]._mats.keys())[0].comp)
     assert_equal(
