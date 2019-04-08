00ae02f0d834c11695566fa6a2a91c2f434b79a8
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2019-04-08 13:07:16 +0200

    cleaning

--- src/material.cpp
+++ src/material.cpp
@@ -647,7 +647,7 @@ std::string pyne::Material::mcnp(std::string frac_type) {
 
   // Set up atom or mass frac map
   std::map<int, double> fracs = get_density_frac(frac_type);
-  std::string frac_signi = "";
+  std::string frac_sign = "";
 
   if ("atom" != frac_type) {
     frac_sign = "-";
@@ -727,7 +727,7 @@ std::string pyne::Material::phits(std::string frac_type) {
 
   // Set up atom or mass frac map
   std::map<int, double> fracs = get_density_frac(frac_type);
-  std::string frac_signi = "";
+  std::string frac_sign = "";
 
   if ("atom" != frac_type) {
     frac_sign = "-";
@@ -1500,7 +1500,6 @@ std::map<int, double> pyne::Material::get_density_frac(std::string frac_type){
     } else {
       fracs = to_atom_frac();
     }
-    frac_sign = "";
   } else {
     fracs = comp;
     if (density != -1.0) {
