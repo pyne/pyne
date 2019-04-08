a5f4aacf170ccd1b437f7c45db6afa02776f651a
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2019-04-08 13:39:27 +0200

    fix

--- src/utils.h
+++ src/utils.h
@@ -89,7 +89,7 @@ namespace pyne {
   std::string capitalize(std::string s);
 
   /// Forms and returns the wrapped lines with a lenght up to line_lenght.
-  std::ostringstream line_wrapping(std::ostringstream line, int line_lenght = 80) {
+  std::ostringstream line_wrapping(std::ostringstream line, int line_lenght = 80);
 
   /// Finds and returns the first white-space delimited token of a line.
   /// \param line a character array to take the first token from.
