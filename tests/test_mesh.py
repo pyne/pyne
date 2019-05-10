8ac334a58d3ecb4f6fd6d8aff20a898712646e60
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2019-05-09 19:48:20 -0500

    fix

--- tests/test_mesh.py
+++ tests/test_mesh.py
@@ -553,10 +553,10 @@ def test_large_iterator():
 @with_setup(None, try_rm_file('test_matlib2.h5m'))
 def test_matlib():
     mats = {
-        0: Material({'H1': 1.0, 'K39': 1.0}, density=1.1, metadata={'mat_number'=1}),
-        1: Material({'H1': 0.1, 'O16': 1.0}, density=2.2, metadata={'mat_number'=2}),
+        0: Material({'H1': 1.0, 'K39': 1.0}, density=1.1, metadata={'mat_number':1}),
+        1: Material({'H1': 0.1, 'O16': 1.0}, density=2.2, metadata={'mat_number':2}),
         2: Material({'He4': 42.0}, density=3.3, metadata={'mat_numner'=3}),
-        3: Material({'Tm171': 171.0}, density=4.4, metadata={'mat_number'=4}),
+        3: Material({'Tm171': 171.0}, density=4.4, metadata={'mat_number':4}),
     }
     m = gen_mesh(mats=mats)
     for i, ve in enumerate(mesh_iterate(m.mesh)):
@@ -874,10 +874,10 @@ def test_issue360():
 
 def test_iter():
     mats = {
-        0: Material({'H1': 1.0, 'K39': 1.0}, density=42.0, metadata={'mat_number'=1}),
-        1: Material({'H1': 0.1, 'O16': 1.0}, density=43.0, metadata={'mat_number'=2}),
-        2: Material({'He4': 42.0}, density=44.0, metadata={'mat_number'=3}),
-        3: Material({'Tm171': 171.0}, density=45.0, metadata={'mat_number'=4}),
+        0: Material({'H1': 1.0, 'K39': 1.0}, density=42.0, metadata={'mat_number':1}),
+        1: Material({'H1': 0.1, 'O16': 1.0}, density=43.0, metadata={'mat_number':2}),
+        2: Material({'He4': 42.0}, density=44.0, metadata={'mat_number':3}),
+        3: Material({'Tm171': 171.0}, density=45.0, metadata={'mat_number':4}),
     }
     m = gen_mesh(mats=mats)
     j = 0
