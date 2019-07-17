36c2020f0cb168cb21d5cf336a3b8d7d9f2ce775
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2019-07-17 01:48:56 -0500

    better ??

--- pyne/material_library.pyx
+++ pyne/material_library.pyx
@@ -86,8 +86,8 @@ cdef class _MaterialLibrary:
                     mat = lib[key]
                     self.__setitem__(key, material.ensure_material(mat))
             elif isinstance(lib, basestring):
-                c_filename = lib
-                c_datapath = datapath
+                c_filename = lib.encode('UTF-8')
+                c_datapath = datapath.encode('UTF-8')
                 self._inst = new cpp_material_library.MaterialLibrary(c_filename, c_datapath)
             elif isinstance(lib, unicode):
                 c_filename = lib.encode('UTF-8')
