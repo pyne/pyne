e4d07b2a84653473ba5ed84e5aa3c18fcef10b86
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2019-07-24 13:59:42 -0500

    reset formating, removing tabs

--- src/tally.h
+++ src/tally.h
@@ -1,5 +1,5 @@
 /// \brief The tally class and helper functions.
-///
+/// 
 /// The tally class is in essesence a structure containing attributes
 /// related to tallies.
 
@@ -19,6 +19,7 @@
   #include "utils.h"
 #endif
 
+
 namespace pyne 
 {
   double def_empty_vec3[3] = {0, 0, 0};
@@ -28,25 +29,25 @@ namespace pyne
     std::map<std::string, std::string> rx2fluka;
     std::map<std::string, std::string> rx2mcnp5;
     std::map<std::string, std::string> rx2mcnp6;
-  
+
     /// Tally Constructors
     Tally();  /// empty constructor
   
     /// Constructor from passed in vars
     /// \param type the type of tally (flux or current)
     /// \param particle_name the name of the particle type
-    /// \param entity the entity id of the tally (eg. surface index,
+    /// \param entity the entity id of the tally (eg. surface index, 
     ///          volume number)
     /// \param entity_type (volume or surface)
     /// \param entity_name string identifying the entity
-    /// \param tally_name string identifying the tally
+    /// \param tally_name string identifying the tally 
     /// \param entity_size the physical size of the tally volume
     /// \param normalization the number required to normalize your tally
     Tally(std::string type, std::string particle_name, int entity,
           std::string entity_type, std::string entity_name,
-          std::string tally_name = "", double entity_size = 0.0,
+          std::string tally_name = "", double entity_size = 0.0, 
           double normalization = 1.0);
-  
+ 
     /// Constructor from passed in vars
     /// \param particle_name the name of the particle type
     /// \param entity the entity id of the tally (eg. surface index,
@@ -64,54 +65,54 @@ namespace pyne
           std::string tally_name = "", double norm = 1.0,
           double vec[3] = def_empty_vec3, double axl[3] = def_empty_vec3);
     ~Tally();  /// default destructor
-  
+ 
     // Create hdf5 datatable for tallies
     hid_t create_dataspace(hid_t file, std::string datapath);
-  
+
     // create filetype of data
     hid_t create_filetype();
-  
+
     // create memory type for tally struct
     hid_t create_memtype();
-  
+
     /// Dummy read method wrapper around c style strings
     /// \param filename the filename of the file to read from
     /// \param datapath _name the name of the region where tallies
     ///          are stored
     /// \param row  the array index of data to access
     void from_hdf5(char* filename, char* datapath, int row = -1);
-  
+
     /// Main read tally method
     /// \param filename the filename of the file to read from
     /// \param datapath _name the name of the region where tallies
     ///          are stored
     /// \param row  the array index of data to access
     void from_hdf5(std::string filename, std::string datapath, int row = -1);
-  
+
     /// Dummy write method wrapper around c style strings
     /// \param filename the filename of the file to write to
     /// \param datapath _name the name of the region where tallies
     ///          are to be stored
     void write_hdf5(char* filename, char* datapath);
-  
+
     /// Main write tally method
     /// \param filename the filename of the file to write to
     /// \param datapath _name the name of the region where tallies
     ///          are to be stored
     void write_hdf5(std::string filename, std::string datapath);
-  
+
     // mcnp tally
     std::string mcnp(int tally_index = 1, std::string mcnp_version = "mcnp5",
                      std::string out = "");
-  
+
     // Form the mcnp tally line as function of its properties
     std::string form_mcnp_tally(int tally_index, int type,
                                     std::string particle_token, int entity_id,
                                     double entity_size, double normalization);
-    
+
     // fluka tally 
     std::string fluka(std::string unit_number = "-21");
-  
+
     /// fundamental tally variables
     std::string entity_type; ///< the type of entity (volume,surface)
     std::string entity_name; ///< the name of the entity (optional)
@@ -121,7 +122,7 @@ namespace pyne
     int entity_id; ///< id number of the entity being tallied upon    
     double entity_size; ///< the physical size of the entity 
     double normalization; ///< the tally normalization 
-  
+
     /// fundemental mesh tally variables
     /// Mesh tally definition variable
     std::string entity_geometry;
