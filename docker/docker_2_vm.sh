86f9312dfd20c8ce76b39b4883f4ff5cade8f2b8
Paul P.H. Wilson <paul.wilson@wisc.edu>
2020-08-13 22:41:23 -0500

    Add booting to docker image for VM

--- docker/docker_2_vm.sh
+++ docker/docker_2_vm.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 # based on solution in https://gist.github.com/gunnarx/527fbc385a2e76f89609d837b6447f85
+# with additions from https://github.com/iximiuz/docker-to-linux
+
+set -e
+
+LOOPDEVICE=`losetup -f`
 
 docker_img=$1
 docker_ver=$2
@@ -11,16 +16,36 @@ docker_size=`echo ${docker_info##* }`
 docker_img_size=`echo 1000*${docker_size%??} + 1000 | bc`
 docker_img_size=`echo $docker_img_size/1 | bc`
 
+# create emtpy disk image
 dd if=/dev/zero of=$vm_name.img bs=1 count=0 seek=${docker_img_size}M
-mkfs.ext2 -F $vm_name.img
+
+# make partition onf disk image
+sfdisk $vm_name.img < partition.txt
+
+# format partition with ext3
+losetup -D
+losetup -o $(expr 512 \* 2048) $LOOPDEVICE $vm_name.img
+mkfs.ext3 $LOOPDEVICE
+
+# copy docker filesystem
 sudo mkdir -p /mnt/$vm_name
-sudo mount -o loop $vm_name.img /mnt/$vm_name
+sudo mount -t auto $LOOPDEVICE /mnt/$vm_name
 
 container=`docker run -d $docker_img:$docker_ver /bin/bash`
 docker export $container | sudo tar x -C /mnt/$vm_name
 docker stop $container
 
+#setup extlinux
+extlinux --install /mnt/$vm_name/boot
+cp syslinux.cfg /mnt/$vm_name/boot/syslinux.cfg
+
+# unmount
 sudo umount /mnt/$vm_name
+
+# write master boot record
+dd if=/usr/lib/syslinux/mbr/mbr.bin of=$vm_name.img bs=440 count=1 conv=notrunc
+
+## convert to VM virtual disk
 qemu-img convert -p -O vmdk $vm_name.img $vm_name.vmdk
 rm $vm_name.img
 
--- docker/ubuntu_18.04-dev.dockerfile
+++ docker/ubuntu_18.04-dev.dockerfile
@@ -180,3 +180,9 @@ RUN if [ "$install_openmc" = "YES" ]; then \
         && pip install . ; \
     fi
 
+ARG bootable="NO"
+RUN if [ "$bootable" != "NO" ]; then \
+       apt-get -y install --no-install-recommends linux-image-virtual systemd-sysv; \
+       fi;
+RUN echo "root:root" | chpasswd
+
