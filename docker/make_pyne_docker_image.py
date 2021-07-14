438182c6a70f450ea4c492e5f1fed2ca50feab46
Paul P.H. Wilson <paul.wilson@wisc.edu>
2021-07-14 08:48:00 -0500

    upgrade default v3 to 3.7

--- docker/make_pyne_docker_image.py
+++ docker/make_pyne_docker_image.py
@@ -84,7 +84,7 @@ def build_docker(args):
             if args.openmc:
                 raise ValueError("OpenMC Python API does not support python2!")
         elif args.py_version == 3:
-            docker_args += ["--build-arg", "py_version=3.6"]
+            docker_args += ["--build-arg", "py_version=3.7"]
         else:
             print("Can only deal with python 2 or 3")
             return
