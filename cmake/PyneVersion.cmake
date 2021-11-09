35aae69c4511c5a63687e59a2817f0f730727072
Paul P.H. Wilson <paul.wilson@wisc.edu>
2021-11-09 06:32:03 -0600

    add status message

--- cmake/PyneVersion.cmake
+++ cmake/PyneVersion.cmake
@@ -4,6 +4,8 @@ set(PYNE_MINOR_VERSION 7)
 set(PYNE_PATCH_VERSION 6)
 set(PYNE_VERSION ${PYNE_MAJOR_VERSION}.${PYNE_MINOR_VERSION}.${PYNE_PATCH_VERSION})
 
+MESSAGE(STATUS "Setting PyNE Version to ${PYNE_VERSION}")
+
 # Configure Pyne and cpp version headers
 configure_file(${PYNE_SUBMODULE_PATH}pyne/pyne_version.py.in ${PYNE_SUBMODULE_PATH}pyne/pyne_version.py)
 configure_file(${PYNE_SUBMODULE_PATH}src/pyne_version.h.in ${PYNE_SUBMODULE_PATH}src/pyne_version.h)
