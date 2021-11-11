a310f3173365bf2fb971b29d1ad777543ec445e0
Paul P.H. Wilson <paul.wilson@wisc.edu>
2021-11-11 08:19:38 -0600

    cleanup messages in macro

--- cmake/PyneVersion.cmake
+++ cmake/PyneVersion.cmake
@@ -4,10 +4,8 @@ set(PYNE_MINOR_VERSION 7)
 set(PYNE_PATCH_VERSION 6)
 set(PYNE_VERSION ${PYNE_MAJOR_VERSION}.${PYNE_MINOR_VERSION}.${PYNE_PATCH_VERSION})
 
-MESSAGE(STATUS "Setting PyNE Version to ${PYNE_VERSION} by changing files in ${PYNE_SUBMODULE_PATH}")
-
 # Configure Pyne and cpp version headers
 configure_file(${PYNE_SUBMODULE_PATH}pyne/pyne_version.py.in ${PYNE_SUBMODULE_PATH}${CMAKE_CURRENT_SOURCE_DIR}/pyne/pyne_version.py)
 configure_file(${PYNE_SUBMODULE_PATH}src/pyne_version.h.in ${PYNE_SUBMODULE_PATH}${CMAKE_CURRENT_SOURCE_DIR}/src/pyne_version.h)
 
-MESSAGE(STATUS "Set PyNE Version to ${PYNE_VERSION} in ${CMAKE_CURRENT_SOURCE_DIR}/pyne and ${CMAKE_CURRENT_SOURCE_DIR}/src")
+MESSAGE(STATUS "Setting PyNE Version to ${PYNE_VERSION} by changing files in ${PYNE_SUBMODULE_PATH}${CMAKE_CURRENT_SOURCE_DIR}")
