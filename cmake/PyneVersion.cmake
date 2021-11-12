1afea27fd47fc6534c63d762d4f648e962af6bff
Paul P.H. Wilson <paul.wilson@wisc.edu>
2021-11-12 11:41:50 -0600

    increasing robustness of submodul e path

--- cmake/PyneVersion.cmake
+++ cmake/PyneVersion.cmake
@@ -4,6 +4,11 @@ set(PYNE_MINOR_VERSION 7)
 set(PYNE_PATCH_VERSION 6)
 set(PYNE_VERSION ${PYNE_MAJOR_VERSION}.${PYNE_MINOR_VERSION}.${PYNE_PATCH_VERSION})
 
+if( NOT (PYNE_SUBMODULE_PATH) )
+   MESSAGE(STATUS "PyNE not used as submodule, using current directory")
+   set(PYNE_SUBMODULE_PATH "./")
+endif(PYNE_SUBMODULE_PATH)
+
 # Configure Pyne and cpp version headers
 configure_file(${PYNE_SUBMODULE_PATH}/pyne/pyne_version.py.in ${CMAKE_CURRENT_SOURCE_DIR}/${PYNE_SUBMODULE_PATH}/pyne/pyne_version.py)
 configure_file(${PYNE_SUBMODULE_PATH}/src/pyne_version.h.in ${CMAKE_CURRENT_SOURCE_DIR}/${PYNE_SUBMODULE_PATH}/src/pyne_version.h)
