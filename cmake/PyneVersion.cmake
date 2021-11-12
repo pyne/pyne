c0b345fc3463e3356f718dd80b785846689a7f65
Paul P.H. Wilson <paul.wilson@wisc.edu>
2021-11-12 11:20:05 -0600

    ensure message matches reality

--- cmake/PyneVersion.cmake
+++ cmake/PyneVersion.cmake
@@ -8,4 +8,4 @@ set(PYNE_VERSION ${PYNE_MAJOR_VERSION}.${PYNE_MINOR_VERSION}.${PYNE_PATCH_VERSIO
 configure_file(${PYNE_SUBMODULE_PATH}pyne/pyne_version.py.in ${CMAKE_CURRENT_SOURCE_DIR}/${PYNE_SUBMODULE_PATH}/pyne/pyne_version.py)
 configure_file(${PYNE_SUBMODULE_PATH}src/pyne_version.h.in ${CMAKE_CURRENT_SOURCE_DIR}/${PYNE_SUBMODULE_PATH}/src/pyne_version.h)
 
-MESSAGE(STATUS "Setting PyNE Version to ${PYNE_VERSION} by changing files in ${PYNE_SUBMODULE_PATH}${CMAKE_CURRENT_SOURCE_DIR}")
+MESSAGE(STATUS "Setting PyNE Version to ${PYNE_VERSION} by changing files in ${CMAKE_CURRENT_SOURCE_DIR}/${PYNE_SUBMODULE_PATH}")
