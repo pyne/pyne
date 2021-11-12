7b93f3d1da63266eb91cc47c02939394ce42a740
Paul P.H. Wilson <paul.wilson@wisc.edu>
2021-11-12 11:15:49 -0600

    change path of configured files

--- cmake/PyneVersion.cmake
+++ cmake/PyneVersion.cmake
@@ -5,7 +5,7 @@ set(PYNE_PATCH_VERSION 6)
 set(PYNE_VERSION ${PYNE_MAJOR_VERSION}.${PYNE_MINOR_VERSION}.${PYNE_PATCH_VERSION})
 
 # Configure Pyne and cpp version headers
-configure_file(${PYNE_SUBMODULE_PATH}pyne/pyne_version.py.in ${PYNE_SUBMODULE_PATH}${CMAKE_CURRENT_SOURCE_DIR}/pyne/pyne_version.py)
-configure_file(${PYNE_SUBMODULE_PATH}src/pyne_version.h.in ${PYNE_SUBMODULE_PATH}${CMAKE_CURRENT_SOURCE_DIR}/src/pyne_version.h)
+configure_file(${PYNE_SUBMODULE_PATH}pyne/pyne_version.py.in ${CMAKE_CURRENT_SOURCE_DIR}/${PYNE_SUBMODULE_PATH}/pyne/pyne_version.py)
+configure_file(${PYNE_SUBMODULE_PATH}src/pyne_version.h.in ${CMAKE_CURRENT_SOURCE_DIR}/${PYNE_SUBMODULE_PATH}/src/pyne_version.h)
 
 MESSAGE(STATUS "Setting PyNE Version to ${PYNE_VERSION} by changing files in ${PYNE_SUBMODULE_PATH}${CMAKE_CURRENT_SOURCE_DIR}")
