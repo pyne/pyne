92f27fd063ac288d7c6172792d89374d74099e8c
Paul P.H. Wilson <paul.wilson@wisc.edu>
2022-02-08 06:22:19 -0600

    more f-string removal

--- pyne/pyne_version.py
+++ pyne/pyne_version.py
@@ -10,14 +10,14 @@ PYNE_VERSION = '{0}.{1}.{2}'.format(PYNE_MAJOR_VERSION, PYNE_MINOR_VERSION, PYNE
 def write_cpp_header(path="."):
 
     with open(os.path.join(path , 'pyne_version.h') , 'w') as pvh:
-        header_text = f'''#ifndef PYNE_VERSION_HEADER
+        header_text = '''#ifndef PYNE_VERSION_HEADER
 #define PYNE_VERSION_HEADER
 
-#define PYNE_VERSION {PYNE_VERSION}
-#define PYNE_VERSION_STRING "{PYNE_VERSION}"
+#define PYNE_VERSION {0}
+#define PYNE_VERSION_STRING "{0}"
 
 #endif // PYNE_VERSION_HEADER
-'''
+'''.format(PYNE_VERSION)
         pvh.write(header_text)
 
 
