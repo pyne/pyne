780284f285176b3c9e6d081c8eb6f0b08cd4d741
bquan0 <powerbowen@gmail.com>
2023-03-03 10:06:27 -0600

    finish modifying bash script

--- scripts/ubuntu.sh
+++ scripts/ubuntu.sh
@@ -29,10 +29,6 @@ pip_package_list="numpy==1.23 \
                   future \
                   progress"
 
-
-# hdf5 std directory
-hdf5_libdir=/usr/lib/x86_64-linux-gnu/hdf5/serial
-
 # function to check if build folder already exists
 function check_repo() {
 
@@ -59,6 +55,9 @@ pip3 install ${pip_package_list}
 install_dir=${HOME}/opt
 mkdir -p ${install_dir}
 
+# hdf5 std directory
+hdf5_libdir=$HOME/opt/hdf5/$build_hdf5
+
 # need to put libhdf5.so on LD_LIBRARY_PATH (Making sure that LD_LIBRARY_PATH is defined first)
 if [ -z $LD_LIBRARY_PATH ]; then
   export LD_LIBRARY_PATH="${hdf5_libdir}"
@@ -77,20 +76,22 @@ mkdir -p moab
 cd moab
 
 # clone and version
-git clone --depth 1 --single-branch -b 5.3.0 https://bitbucket.org/fathomteam/moab 
+git clone --depth 1 --single-branch -b 5.3.0 https://bitbucket.org/fathomteam/moab moab-repo
 cd moab-repo
 mkdir -p build
 cd build
 
 # cmake, build and install
-cmake ../ -DENABLE_HDF5=ON -DHDF5_ROOT=${hdf5_libdir} \
+cmake ../ -DENABLE_PYMOAB=ON \
+          -DCMAKE_INSTALL_PREFIX=${install_dir}/moab \
+          -DENABLE_HDF5=ON -DHDF5_ROOT=${hdf5_libdir} \
           -DBUILD_SHARED_LIBS=ON \
-          -DENABLE_PYMOAB=ON \
           -DENABLE_BLASLAPACK=OFF \
-          -DENABLE_FORTRAN=OFF \
-          -DCMAKE_INSTALL_PREFIX=${install_dir}/moab
-make
+          -DENABLE_FORTRAN=OFF    
+make -j 3 
 make install
+cd ..
+rm -rf moab-repo
 
 # Adding MOAB/lib to $LD_LIBRARY_PATH and $LIBRARY_PATH
 export LD_LIBRARY_PATH="${install_dir}/moab/lib:$LD_LIBRARY_PATH"
@@ -114,7 +115,7 @@ mkdir -p dagmc
 cd dagmc
 
 # clone and version
-git clone https://github.com/svalinn/DAGMC.git dagmc-repo
+git clone --depth 1 --branch stable https://github.com/svalinn/DAGMC.git dagmc-repo
 cd dagmc-repo
 git checkout develop
 mkdir build
@@ -122,10 +123,17 @@ cd build
 
 # cmake, build and install
 cmake .. -DMOAB_DIR=${install_dir}/moab \
+         -DCMAKE_INSTALL_PREFIX=${install_dir}/dagmc \
          -DBUILD_STATIC_LIBS=OFF \
-         -DCMAKE_INSTALL_PREFIX=${install_dir}/dagmc
-make
+         -DBUILD_UWUW=OFF \
+         -DBUILD_TALLY=OFF \
+         -DBUILD_MAKE_WATERTIGHT=OFF \
+         -DBUILD_OVERLAP_CHECK=OFF \
+         -DBUILD_TESTS=OFF 
+make -j 3
 make install
+cd ../..
+rm -rf dagmc-repo
 
 # Adding DAGMC/lib to $LD_LIBRARY_PATH and $LIBRARY_PATH
 export LD_LIBRARY_PATH="${install_dir}/dagmc/lib:$LD_LIBRARY_PATH"
@@ -140,7 +148,7 @@ export PATH="${install_dir}/dagmc/bin:$PATH"
 cd ${install_dir}
 git clone https://github.com/openmc-dev/openmc.git
 cd openmc
-git checkout develop
+git checkout tags/v0.13.0
 mkdir bld
 cd bld
 cmake .. -DCMAKE_INSTALL_PREFIX=$HOME/.local
@@ -154,12 +162,13 @@ pip3 install .
 ############
 
 # pre-setup
+export PYNE_HDF5_ARGS="--hdf5 ${hdf5_libdir}"
 cd ${install_dir}
 check_repo pyne
 mkdir -p pyne
 cd pyne
 # clone and version
-git clone https://github.com/pyne/pyne.git pyne-repo
+git clone -b develop --single-branch https://github.com/pyne/pyne.git pyne-repo
 cd pyne-repo
 if [ $1 == 'stable' ] ; then
   TAG=$(git describe --abbrev=0 --tags)
@@ -169,7 +178,8 @@ fi
 python setup.py install --user \
                         --moab ${install_dir}/moab \
                         --dagmc ${install_dir}/dagmc \
-                        --clean
+                        ${PYNE_HDF5_ARGS} \
+                        --clean -j 3
 
 # Adding .local/lib to $LD_LIBRARY_PATH and $LIBRARY_PATH
 export LD_LIBRARY_PATH="${HOME}/.local/lib:$LD_LIBRARY_PATH"
