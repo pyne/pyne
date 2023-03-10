fa71e6989f38a59b5d7eb3e8e3d55446e28d39f3
Nancy Granda <grandaduarte@wisc.edu>
2023-03-10 20:47:06 +0000

    run test in serial

--- tests/ci-run-tests.sh
+++ tests/ci-run-tests.sh
@@ -6,12 +6,12 @@ for x in $(ls test*.py */test*.py); do
   echo "Testing $x:" 
   echo 
   if [ $x == "ensdf_processing.py" ]; then
-    nosetests test_ensdf_processing.py --process-timeout=120
+    pytest test_ensdf_processing.py --process-timeout=120
   else
-    nosetests "$x"
+    pytest "$x"
   fi
   status=$?
-  if [ $status -ne 0 ]; then 
+  if [ $status -ne 0 ] && [ $status -ne 5 ]; then 
     echo "Testing failed on $x" 
     exit $status
   fi
