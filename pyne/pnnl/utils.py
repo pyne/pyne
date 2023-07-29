7c1805538d471164b58f975ea55ac80b976f2070
Ahnaf Tahmid Chowdhury <ahnaf-tahmid@outlook.com>
2023-07-29 12:11:38 +0600

    Add Isotopes and Elements class

--- pyne/pnnl/utils.py
+++ pyne/pnnl/utils.py
@@ -2,9 +2,62 @@
 Separate classes and functions to perform specific tasks
 to retrieve specific information from the Materials Compendium
 """
+import json
 from dataclasses import dataclass
 import difflib
-from .materials_compendium import MaterialsCompendium, Datum
+from .materials_compendium import MaterialsCompendium, Datum, Isotope, Element
+
+
+@dataclass
+class Isotopes:
+    def __init__(self, isotope_data: Isotope):
+        self.weight_percent = isotope_data.WeightPercent
+        self.isotope = isotope_data.Isotope
+        self.weight_fraction_whole = isotope_data.WeightFraction_whole
+        self.isotopic_weight_fraction_whole = isotope_data.IsotopicWeightFraction_whole
+        self.weight_fraction = isotope_data.WeightFraction
+        self.abundance = isotope_data.Abundance
+        self.isotopic_atom_density = isotope_data.IsotopicAtomDensity
+        self.atomic_number_whole = isotope_data.AtomicNumber_whole
+        self.zaid = isotope_data.ZAID
+        self.atom_fraction = isotope_data.AtomFraction
+        self.atomic_number = isotope_data.AtomicNumber
+        self.isotopic_weight_fraction = isotope_data.IsotopicWeightFraction
+        self.relative_atomic_mass = isotope_data.RelativeAtomicMass
+        self.relative_atomic_mass_whole = isotope_data.RelativeAtomicMass_whole
+        self.isotopic_atom_fraction = isotope_data.IsotopicAtomFraction
+        self.abundance_whole = isotope_data.Abundance_whole
+        self.isotopic_atom_fraction_whole = isotope_data.IsotopicAtomFraction_whole
+        self.atom_fraction_whole = isotope_data.AtomFraction_whole
+        self.isotopic_atom_density_whole = isotope_data.IsotopicAtomDensity_whole
+
+    def __str__(self):
+        return f"Isotope: {self.isotope}, Weight Percent: {self.weight_percent}, ZAID: {self.zaid}"
+
+
+@dataclass
+class Elements:
+    def __init__(self, element_data: Element):
+        self.weight_fraction_whole = element_data.WeightFraction_whole
+        self.non_isotopic = element_data.NonIsotopic
+        self.element = element_data.Element
+        self.weight_fraction = element_data.WeightFraction
+        self.atomic_mass = element_data.AtomicMass
+        self.zaid = element_data.ZAID
+        self.atom_fraction = element_data.AtomFraction
+        self.atom_density_whole = element_data.AtomDensity_whole
+        self.atom_fraction_whole = element_data.AtomFraction_whole
+        self.id = element_data.id
+        self.isotopes = [
+            Isotopes(isotope_data) for isotope_data in element_data.Isotopes
+        ]
+        self.atom_density = element_data.AtomDensity
+        self.atomic_mass_whole = element_data.AtomicMass_whole
+        self.abundances = element_data.Abundances
+
+    def __str__(self):
+        return f"Element: {self.element}, Weight Fraction Whole: {self.weight_fraction_whole}, ZAID: {self.zaid}"
+
 
 @dataclass
 class Material:
@@ -12,7 +65,7 @@ class Material:
         self.comment = datum.Comment
         self.density = datum.Density
         self.acronym = datum.Acronym
-        self.elements = datum.Elements
+        self.elements = [Elements(element_data) for element_data in datum.Elements]
         self.source = datum.Source
         self.references = datum.References
         self.contact = datum.Contact
@@ -25,50 +78,56 @@ class Material:
         self.formula = datum.Formula
 
     def __str__(self):
-        return f"Material Name: {self.name} \nAcronym: {self.acronym} \nFormula: {self.formula} \nDensity: {self.density} \nSource: {self.source}"
+        element_info = []
+        for element in self.elements:
+            element_data = f" Element: {element.element} \n  Weight Fraction Whole: {element.weight_fraction_whole} \n  ZAID: {element.zaid} \n  Isotopes:{', '.join([isotope.isotope for isotope in element.isotopes])}"
+
+            element_info.append(element_data)
+        elements_str = "\n".join(element_info)
+        return f"Material Name: {self.name} \nAcronym: {self.acronym} \nFormula: {self.formula} \nDensity: {self.density} \nElements: \n{elements_str} \nSource: {self.source}"
 
     def get_comment(self):
         return self.comment
-    
+
     def get_density(self):
         return self.density
-    
+
     def get_acronym(self):
         return self.acronym
-    
+
     def get_elements(self):
         return self.elements
-    
+
     def get_source(self):
         return self.source
-    
+
     def get_references(self):
         return self.references
-    
+
     def get_contact(self):
         return self.contact
-    
+
     def get_material_atom_density(self):
         return self.material_atom_density
-    
+
     def get_mols(self):
         return self.mols
-    
+
     def get_mat_num(self):
         return self.mat_num
-    
+
     def get_material_weight(self):
         return self.material_weight
-    
+
     def get_name(self):
         return self.name
-    
+
     def get_verification_notes(self):
         return self.verification_notes
-    
+
     def get_formula(self):
         return self.formula
-    
+
     @classmethod
     def from_name(cls, material_name):
         matched_materials = []
@@ -93,13 +152,21 @@ class Material:
         for datum in MaterialsCompendium:
             if datum.Formula is not None and datum.Formula == material_formula:
                 return cls(datum)
-            elif datum.Formula is not None and difflib.SequenceMatcher(None, material_formula, datum.Formula).ratio() > 0.6:
+            elif (
+                datum.Formula is not None
+                and difflib.SequenceMatcher(
+                    None, material_formula, datum.Formula
+                ).ratio()
+                > 0.6
+            ):
                 # Consider a match if the similarity ratio is greater than 0.6 (adjust the threshold as needed)
                 matched_materials.append(datum.Formula)
-                
+
         if matched_materials:
             suggestions = "\n".join(matched_materials)
-            print(f"Material with formula '{material_formula}' not found. Did you mean:\n{suggestions}")
+            print(
+                f"Material with formula '{material_formula}' not found. Did you mean:\n{suggestions}"
+            )
         else:
             print(f"Material with formula '{material_formula}' not found in the data.")
         return None
@@ -110,13 +177,21 @@ class Material:
         for datum in MaterialsCompendium:
             if datum.Acronym is not None and datum.Acronym == material_acronym:
                 return cls(datum)
-            elif datum.Acronym is not None and difflib.SequenceMatcher(None, material_acronym, datum.Acronym).ratio() > 0.6:
+            elif (
+                datum.Acronym is not None
+                and difflib.SequenceMatcher(
+                    None, material_acronym, datum.Acronym
+                ).ratio()
+                > 0.6
+            ):
                 # Consider a match if the similarity ratio is greater than 0.6 (adjust the threshold as needed)
                 matched_materials.append(datum.Acronym)
 
         if matched_materials:
             suggestions = "\n".join(matched_materials)
-            print(f"Material with acronym '{material_acronym}' not found. Did you mean:\n{suggestions}")
+            print(
+                f"Material with acronym '{material_acronym}' not found. Did you mean:\n{suggestions}"
+            )
         else:
             print(f"Material with acronym '{material_acronym}' not found in the data.")
         return None
