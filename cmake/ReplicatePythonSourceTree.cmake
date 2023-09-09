240fa2a4c2f3256373bef66a091bb56b5ec524a0
Ahnaf Tahmid Chowdhury <ahnaf-tahmid@outlook.com>
2023-09-09 21:26:54 +0600

    remove setup_sub.py

--- cmake/ReplicatePythonSourceTree.cmake
+++ cmake/ReplicatePythonSourceTree.cmake
@@ -1,7 +1,7 @@
 # Note: when executed in the build dir, then CMAKE_CURRENT_SOURCE_DIR is the
 # build dir.
 file(COPY scripts DESTINATION "${CMAKE_ARGV3}")
-file(COPY setup.py setup_sub.py pyne DESTINATION "${CMAKE_ARGV3}"
+file(COPY setup.py pyne DESTINATION "${CMAKE_ARGV3}"
      FILES_MATCHING PATTERN "*.py"
                     PATTERN "*.pyw"
                     PATTERN "*.csv"
