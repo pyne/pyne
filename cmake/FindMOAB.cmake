80e277f00b3cdcba4e3c2b7cf76cc5664ff2e69e
Ahnaf Tahmid Chowdhury <ahnaf-tahmid@outlook.com>
2023-09-14 11:28:39 +0600

    fixing invalid character escape

--- cmake/FindMOAB.cmake
+++ cmake/FindMOAB.cmake
@@ -18,32 +18,34 @@ if (MOAB_LIBRARIES AND MOAB_INCLUDE_DIRS)
   set(MOAB_FOUND TRUE)
 else (MOAB_LIBRARIES AND MOAB_INCLUDE_DIRS)
   find_path(MOAB_INCLUDE_DIR NAMES MBiMesh.hpp
-    HINTS ${MOAB_ROOT}/include ${DEPS_INCLUDE_HINTS}
+    HINTS ${MOAB_ROOT}\\\\include ${DEPS_INCLUDE_HINTS}
     PATHS $ENV{HOME}/.local/include
     PATH_SUFFIXES include Include
-    PATHS "${BASE_DIR}/include" "${BASE_DIR}/../install/include"
+    PATHS "${BASE_DIR}\\\\include" "${BASE_DIR}\\\\..\\\\install\\\\include"
     ENV MOAB_ROOT
     NO_DEFAULT_PATH
     )
   find_path(MOAB_INCLUDE_DIR NAMES MBiMesh.hpp
-            HINTS ${MOAB_ROOT}/include ${DEPS_INCLUDE_HINTS})
+            HINTS ${MOAB_ROOT}\\\\include ${DEPS_INCLUDE_HINTS})
 
   find_library(MOAB_LIBRARY NAMES MOAB
-    HINTS ${MOAB_ROOT}/lib ${DEPS_LIB_HINTS}
+    HINTS ${MOAB_ROOT}\\\\lib ${DEPS_LIB_HINTS}
     PATHS $ENV{HOME}/.local/lib
         PATH_SUFFIXES lib Lib
-    PATHS "${BASE_DIR_LIB}" "${BASE_DIR_LIB}/../../install/lib"
+    PATHS "${BASE_DIR_LIB}" "${BASE_DIR_LIB}\\\\..\\\\..\\\\install\\\\lib"
     ENV MOAB_ROOT
     NO_DEFAULT_PATH
     )
   find_library(MOAB_LIBRARY NAMES MOAB
-               HINTS ${MOAB_ROOT}/lib ${DEPS_LIB_HINTS})
+               HINTS ${MOAB_ROOT}\\\\lib ${DEPS_LIB_HINTS})
 
   set(MOAB_INCLUDE_DIRS
       ${MOAB_INCLUDE_DIR} CACHE PATH "Path to MOAB headers")
 
   set(MOAB_LIBRARIES
       ${MOAB_LIBRARY} CACHE STRING "Directories to be linked to use MOAB")
+endif()
+
 
   include(FindPackageHandleStandardArgs)
   # handle the QUIETLY and REQUIRED arguments and set MOAB_FOUND to TRUE
