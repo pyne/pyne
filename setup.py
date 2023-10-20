cae54586ec0df972fee6f65087338e2d6855b6f5
Ahnaf Tahmid Chowdhury <ahnaf-tahmid@outlook.com>
2023-10-20 21:16:48 +0600

    try with ninja

--- setup.py
+++ setup.py
@@ -171,7 +171,7 @@ cmake_args = [
 
 # Specify GCC as the compiler for Windows
 if IS_NT:
-    #cmake_args.append("-DCMAKE_CXX_COMPILER:FILEPATH=g++")
+    cmake_args.append("-DCMAKE_CXX_COMPILER:FILEPATH=ninja")
     cmake_args.append("-DPYNE_FAST_COMPILE:BOOL=OFF")
 
 # Check for DAGMC_ROOT and MOAB_ROOT
