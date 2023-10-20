ea6767d4a7863c2b9892ec2f9b13a308b63afb96
Ahnaf Tahmid Chowdhury <ahnaf-tahmid@outlook.com>
2023-10-20 22:41:28 +0600

    test windows

--- setup.py
+++ setup.py
@@ -171,7 +171,7 @@ cmake_args = [
 
 # Specify GCC as the compiler for Windows
 if IS_NT:
-    cmake_args.append("-DCMAKE_CXX_COMPILER:FILEPATH=g++")
+    #cmake_args.append("-DCMAKE_CXX_COMPILER:FILEPATH=g++")
     cmake_args.append("-DCMAKE_CXX_FLAGS:STRING=-std=c++11")
     cmake_args.append("-DPYNE_FAST_COMPILE:BOOL=OFF")
 
