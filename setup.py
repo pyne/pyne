4be835c0121f368a99ab642df0d49f10ceb67343
Ahnaf Tahmid Chowdhury <ahnaf-tahmid@outlook.com>
2023-10-21 08:09:53 +0600

    MSVC_SUPPORTS_CXX11

--- setup.py
+++ setup.py
@@ -172,7 +172,6 @@ cmake_args = [
 # Specify GCC as the compiler for Windows
 if IS_NT:
     #cmake_args.append("-DCMAKE_CXX_COMPILER:FILEPATH=g++")
-    cmake_args.append("-DCMAKE_CXX_FLAGS:STRING=-std=c++11")
     cmake_args.append("-DPYNE_FAST_COMPILE:BOOL=OFF")
 
 # Check for DAGMC_ROOT and MOAB_ROOT
