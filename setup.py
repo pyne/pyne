67bc706f3f60e067f19546e7c2bff45dfc93483d
Ahnaf Tahmid Chowdhury <ahnaf-tahmid@outlook.com>
2023-10-21 11:28:52 +0600

    "-ARCHITECTURE:STRING=x64"

--- setup.py
+++ setup.py
@@ -171,8 +171,10 @@ cmake_args = [
 
 # Specify GCC as the compiler for Windows
 if IS_NT:
-    cmake_args.append("-GVisual Studio 17 2022")
-    cmake_args.append("-DCMAKE_CXX_COMPILER:FILEPATH=g++")
+    #cmake_args.append("-DCMAKE_CXX_COMPILER:FILEPATH=g++")
+    cmake_args.append("-GVisual Studio 2022")
+    cmake_args.append("-DCMAKE_GENERATOR_PLATFORM=x64")
+    cmake_args.append("-ARCHITECTURE:STRING=x64")
     cmake_args.append("-DPYNE_FAST_COMPILE:BOOL=OFF")
 
 # Check for DAGMC_ROOT and MOAB_ROOT
