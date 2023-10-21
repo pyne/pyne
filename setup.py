c09d513f1c1a418e3125be9c613d9a2f9c710c77
Ahnaf Tahmid Chowdhury <ahnaf-tahmid@outlook.com>
2023-10-21 16:01:10 +0600

    CMAKE_C_COMPILER

--- setup.py
+++ setup.py
@@ -173,7 +173,8 @@ cmake_args = [
 if IS_NT:
     cmake_args.append("-GVisual Studio 17 2022")
     cmake_args.append("-DCMAKE_CXX_COMPILER:FILEPATH=g++")
-    cmake_args.append("-DCMAKE_Fortran_COMPILER:FILEPATH=C:/mingw/bin/gfortran")
+    cmake_args.append("-DCMAKE_C_COMPILER:FILEPATH=gcc")
+    cmake_args.append("-DCMAKE_Fortran_COMPILER:FILEPATH=gfortran")
     cmake_args.append("-DPYNE_FAST_COMPILE:BOOL=OFF")
 
 # Check for DAGMC_ROOT and MOAB_ROOT
