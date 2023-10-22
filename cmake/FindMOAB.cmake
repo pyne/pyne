6febb31d2c6daa999b5a2315ee2a7bd4a4f805b7
Ahnaf Tahmid Chowdhury <ahnaf-tahmid@outlook.com>
2023-10-22 20:36:00 +0600

    simple moab

--- cmake/FindMOAB.cmake
+++ cmake/FindMOAB.cmake
@@ -11,48 +11,29 @@
 #  Redistribution and use is allowed according to the terms of the New
 #  BSD license.
 #  For details see the accompanying COPYING-CMAKE-SCRIPTS file.
-#
 
 if (MOAB_LIBRARIES AND MOAB_INCLUDE_DIRS)
   # in cache already
   set(MOAB_FOUND TRUE)
 else (MOAB_LIBRARIES AND MOAB_INCLUDE_DIRS)
-  find_path(MOAB_INCLUDE_DIR NAMES MBiMesh.hpp
-    HINTS ${MOAB_ROOT}/include ${DEPS_INCLUDE_HINTS}
-    PATH_SUFFIXES include Include
-    PATHS "${BASE_DIR}/include" "${BASE_DIR}/../install/include"
-    ENV MOAB_ROOT
-    NO_DEFAULT_PATH
-    )
-  find_path(MOAB_INCLUDE_DIR NAMES MBiMesh.hpp
+  find_path(MOAB_INCLUDE_DIR NAMES MBiMesh.hpp moab_export.h
             HINTS ${MOAB_ROOT}/include ${DEPS_INCLUDE_HINTS})
 
   find_library(MOAB_LIBRARY NAMES MOAB
-    HINTS ${MOAB_ROOT}/lib ${DEPS_LIB_HINTS}
-    PATHS $ENV{HOME}/.local/lib
-        PATH_SUFFIXES lib Lib
-    PATHS "${BASE_DIR_LIB}" "${BASE_DIR_LIB}/../../install/lib"
-    ENV MOAB_ROOT
-    NO_DEFAULT_PATH
-    )
-  find_library(MOAB_LIBRARY NAMES MOAB
               HINTS ${MOAB_ROOT}/lib ${DEPS_LIB_HINTS})
 
-  set(MOAB_INCLUDE_DIRS
-      ${MOAB_INCLUDE_DIR} CACHE PATH "Path to MOAB headers")
-
-  set(MOAB_LIBRARIES
-      ${MOAB_LIBRARY} CACHE STRING "Directories to be linked to use MOAB")
+  set(MOAB_INCLUDE_DIRS ${MOAB_INCLUDE_DIR} CACHE PATH "Path to MOAB headers")
+  set(MOAB_LIBRARIES ${MOAB_LIBRARY} CACHE STRING "Directories to be linked to use MOAB")
 
   include(FindPackageHandleStandardArgs)
-  # handle the QUIETLY and REQUIRED arguments and set MOAB_FOUND to TRUE
-  # if all listed variables are TRUE
-  find_package_handle_standard_args(MOAB  DEFAULT_MSG
-      MOAB_LIBRARY MOAB_INCLUDE_DIRS)
+  find_package_handle_standard_args(MOAB  DEFAULT_MSG MOAB_LIBRARY MOAB_INCLUDE_DIRS)
+
   if (MOAB_FOUND)
     message(STATUS "MOAB header files: ${MOAB_INCLUDE_DIRS}")
     message(STATUS "MOAB library: ${MOAB_LIBRARY}")
   endif (MOAB_FOUND)
+
   mark_as_advanced(MOAB_INCLUDE_DIRS MOAB_LIBRARIES)
+
 endif (MOAB_LIBRARIES AND MOAB_INCLUDE_DIRS)
 
