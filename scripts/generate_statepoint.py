8509e2c690f04d070dd804d4f32b843147b17f35
Edgar-21 <ejpflug@gmail.com>
2024-04-26 15:15:19 -0500

    add script to generate statepoint file, update statepoint filename in test_openmc_utils

---
+++ scripts/generate_statepoint.py
@@ -0,0 +1,36 @@
+import openmc
+import os
+
+box = openmc.model.RectangularParallelepiped(-40, 80, -12.5, 25, -2.5, 5,
+                                              boundary_type='vacuum')
+box_cell = openmc.Cell(region=-box)
+geometry = openmc.Geometry([box_cell])
+
+mesh = openmc.RegularMesh()
+mesh.lower_left = (-40, -12.5, -2.5)
+mesh.upper_right = (80, 25, 5)
+mesh.dimension = (3,2,1)
+
+energy_bins = [0.0, 1e6, 2e6]
+
+energy_filter = openmc.EnergyFilter(energy_bins)
+mesh_filter = openmc.MeshFilter(mesh)
+
+tally = openmc.Tally(name='flux tally', tally_id=1)
+tally.filters = [energy_filter, mesh_filter]
+tally.scores = ['flux']
+tallies = openmc.Tallies([tally])
+
+settings = openmc.Settings()
+settings.particles = 1000
+settings.batches = 10
+settings.output = {'path':'../tests/files_test_openmc/',
+                   'summary': False,
+                   'tallies': False}
+settings.run_mode = 'fixed source'
+
+model = openmc.Model(geometry=geometry, settings=settings, tallies=tallies)
+
+model.run()
+
+os.remove('model.xml')
