10ff444796eb1e11f528f84cdc2f21bc3bc0c042
Ahnaf Tahmid Chowdhury <ahnaf-tahmid@outlook.com>
2024-05-18 21:15:03 +0600

    update find method

--- cmake/FindDAGMC.cmake
+++ cmake/FindDAGMC.cmake
@@ -1,8 +1,11 @@
 # - Try to find DAGMC
 # Once done this will define
 #
+#  DAGMC_CMAKE_CONFIG - The path to the DAGMCConfig.cmake file
 #  DAGMC_FOUND - system has DAGMC
+#  DAGMC_VERSION - the version of DAGMC found
 #  DAGMC_INCLUDE_DIRS - the DAGMC include directory
+#  DAGMC_LIBRARY_DIRS - the DAGMC library directory
 #  DAGMC_LIBRARIES - Link these to use DAGMC
 #  DAGMC_DEFINITIONS - Compiler switches required for using DAGMC
 #
@@ -13,19 +16,26 @@
 #  For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 #
 
+message(STATUS "DAGMC_ROOT: ${DAGMC_ROOT}")
 
-message(STATUS ${DAGMC_ROOT})
-
+# Find DAGMC cmake config
 find_path(DAGMC_CMAKE_CONFIG NAMES DAGMCConfig.cmake
           HINTS ${DAGMC_ROOT}
           PATH_SUFFIXES lib Lib cmake dagmc lib/cmake/dagmc
-          NO_DEFAULT_PATH)
+          NO_DEFAULT_PATH
+          )
 
-message(STATUS "Found DAGMC in ${DAGMC_CMAKE_CONFIG}")
-
-if( "DAGMC_CMAKE_CONFIG-NOTFOUND" STREQUAL "${DAGMC_CMAKE_CONFIG}")
-  set(DAGMC_FOUND FALSE)
+# Include DAGMC config
+if(DAGMC_CMAKE_CONFIG)
+  set(DAGMC_CMAKE_CONFIG ${DAGMC_CMAKE_CONFIG}/DAGMCConfig.cmake)
+  include(${DAGMC_CMAKE_CONFIG})
+  message(STATUS "DAGMC_CMAKE_CONFIG: ${DAGMC_CMAKE_CONFIG}")
+  message(STATUS "DAGMC Include directory: ${DAGMC_INCLUDE_DIRS}")
+  message(STATUS "DAGMC Library directories: ${DAGMC_LIBRARY_DIRS}")
+  message(STATUS "DAGMC Libraries: ${DAGMC_LIBRARIES}")
+  message(STATUS "DAGMC library version: ${DAGMC_VERSION}")
 else()
-  include(${DAGMC_CMAKE_CONFIG}/DAGMCConfig.cmake)
-  set(DAGMC_FOUND TRUE)
+  message(WARNING "Could not find DAGMC config file. "
+    "Set -DDAGMC_ROOT=/path/to/DAGMC when running cmake or use the $DAGMC_ROOT environment variable."
+    )
 endif()
--- cmake/FindMOAB.cmake
+++ cmake/FindMOAB.cmake
@@ -1,8 +1,12 @@
 # This code sets the following variables:
 #
+#  MOAB_CMAKE_CONFIG
 #  MOAB_FOUND
 #  MOAB_INCLUDE_DIRS
+#  MOAB_LIBRARY_DIRS
 #  MOAB_LIBRARIES
+
+message(STATUS "MOAB_ROOT: ${MOAB_ROOT}")
 
 # Find MOAB cmake config file
 set(MOAB_SEARCH_DIRS)
@@ -13,15 +17,16 @@ find_path(MOAB_CMAKE_CONFIG
   PATHS ${MOAB_SEARCH_DIRS}
   NO_DEFAULT_PATH
 )
+
+# Include MOAB config file
 if (MOAB_CMAKE_CONFIG)
   set(MOAB_CMAKE_CONFIG ${MOAB_CMAKE_CONFIG}/MOABConfig.cmake)
+  include(${MOAB_CMAKE_CONFIG})
   message(STATUS "MOAB_CMAKE_CONFIG: ${MOAB_CMAKE_CONFIG}")
+  message(STATUS "MOAB Include directory: ${MOAB_INCLUDE_DIRS}")
+  message(STATUS "MOAB Library directories: ${MOAB_LIBRARY_DIRS}")
+  message(STATUS "MOAB Libraries: ${MOAB_LIBRARIES}")
+  message(STATUS "MOAB library version: ${MOAB_VERSION}")
 else ()
-  message(FATAL_ERROR "Could not find MOAB. Set -DMOAB_ROOT=/path/to/MOAB when running cmake or use the $MOAB_ROOT environment variable.")
+  message(WARNING "Could not find MOAB. Set -DMOAB_ROOT=/path/to/MOAB when running cmake or use the $MOAB_ROOT environment variable.")
 endif ()
-
-# Include MOAB config
-include(${MOAB_CMAKE_CONFIG})
-
-# Add MOAB include directories
-include_directories(${MOAB_INCLUDE_DIRS})
