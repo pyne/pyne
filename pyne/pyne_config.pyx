031cad11b760b16b42e89f5864a8a27c379598ae
Ahnaf Tahmid Chowdhury <ahnaf-tahmid@outlook.com>
2024-10-17 10:08:51 +0600

    Temporary fix for old pyne

--- pyne/pyne_config.pyx
+++ pyne/pyne_config.pyx
@@ -24,7 +24,7 @@ def get_path(subdir, pattern="*"):
 
 def get_include_path():
     """Return the include directory path for PyNE headers."""
-    return os.path.join(sys.prefix, "include")
+    return os.path.join(__path__[0], '..', '..', '..', '..', "include")
 
 def get_core_libraries():
     """Return library paths and library directory paths."""
